173f27b42c2cf97b58cab4079c40aba1bdab1250
Brad Vincent <bradvin@gmail.com>
2014-05-08 07:28:02 +0200

    refactored gallery columns

--- includes/admin/class-columns.php
+++ includes/admin/class-columns.php
@@ -3,18 +3,23 @@
  * FooGallery Admin Columns class
  */
 
-if (!class_exists('FooGallery_Admin_Columns')) {
+if ( ! class_exists( 'FooGallery_Admin_Columns' ) ) {
 
 	class FooGallery_Admin_Columns {
+
 		function __construct() {
-			add_filter( 'manage_upload_columns', array($this, 'setup_media_columns') );
-			add_action( 'manage_media_custom_column', array($this, 'media_columns_content'), 10, 2 );
-			add_filter( 'manage_edit-' . FOOGALLERY_CPT_GALLERY . '_columns', array($this, 'gallery_custom_columns') );
-			add_action( 'manage_posts_custom_column', array($this, 'gallery_custom_column_content' ));
+			//add_filter( 'manage_upload_columns', array($this, 'setup_media_columns') );
+			//add_action( 'manage_media_custom_column', array($this, 'media_columns_content'), 10, 2 );
+			add_filter( 'manage_edit-' . FOOGALLERY_CPT_GALLERY . '_columns', array(
+				$this,
+				'gallery_custom_columns'
+			) );
+			add_action( 'manage_posts_custom_column', array( $this, 'gallery_custom_column_content' ) );
 		}
 
 		function setup_media_columns( $columns ) {
-			$columns['_galleries'] = __('Galleries', 'foogallery');
+			$columns['_galleries'] = __( 'Galleries', 'foogallery' );
+
 			return $columns;
 		}
 
@@ -22,19 +27,33 @@ if (!class_exists('FooGallery_Admin_Columns')) {
 
 		}
 
-		function gallery_custom_columns($columns) {
-			$new_columns = array(
-				FOOGALLERY_CPT_GALLERY . '_count' => __('Images', 'foogallery')
-			);
-			return array_merge($columns, $new_columns);
+		function gallery_custom_columns( $columns ) {
+			return array_slice( $columns, 0, 1, true ) +
+			       array( 'icon' => '' ) +
+			       array_slice( $columns, 1, null, true ) +
+			       array(
+				       FOOGALLERY_CPT_GALLERY . '_count' => __( 'Images', 'foogallery' ),
+				       FOOGALLERY_CPT_GALLERY . '_shortcode' => __( 'Shortcode', 'foogallery' )
+			       );
 		}
 
-		function gallery_custom_column_content($column) {
+		function gallery_custom_column_content( $column ) {
 			global $post;
 
-			if ( $column == FOOGALLERY_CPT_GALLERY . '_count' ) {
-				$gallery = FooGallery::get($post);
-				echo sizeof( $gallery->attachments(false) );
+			switch ( $column ) {
+				case FOOGALLERY_CPT_GALLERY . '_count':
+					$gallery = FooGallery::get( $post );
+					echo sizeof( $gallery->attachments( false ) );
+					break;
+				case FOOGALLERY_CPT_GALLERY . '_shortcode':
+					echo '<code>' . foogallery_build_gallery_shortcode( $post->ID ) . '</code>';
+					break;
+				case 'icon':
+					$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+					if ( $post_thumbnail_id && $thumb = wp_get_attachment_image( $post_thumbnail_id, array(80, 60), true ) ) {
+						echo $thumb;
+					}
+					break;
 			}
 		}
 	}
