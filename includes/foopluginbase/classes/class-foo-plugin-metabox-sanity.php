5432ee1ffabddb1b49a15094173d89a7ca8a5867
Brad Vincent <bradvin@gmail.com>
2014-05-25 13:01:09 +0200

    formatting

--- includes/foopluginbase/classes/class-foo-plugin-metabox-sanity.php
+++ includes/foopluginbase/classes/class-foo-plugin-metabox-sanity.php
@@ -28,12 +28,12 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if ( ! class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
+if ( !class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
 	/**
 	 * Provides an intuitive way for plugins to whitelist
 	 * metaboxes on their custom post type interfaces.
 	 *
-	 * @since 2.1.0
+	 * @since   2.1.0
 	 *
 	 * @package Foo_Plugin_Base
 	 * @author  Brad Vincent
@@ -42,18 +42,21 @@ if ( ! class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
 
 		protected $plugin_slug;
 
-		public function __construct( $plugin_slug ) {
+		public function __construct($plugin_slug) {
 			$this->plugin_slug = $plugin_slug;
 
 			add_action( 'add_meta_boxes', array($this, 'maintain_sanity'), 999 );
 		}
 
+		/**
+		 * TODO
+		 */
 		function maintain_sanity() {
 
 			$metabox_sanity_config = apply_filters( $this->plugin_slug . '_metabox_sanity', false );
 
 			// If there is no data, return.
-			if ( empty( $metabox_sanity_config ) ) {
+			if ( empty($metabox_sanity_config) ) {
 				return;
 			}
 
@@ -63,7 +66,7 @@ if ( ! class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
 			// Loop through each post type and start whitelisting metaboxes!
 			foreach ( $metabox_sanity_config as $post_type => $data ) {
 				// If no contexts or priorities have been specified, do nothing.
-				if ( empty( $data['contexts'] ) || empty( $data['priorities'] ) ) {
+				if ( empty($data['contexts']) || empty($data['priorities']) ) {
 					continue;
 				}
 
@@ -71,12 +74,12 @@ if ( ! class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
 				foreach ( (array) $data['contexts'] as $context ) {
 					// Now loop through each priority within the context.
 					foreach ( (array) $data['priorities'] as $priority ) {
-						if ( isset( $wp_meta_boxes[$post_type][$context][$priority] ) ) {
+						if ( isset($wp_meta_boxes[$post_type][$context][$priority]) ) {
 							// Loop through each priority and check the data to determine whether the metabox will stay or go.
 							foreach ( (array) $wp_meta_boxes[$post_type][$context][$priority] as $id => $metabox_data ) {
 								// If the metabox ID matches one to pass over, whitelist it and allow it to be registered.
 								if ( in_array( $id, (array) $data['whitelist'] ) ) {
-									unset( $data['whitelist'][$id] );
+									unset($data['whitelist'][$id]);
 									continue;
 								}
 
@@ -88,7 +91,7 @@ if ( ! class_exists( 'Foo_Plugin_Metabox_Sanity_v1' ) ) {
 								}
 
 								// The metabox is not allowed on this screen. Prevent it from being registered.
-								unset( $wp_meta_boxes[$post_type][$context][$priority][$id] );
+								unset($wp_meta_boxes[$post_type][$context][$priority][$id]);
 							}
 						}
 					}
