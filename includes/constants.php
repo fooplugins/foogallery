74ba71216cbac55b941551a7a31acb565323da94
Brad Vincent <bradvin@gmail.com>
2014-05-25 12:47:54 +0200

    removed album constant

--- includes/constants.php
+++ includes/constants.php
@@ -7,10 +7,6 @@ if ( ! defined( 'FOOGALLERY_CPT_GALLERY' ) ) {
 	define('FOOGALLERY_CPT_GALLERY', 'foogallery');
 }
 
-if ( ! defined( 'FOOGALLERY_TAX_ALBUM' ) ) {
-	define('FOOGALLERY_TAX_ALBUM', 'foogallery-album');
-}
-
 if ( ! defined( 'FOOGALLERY_META_SETTINGS' ) ) {
 	define('FOOGALLERY_META_SETTINGS', 'foogallery_settings');
 }
