62675f223b75f75e83cb8f5ea6696d2a998d8b94
Brad Vincent <bradvin@gmail.com>
2014-05-25 12:47:45 +0200

    refactored templates out

--- includes/functions.php
+++ includes/functions.php
@@ -15,59 +15,7 @@
  * @return array
  */
 function foogallery_gallery_templates() {
-	$gallery_templates[] = array(
-		'key'         => 'default',
-		'name'        => __( 'Responsive Image Gallery', 'foogallery'),
-		'description' => __( 'The default image gallery template : clean and responsive and looks good in any theme.', 'foogallery'),
-		'author'      => 'FooPlugins',
-		'author_url'  => 'http://fooplugins.com',
-		'demo_url'    => 'http://fooplugins.com/plugins/foogallery',
-		'thumbnail'   => FOOGALLERY_URL . 'templates/default/thumb.png',
-		'fields'	  => array(
-			array(
-				'id'      => 'link_to_image',
-				'title'   => __('Link To Image', 'foogallery'),
-				'desc'    => __('Should the gallery thumbnails link to the full size images. If not set, then the images will link to the attachment page.', 'foogallery'),
-				'default' => 'on' ,
-				'type'    => 'checkbox',
-				//'section' => __('Responsive Image Gallery Settings', 'foogallery'),
-			),
-			array(
-				'id'      => 'lightbox',
-				'title'   => __('Lightbox', 'foogallery'),
-				'desc'    => __('Choose which lightbox you want to use in the gallery.', 'foogallery'),
-				'type'    => 'select',
-			),
-		)
-	);
-
-	$gallery_templates[] = array(
-		'key'         => 'masonry',
-		'name'        => __( 'Masonry Image Gallery', 'foogallery'),
-		'description' => __( 'A masonry-style image gallery template', 'foogallery'),
-		'author'      => 'FooPlugins',
-		'author_url'  => 'http://fooplugins.com',
-		'demo_url'    => 'http://fooplugins.com/plugins/foogallery',
-		'thumbnail'   => FOOGALLERY_URL . 'templates/masonry/thumb.png',
-		'fields'	  => array(
-			array(
-				'id'      => 'link_to_image',
-				'title'   => __('Link To Image2', 'foogallery'),
-				'desc'    => __('Should the gallery thumbnails link to the full size images. If not set, then the images will link to the attachment page.', 'foogallery'),
-				'default' => 'on' ,
-				'type'    => 'checkbox',
-				//'section' => __('Responsive Image Gallery Settings', 'foogallery'),
-			),
-			array(
-				'id'      => 'lightbox',
-				'title'   => __('Lightbox2', 'foogallery'),
-				'desc'    => __('Choose which lightbox you want to use in the gallery.', 'foogallery'),
-				'type'    => 'select',
-			),
-		)
-	);
-
-	return apply_filters( 'foogallery_gallery_templates', $gallery_templates );
+	return apply_filters( 'foogallery_gallery_templates', array() );
 }
 
 /**
@@ -75,8 +23,8 @@ function foogallery_gallery_templates() {
  *
  * @return
  */
-function foogallery_extension_api() {
-
+function foogallery_extensions_api() {
+	return new FooGallery_Extensions_API();
 }
 
 /**
@@ -86,15 +34,6 @@ function foogallery_extension_api() {
  */
 function foogallery_default_gallery_template() {
 	return foogallery_get_setting( 'gallery_template' );
-}
-
-/**
- * Returns if the menu should move under Media
- *
- * @return bool
- */
-function foogallery_use_media_menu() {
-	return foogallery_get_setting( 'use_media_menu' );
 }
 
 /**
@@ -142,7 +81,6 @@ function foogallery_get_default( $key ) {
 
 	$defaults = array(
 		'gallery_template'              => 'default',
-		'use_media_menu'                => false,
 		'gallery_permalinks_enabled'    => false,
 		'gallery_permalink'             => 'gallery',
 		'lightbox'                      => 'none'
