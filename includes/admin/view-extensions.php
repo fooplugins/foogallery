d123bd14c67bc6bb8af710131bcc929db5328b77
Brad Vincent <bradvin@gmail.com>
2014-05-25 12:46:19 +0200

    extensions refactor

--- includes/admin/view-extensions.php
+++ includes/admin/view-extensions.php
@@ -1,221 +1,144 @@
 <?php
 $instance = FooGallery_Plugin::get_instance();
-$info = $instance->get_plugin_info();
+$api = new FooGallery_Extensions_API();
+
+$extensions = $api->get_all();
+
+$categories = $api->get_all_categories();
+
+$show_message = safe_get_from_request('show_message');
+
+if ( 'yes' === $show_message ) {
+	$result = get_transient( FOOGALLERY_EXTENSIONS_MESSAGE_TRANSIENT_KEY );
+}
 ?>
 <style>
 	.foogallery-badge-foobot {
 		position: absolute;
-		top: 15px;
+		top: 5px;
 		right: 0;
-		background:url(<?php echo FOOGALLERY_URL; ?>assets/foobot.png) no-repeat;
-		width:109px;
-		height:200px;
+		background:url(<?php echo FOOGALLERY_URL; ?>assets/foobot_small.png) no-repeat;
+		width:82px;
+		height:150px;
+		z-index: 100;
 	}
-
-	.foogallery-extension-browser {
-		margin-top: 20px;
+	.foogallery-text {
+		font-size:18px;
+		margin: 10px 0;
 	}
-
-		.foogallery-extension-browser .extensions .extension {
-			float: left;
-			margin: 0 4% 4% 0;
-			position: relative;
-			width: 30.6%;
-			border: 1px solid #DEDEDE;
-			-webkit-box-shadow: 0 1px 1px -1px rgba(0, 0, 0, 0.1);
-			box-shadow: 0 1px 1px -1px rgba(0, 0, 0, 0.1);
-			-webkit-box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			box-sizing: border-box;
-		}
-
-			.foogallery-extension-browser .extensions .extension h3 {
-				font-size: 15px;
-				font-weight: 600;
-				height: 18px;
-				line-height: 18px;
-				margin: 0;
-				padding: 15px;
-				-webkit-box-shadow: inset 0 1px 0 rgba(0, 0, 0, 0.1);
-				box-shadow: inset 0 1px 0 rgba(0, 0, 0, 0.1);
-				overflow: hidden;
-				white-space: nowrap;
-				text-overflow: ellipsis;
-				background: #FFF;
-				background: rgba(255, 255, 255, 0.65);
-			}
-
-				.foogallery-extension-browser .extensions .extension h3 .new {
-					background: #D54E21;
-					background: rgba(213, 78, 33, 0.95);
-					-webkit-border-radius: 3px;
-					border-radius: 3px;
-					padding:2px 5px;
-					color:#fff;
-					font-size: 10px;
-					margin-left: 5px;
-					margin-bottom: 3px;
-				}
-
-			.foogallery-extension-browser .extensions .extension .screenshot {
-				-webkit-transform: translateZ(0);
-				-webkit-transition: opacity .2s ease-in-out;
-				transition: opacity .2s ease-in-out;
-			}
-
-				.foogallery-extension-browser .extensions .extension:hover .screenshot {
-					opacity: .4;
-				}
-
-			.foogallery-extension-browser .extensions .extension .extension-actions {
-				-webkit-transition: opacity .1s ease-in-out;
-				transition: opacity .1s ease-in-out;
-				position: absolute;
-				bottom: 0;
-				right: 0;
-				height: 38px;
-				padding: 9px 10px 0;
-			}
-
-			.foogallery-extension-browser .extensions .extension .extension-details {
-				-ms-filter: "alpha(Opacity=0)";
-				opacity: 0;
-				position: absolute;
-				top: 25%;
-				right: 20%;
-				left: 20%;
-				background: #222;
-				background: rgba(0, 0, 0, 0.7);
-				color: #FFF;
-				font-size: 15px;
-				text-shadow: 0 1px 0 rgba(0, 0, 0, 0.6);
-				-webkit-font-smoothing: antialiased;
-				font-weight: 600;
-				padding: 15px 12px;
-				text-align: center;
-				-webkit-border-radius: 3px;
-				border-radius: 3px;
-				-webkit-transition: opacity .1s ease-in-out;
-				transition: opacity .1s ease-in-out;
-			}
-
-				.foogallery-extension-browser .extensions .extension:hover .extension-details {
-					-ms-filter: "alpha(Opacity=1)";
-					opacity: 1;
-				}
-
-			.foogallery-extension-browser .extensions .extension.active .banner {
-				background: #0074A2;
-				color: #FFF;
-				display: block;
-				font-size: 13px;
-				font-weight: 400;
-				height: 48px;
-				line-height: 48px;
-				padding: 0 10px 0 40px;
-				position: absolute;
-				top: 0;
-				right: 0;
-				left: 0;
-				border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-				overflow: hidden;
-			}
-
-				.foogallery-extension-browser .extensions .extension.active .banner:before {
-					content: "\f147";
-					display: inline-block;
-					font: 400 40px/1 dashicons;
-					top: 5px;
-					left: 0;
-					position: absolute;
-					speak: none;
-					-webkit-font-smoothing: antialiased;
-				}
-
-			.foogallery-extension-browser .extensions .extension.active .activate {
-				display: none;
-			}
-
-			.foogallery-extension-browser .extensions .extension .deactivate {
-				display: none;
-			}
-
-			.foogallery-extension-browser .extensions .extension.active .deactivate {
-				display: block;
-			}
+	.spinner {
+		width: 16px;
+		height: 16px;
+		background: url(/wp-admin/images/wpspin_light.gif) no-repeat;
+	}
 </style>
-<div class="wrap about-wrap">
+<script>
+
+</script>
+<div class="wrap about-wrap extensions-wrap">
+	<?php
+	if ( isset( $result ) ) { ?>
+		<div class="foogallery-message-<?php echo $result['type']; ?>">
+			<p><?php echo $result['message']; ?></p>
+		</div>
+	<?php } ?>
 	<h1><?php _e( 'FooGallery Extensions', 'foogallery' ); ?></h1>
 
-	<div class="about-text"><?php _e( 'Extensions make FooGallery even more awesome, without bloating the core plugin.', 'foogallery' ); ?></div>
+	<div class="foogallery-text"><?php _e( 'Extensions make FooGallery even more awesome, without bloating the core plugin.', 'foogallery' ); ?></div>
 	<div class="foogallery-badge-foobot"></div>
 
-	<h2 class="nav-tab-wrapper">
-		<a class="nav-tab" href="#">
-			<?php _e( "Getting Started", 'foogallery' ); ?>
-		</a>
-		<a class="nav-tab nav-tab-active" href="<?php echo foogallery_admin_extensions_url(); ?>">
-			<?php _e( 'Extensions', 'foogallery' ); ?>
-		</a>
-		<a class="nav-tab" href="#other">
-			<?php _e( 'Other Plugins', 'foogallery' ); ?>
-		</a>
-	</h2>
-
-	<div class="foogallery-extension-browser">
-		<div class="extensions">
-
-			<div class="extension active">
-
-				<div class="screenshot">
-					<img src="http://wpfoo/wp-content/themes/stargazer/screenshot.png" alt="">
-				</div>
-
-				<div class="extension-details">
-					<p>Some awesome description that means something</p>
-					<a href="#">By FooPlugins</a>
-				</div>
-
-				<h3>Albums</h3>
-
-
-				<div class="extension-actions">
-
-					<a class="button button-primary activate" href="#">Activate</a>
-					<a class="button button-secondary deactivate" href="#">Deactivate</a>
-
-				</div>
-
-
-				<div class="banner">Activated</div>
-
-			</div>
-
-			<div class="extension">
-
-				<div class="screenshot">
-					<img src="http://wpfoo/wp-content/themes/stargazer/screenshot.png" alt="">
-				</div>
-
-				<div class="extension-details">
-					<p>Enable the FooBox lightbox</p>
-					<a href="#">By FooPlugins</a>
-				</div>
-
-				<h3>FooBox<span class="new">New!</span></h3>
-
-
-
-				<div class="extension-actions">
-
-					<a class="button button-primary activate" href="#">Activate</a>
-					<a class="button button-secondary deactivate" href="#">Deactivate</a>
-
-				</div>
-
-			</div>
+	<h2 class="foo-nav-tabs nav-tab-wrapper">
+		<?php
+		foreach ( $categories as $category_slug=>$category ) {
+			$class = ( $category['first'] ) ? ' nav-tab-active' : '';
+			echo "<a href=\"#{$category_slug}\" class=\"nav-tab{$class}\">{$category['name']}</a>";
+		} ?><a href="#build_your_own" class="nav-tab"><?php _e('Build Your Own', 'foogallery'); ?></a>
+		<div class="extension-search-box">
+			<label class="screen-reader-text" for="plugin-search-input">Search Extensions:</label>
+			<input placeholder="<?php echo __('search...', 'foogallery'); ?>" type="search" id="extensions-search-input">
 		</div>
+		<div class="extension-reload">
+			<a class="button" href="<?php echo add_query_arg( 'action', 'reload' ); ?>"><span class="dashicons dashicons-update"></span> <?php _e('Reload', 'foogallery'); ?></a>
+		</div>
+	</h2>
+</div>
 
+<div class="foogallery-extension-browser">
+	<div class="extensions">
+		<?php foreach( $extensions as $extension ) {
+			$slug = $extension['slug'];
+			$classes = 'extension extension-' . $slug;
+			if ( $api->is_downloaded( $extension ) ) {
+				$classes .= ' downloaded';
+			} else {
+				$classes .= ' download';
+			}
+			if ( $api->is_active( $slug, true ) ) {
+				$classes .= ' active';
+			}
+			if ( $api->has_loading_errors( $slug ) ) {
+				$classes .= ' has_error';
+			}
+			$tag_html = '';
+			if ( isset( $extension['tags'] ) ) {
+				foreach ( $extension['tags'] as $tag ) {
+					$classes .= ' ' . $tag;
+					$tag_html .= '<span class="tag ' . $tag . '">'. $tag . '</span>';
+				}
+			}
+			foreach ( $extension['categories'] as $category ) {
+				$classes .= ' ' . foo_convert_to_key($category);
+			}
+			if ( isset( $extension['css_class'] ) ) {
+				$classes .= ' ' . $extension['css_class'];
+			}
+			$thumbnail = $extension['thumbnail'];
+			if ( foo_starts_with( $thumbnail, '/') ) {
+				$thumbnail = rtrim( FOOGALLERY_URL, '/' ) . $thumbnail;
+			}
+			$base_url = add_query_arg( 'extension', $slug );
+			$download_url = add_query_arg( 'action', 'download', $base_url );
+			$activate_url = add_query_arg( 'action', 'activate', $base_url );
+			$deactivate_url = add_query_arg( 'action', 'deactivate', $base_url );
+
+			$download_button_html = '';
+
+			//check if we want to override the download button
+			if ( isset( $extension['download_button'] ) ) {
+				$download_button = $extension['download_button'];
+				$download_button_href = isset( $download_button['href'] ) ? $download_button['href'] : $download_url;
+				$download_button_target = isset( $download_button['target'] ) ? ' target="' . $download_button['target'] . '" ' : '';
+				$download_button_text = isset( $download_button['text'] ) ? __( $download_button['text'], 'foogallery' ) : '';
+				$download_button_confirm = isset( $download_button['confirm'] ) ? ' data-confirm="' .$download_button['confirm'] . '" ' : '';
+				$download_button_html = "<a class=\"button button-primary download\" {$download_button_target}href=\"{$download_button_href}\" >{$download_button_text}</a>";
+			}
+			?>
+		<div class="<?php echo $classes; ?>">
+
+			<div class="screenshot" style="background: url(<?php echo $thumbnail; ?>) no-repeat"></div>
+
+			<div class="extension-details">
+				<p class="search-me"><?php echo $extension['description']; ?></p>
+				<a target="_blank" href="<?php echo $extension['author_url']; ?>">By <?php echo $extension['author']; ?></a>
+			</div>
+
+			<h3 class="search-me"><?php echo $extension['title'] . $tag_html; ?></h3>
+
+			<div class="extension-actions">
+				<span class="spinner"></span>
+				<?php if ( !empty( $download_button_html ) ) {
+					echo $download_button_html;
+				} else { ?>
+				<a class="button button-primary download" data-confirm="<?php _e('Are you sure you want to download this extension?', 'foogallery'); ?>" href="<?php echo $download_url; ?>"><?php _e('Download', 'foogallery'); ?></a>
+				<?php } ?>
+				<a class="button button-primary activate" href="<?php echo $activate_url; ?>"><?php _e('Activate', 'foogallery'); ?></a>
+				<a class="button button-secondary deactivate" href="<?php echo $deactivate_url; ?>"><?php _e('Deactivate', 'foogallery'); ?></a>
+
+			</div>
+			<div class="banner active-banner"><?php _e('Activated', 'foogallery'); ?></div>
+			<div class="banner error-banner"><?php _e('Error loading extension!', 'foogallery'); ?></div>
+			<div class="banner coming-soon-banner"><?php _e('Coming Soon!', 'foogallery'); ?></div>
+		</div>
+		<?php } ?>
 	</div>
-
 </div>
