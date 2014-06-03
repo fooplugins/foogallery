b4666da9b6792e20916509ad002aec82615368ed
Brad Vincent <bradvin@gmail.com>
2014-06-03 19:22:44 +0200

    added help file

---
+++ templates/where-are-the-templates.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Q : Where are the built-in gallery templates?
+ * A : They can be found in "wp-content/foogallery/extensions/default-templates"
+ *
+ * Q : Why are they not in "wp-content/foogallery/templates"?
+ * A : We wanted the built-in templates to be packaged in a FooGallery extension, so that you can disable them if you wanted to.
+*/
