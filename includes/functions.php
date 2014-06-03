2626323f49ba14b5f8a7b144fdc89bedef3a1d17
Brad Vincent <bradvin@gmail.com>
2014-06-03 19:26:41 +0200

    more functions

--- includes/functions.php
+++ includes/functions.php
@@ -120,3 +120,71 @@ function foogallery_admin_settings_url() {
 function foogallery_admin_extensions_url() {
 	return esc_url( admin_url( add_query_arg( array('page' => 'foogallery-extensions'), 'index.php' ) ) );
 }
+
+/**
+ * @TODO
+ * @param      $key
+ * @param bool $default
+ *
+ * @return bool
+ */
+function foogallery_gallery_template_setting( $key, $default = false ) {
+	global $current_foogallery;
+	global $current_foogallery_arguments;
+	global $current_foogallery_template;
+
+	$settings_key = "{$current_foogallery_template}_{$key}";
+
+	if ( $current_foogallery_arguments && array_key_exists( $key, $current_foogallery_arguments ) ) {
+		//try to get the value from the arguments
+		$value = $current_foogallery_arguments[ $key ];
+
+	} else if ( $current_foogallery->settings && array_key_exists( $settings_key, $current_foogallery->settings ) ) {
+		//then get the value out of the saved gallery settings
+		$value = $current_foogallery->settings[$settings_key];
+	} else {
+		//otherwise set it to the default
+		$value = $default;
+	}
+
+	$value = apply_filters( 'foogallery_gallery_template_setting-' . $key, $value );
+
+	return $value;
+}
+
+/**
+ * @TODO
+ * @param        $attachment_id
+ * @param string $size
+ * @param string $link
+ *
+ * @return string
+ */
+function foogallery_get_attachment_html( $attachment_id, $size = 'thumbnail', $link = 'image' ) {
+	if ( 'none' === $link ) {
+		return wp_get_attachment_image( $attachment_id, $size );
+	}
+	return wp_get_attachment_link( $attachment_id, $size, ( 'page' === $link ) );
+}
+
+/**
+ * @TODO
+ * @return string
+ */
+function foogallery_admin_menu_parent_slug() {
+	return apply_filters( 'foogallery_menu_parent_slug', FOOGALLERY_ADMIN_MENU_PARENT_SLUG );
+}
+
+/**
+ * @TODO
+ * @param array $extra_args
+ *
+ * @return string|void
+ */
+function foogallery_build_admin_menu_url( $extra_args = array() ) {
+	$url = admin_url( foogallery_admin_menu_parent_slug() );
+	if ( !empty( $extra_args ) ) {
+		$url = add_query_arg( $extra_args, $url );
+	}
+	return $url;
+}
