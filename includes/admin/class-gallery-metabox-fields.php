7cdd67174c472eea9db9341df6c846c1d0d8ea6e
Brad Vincent <bradvin@gmail.com>
2014-06-03 19:29:13 +0200

    metabox refactor

---
+++ includes/admin/class-gallery-metabox-fields.php
@@ -0,0 +1,224 @@
+<?php
+
+if ( !class_exists( 'FooGallery_Admin_Gallery_MetaBox_Fields' ) ) {
+
+	class FooGallery_Admin_Gallery_MetaBox_Fields {
+		function __construct() {
+			//handle some default field types that all templates can reuse
+			add_filter( 'foogallery_alter_gallery_template_field', array( $this, 'alter_gallery_template_field' ), 10, 2 );
+
+			//render the different types of fields for our gallery settings
+			add_action( 'foogallery_render_gallery_template_field', array( $this, 'render_gallery_template_field'), 10, 3 );
+		}
+
+		/**
+		 * @TODO
+		 * @param array $field
+		 * @param       $gallery FooGallery
+		 * @param       $template
+		 */
+		function render_gallery_template_field($field = array(), $gallery, $template) {
+			$template_slug = $template['slug'];
+
+			//only declare up front so no debug warnings are shown
+			$type = $id = $desc = $default = $placeholder = $choices = $class = $section = $spacer = null;
+
+			//allow for the field to be altered by extensions. Also used by the build-in fields, e.g. lightbox
+			$field = apply_filters( 'foogallery_alter_gallery_template_field', $field, $gallery );
+
+			extract( $field );
+
+			$id = $template_slug . '_' . $id;
+
+			$field_value = $gallery->get_meta( $id, $default );
+
+			$field_class = empty($class) ? '' : ' class="' . $class . '"';
+
+			$choices = apply_filters( 'foogallery_render_gallery_template_field_choices', $choices, $field, $gallery );
+
+			$field['choices'] = $choices;
+
+			//allow for customization
+			do_action( 'foogallery_render_gallery_template_field_before', $field, $gallery );
+
+			echo '<div class="foogallery_metabox_field-' . $type . '">';
+
+			switch ( $type ) {
+
+				case 'html':
+					echo $desc;
+					$desc = '';
+					break;
+
+				case 'checkbox':
+					if ( isset($gallery->settings[$id]) && $gallery->settings[$id] == 'on' ) {
+						$field_value = 'on';
+					} else if ( !isset($gallery->settings) && $default == 'on' ) {
+						$field_value = 'on';
+					} else {
+						$field_value = '';
+					}
+
+					$checked = 'on' === $field_value ? ' checked="checked"' : '';
+					echo '<input' . $field_class . ' type="checkbox" id="FooGallerySettings_' . $id . '" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . ']" value="on"' . $checked . ' />';
+					break;
+
+				case 'select':
+					echo '<select' . $field_class . ' id="FooGallerySettings_' . $id . '" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . ']">';
+					foreach ( $choices as $value => $label ) {
+						$selected = '';
+						if ( $field_value == $value ) {
+							$selected = ' selected="selected"';
+						}
+						echo '<option ' . $selected . ' value="' . $value . '">' . $label . '</option>';
+					}
+
+					echo '</select>';
+					break;
+
+				case 'radio':
+					$i = 0;
+					$spacer = isset($spacer) ? $spacer : '<br />';
+					foreach ( $choices as $value => $label ) {
+						$selected = '';
+						if ( $field_value == $value ) {
+							$selected = ' checked="checked"';
+						}
+						echo '<input' . $field_class . $selected . ' type="radio" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . ']"  id="FooGallerySettings_' . $id . $i . '" value="' . $value . '"> <label for="FooGallerySettings_' . $id . $i . '">' . $label . '</label>';
+						if ( $i < count( $choices ) - 1 ) {
+							echo $spacer;
+						}
+						$i++;
+					}
+					break;
+
+				case 'textarea':
+					echo '<textarea' . $field_class . ' id="FooGallerySettings_' . $id . '" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . ']" placeholder="' . $placeholder . '">' . esc_attr( $field_value ) . '</textarea>';
+
+					break;
+
+				case 'text':
+					echo '<input class="regular-text ' . $class . '" type="text" id="FooGallerySettings_' . $id . '" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . ']" placeholder="' . $placeholder . '" value="' . esc_attr( $field_value ) . '" />';
+
+					break;
+
+				case 'checkboxlist':
+					$i = 0;
+					foreach ( $choices as $value => $label ) {
+
+						$checked = '';
+						if ( isset($field_value[$value]) && $field_value[$value] == 'true' ) {
+							$checked = 'checked="checked"';
+						}
+
+						echo '<input' . $field_class . ' ' . $checked . ' type="checkbox" name="' . FOOGALLERY_META_SETTINGS . '[' . $id . '|' . $value . ']" id="FooGallerySettings_' . $id . $i . '" value="on"> <label for="FooGallerySettings_' . $id . $i . '">' . $label . '</label>';
+						if ( $i < count( $choices ) - 1 ) {
+							echo '<br />';
+						}
+						$i++;
+					}
+
+					break;
+				case 'icon':
+					$i = 0;
+					$input_name = FOOGALLERY_META_SETTINGS . '[' . $id . ']';
+					$icon_html = '';
+					foreach ( $choices as $value => $icon ) {
+						$selected = ( $field_value == $value ) ? ' checked="checked"' : '';
+						$icon_html .= '<input style="display:none" name="' . $input_name. '" id="FooGallerySettings_' . $id . $i . '" ' . $selected . ' type="radio" value="' . $value . '" tabindex="' . $i . '"/>';
+						$title = $icon['label'];
+						$img = $icon['img'];
+						$icon_html .= '<label for="FooGallerySettings_' . $id . $i . '" title="' . $title . '"><img src="' . $img . '" /></label>';
+						$i++;
+					}
+					echo $icon_html;
+					break;
+
+				default:
+					do_action( 'foogallery_render_gallery_template_field_custom', $field, $gallery );
+					break;
+			}
+
+			echo '</div>';
+			if ( isset( $desc ) ) {
+				echo '<small>' . $desc . '</small>';
+			}
+
+
+			//allow for more customization
+			do_action( 'foogallery_render_gallery_template_field_after', $field, $gallery );
+		}
+
+		function alter_gallery_template_field( $field, $gallery ) {
+			if ( $field ) {
+				switch ($field['type']) {
+					case 'thumb_size':
+						global $_wp_additional_image_sizes;
+						$field['type'] = 'select';
+						$field['choices'] = $this->get_thumb_size_choices();
+						break;
+					case 'thumb_link':
+						$field['type'] = 'radio';
+						$field['choices'] = $this->get_thumb_link_field_choices();
+						break;
+					case 'lightbox':
+						$lightboxes = $this->get_lightbox_field_choices();
+						if ( 0 === count( $lightboxes ) ) {
+							$field['type'] = 'html';
+							$field['desc'] = '<strong>' . __('You have no lightbox extensions activated!', 'foogallery' ) . '</strong><br />';
+							$api = new FooGallery_Extensions_API();
+							if ( $api->is_downloaded( false, FOOGALLERY_FOOBOX_FREE_EXTENSION_SLUG ) ) {
+								//just need to activate it
+								$foobox_install_link = foogallery_build_admin_menu_url( array(
+									'page' => 'foogallery-extensions',
+									'extension' => FOOGALLERY_FOOBOX_FREE_EXTENSION_SLUG,
+									'action' => 'activate'
+								));
+								$field['desc'] .= '<a target="_blank" href="' . $foobox_install_link . '">' . __('Activate FooBox FREE right now!', 'foogallery') . '</a>';
+							} else {
+								//we need to download it
+								$foobox_install_link = foogallery_build_admin_menu_url( array(
+									'page' => 'foogallery-extensions',
+									'extension' => FOOGALLERY_FOOBOX_FREE_EXTENSION_SLUG,
+									'action' => 'download'
+								));
+								$foobox_install_html = '<a target="_blank" href="' . $foobox_install_link . '">' . __('Download and activate FooBox FREE', 'foogallery') . '</a>';
+								$field['desc'] .= sprintf ( __('%s which works flawlessly with FooGallery.', 'foogallery'), $foobox_install_html );
+							}
+						} else {
+							$field['type'] = 'select';
+							$field['choices'] = $lightboxes;
+						}
+						break;
+				}
+			}
+			return $field;
+		}
+
+		function get_thumb_size_choices() {
+			global $_wp_additional_image_sizes;
+			$sizes = array();
+			foreach( get_intermediate_image_sizes() as $s ){
+				$sizes[ $s ] = array( 0, 0 );
+				if( in_array( $s, array( 'thumbnail', 'medium', 'large' ) ) ){
+					$sizes[ $s ] = $s . ' (' . get_option( $s . '_size_w' ) . 'x' . get_option( $s . '_size_h' ) . ')';
+				}else{
+					if( isset( $_wp_additional_image_sizes ) && isset( $_wp_additional_image_sizes[ $s ] ) )
+						$sizes[ $s ] = $s . ' (' . $_wp_additional_image_sizes[ $s ]['width'] . 'x' . $_wp_additional_image_sizes[ $s ]['height'] . ')';
+				}
+			}
+			return $sizes;
+		}
+
+		function get_thumb_link_field_choices() {
+			return apply_filters('foogallery_gallery_template_field_thumb_links', array(
+				'image' => __('Full Size Image', 'foogallery'),
+				'page' => __('Image Attachment Page', 'foogallery')
+			) );
+		}
+
+		function get_lightbox_field_choices() {
+			return apply_filters( 'foogallery_gallery_template_field_lightboxes', array() );
+		}
+	}
+}
---
+++ includes/admin/class-gallery-metaboxes.php
@@ -0,0 +1,277 @@
+<?php
+
+/*
+ * FooGallery Admin Gallery MetaBoxes class
+ */
+
+if ( !class_exists( 'FooGallery_Admin_Gallery_MetaBoxes' ) ) {
+
+	class FooGallery_Admin_Gallery_MetaBoxes {
+
+		private $_gallery;
+
+		function __construct() {
+			add_action( 'add_meta_boxes', array($this, 'add_meta_boxes_to_gallery') );
+
+			//save extra post data for a gallery
+			add_action( 'save_post', array($this, 'save_gallery') );
+
+			//whitelist metaboxes for our gallery postype
+			add_filter( 'foogallery_metabox_sanity', array($this, 'whitelist_metaboxes') );
+
+			add_action( 'admin_enqueue_scripts', array( $this, 'include_required_scripts' ) );
+		}
+
+		function whitelist_metaboxes() {
+			return array(
+				FOOGALLERY_CPT_GALLERY => array(
+					'whitelist'  => array('submitdiv', 'slugdiv', 'postimagediv', 'foogallery_items', 'foogallery_settings', 'foogallery_help'),
+					'contexts'   => array('normal', 'advanced', 'side'),
+					'priorities' => array('high', 'core', 'default', 'low')
+				)
+			);
+		}
+
+		function add_meta_boxes_to_gallery() {
+			add_meta_box(
+				'foogallery_items',
+				__( 'Gallery Items', 'foogallery' ),
+				array($this, 'render_gallery_media_metabox'),
+				FOOGALLERY_CPT_GALLERY,
+				'normal',
+				'high'
+			);
+
+			add_meta_box(
+				'foogallery_settings',
+				__( 'Gallery Settings', 'foogallery' ),
+				array($this, 'render_gallery_settings_metabox'),
+				FOOGALLERY_CPT_GALLERY,
+				'normal',
+				'high'
+			);
+
+			add_meta_box(
+				'foogallery_help',
+				__( 'Gallery Shortcode', 'foogallery' ),
+				array($this, 'render_gallery_shortcode_metabox'),
+				FOOGALLERY_CPT_GALLERY,
+				'side',
+				'high'
+			);
+		}
+
+		function get_gallery($post) {
+			if ( !isset($this->_gallery) ) {
+				$this->_gallery = FooGallery::get( $post );
+			}
+
+			return $this->_gallery;
+		}
+
+		function save_gallery($post_id) {
+			// check autosave
+			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+				return $post_id;
+			}
+
+			// verify nonce
+			if ( array_key_exists( FOOGALLERY_CPT_GALLERY . '_nonce', $_POST ) &&
+				wp_verify_nonce( $_POST[FOOGALLERY_CPT_GALLERY . '_nonce'], plugin_basename( FOOGALLERY_FILE ) )
+			) {
+				//if we get here, we are dealing with the Gallery custom post type
+
+				//get previous attachments
+				//compare previous to current
+				//remove link to gallery from all attachments that have been removed
+				//add link to gallery for all attachments that have been added
+				//do nothing to all attachments that have stayed the same
+				//do this all in the gallery class
+
+				$attachments = apply_filters( 'foogallery_save_gallery_attachments', $_POST[FOOGALLERY_META_ATTACHMENTS] );
+				update_post_meta( $post_id, FOOGALLERY_META_ATTACHMENTS, $attachments );
+
+				$settings = isset($_POST[FOOGALLERY_META_SETTINGS]) ?
+					$_POST[FOOGALLERY_META_SETTINGS] : array();
+
+				$settings = apply_filters( 'foogallery_save_gallery_settings', $settings );
+
+				update_post_meta( $post_id, FOOGALLERY_META_TEMPLATE, $_POST[FOOGALLERY_META_TEMPLATE] );
+
+				update_post_meta( $post_id, FOOGALLERY_META_SETTINGS, $settings );
+
+				do_action( 'foogallery_after_save_gallery', $post_id, $_POST );
+			}
+		}
+
+		function render_gallery_media_metabox($post) {
+			$gallery = $this->get_gallery( $post );
+
+			wp_enqueue_media();
+
+			?>
+			<input type="hidden" name="<?php echo FOOGALLERY_CPT_GALLERY; ?>_nonce"
+				   id="<?php echo FOOGALLERY_CPT_GALLERY; ?>_nonce"
+				   value="<?php echo wp_create_nonce( plugin_basename( FOOGALLERY_FILE ) ); ?>"/>
+			<input type="hidden" name='foogallery_attachments' id="foogallery_attachments"
+				   value="<?php echo $gallery->attachments_meta; ?>"/>
+			<div>
+				<ul class="foogallery-attachments-list">
+					<?php
+					if ( $gallery->has_attachments() ) {
+						foreach ( $gallery->attachments() as $attachment_id ) {
+							$attachment = wp_get_attachment_image_src( $attachment_id );
+							$this->render_gallery_item( $attachment_id, $attachment );
+						}
+					} ?>
+					<li class="add-attachment">
+						<a href="#" data-uploader-title="<?php _e( 'Add Media To Gallery', 'foogallery' ); ?>"
+						   data-uploader-button-text="<?php _e( 'Add Media', 'foogallery' ); ?>"
+						   data-post-id="<?php echo $post->ID; ?>" class="upload_image_button"
+						   title="<?php _e( 'Add Media To Gallery', 'foogallery' ); ?>">
+							<div class="dashicons dashicons-format-gallery"></div>
+							<span><?php _e( 'Add Media', 'foogallery' ); ?></span>
+						</a>
+					</li>
+				</ul>
+				<div style="clear: both;"></div>
+			</div>
+			<textarea style="display: none" id="foogallery-attachment-template">
+				<?php $this->render_gallery_item(); ?>
+			</textarea>
+		<?php
+
+		}
+
+		function render_gallery_item($attachment_id = '', $attachment = array()) {
+			$data_attribute = empty($attachment_id) ? '' : "data-attachment-id=\"{$attachment_id}\"";
+			$img_tag        = empty($attachment) ? '<img />' : "<img width=\"{$attachment[1]}\" height=\"{$attachment[2]}\" src=\"{$attachment[0]}\" />";
+			?>
+			<li class="attachment details" <?php echo $data_attribute; ?>>
+				<div class="attachment-preview type-image">
+					<div class="thumbnail">
+						<div class="centered">
+							<?php echo $img_tag; ?>
+						</div>
+					</div>
+					<a class="info" href="#" title="<?php _e( 'Edit Info', 'foogallery' ); ?>">
+						<span class="dashicons dashicons-info"></span>
+					</a>
+					<a class="remove" href="#" title="<?php _e( 'Remove from gallery', 'foogallery' ); ?>">
+						<span class="dashicons dashicons-dismiss"></span>
+					</a>
+				</div>
+				<!--				<input type="text" value="" class="describe" data-setting="caption" placeholder="Caption this image…" />-->
+			</li>
+		<?php
+		}
+
+		function render_gallery_settings_metabox($post) {
+			//gallery settings including:
+			//gallery images link to image or attachment page
+			//default template to use
+			$gallery             = $this->get_gallery( $post );
+			$available_templates = foogallery_gallery_templates();
+			$gallery_template    = foogallery_default_gallery_template();
+			if ( !empty($gallery->gallery_template) ) {
+				$gallery_template = $gallery->gallery_template;
+			}
+			?>
+			<table class="foogallery-metabox-settings">
+				<tbody>
+				<tr class="gallery_template_field gallery_template_field_selector">
+					<th>
+						<label for="FooGallerySettings_GalleryTemplate"><?php _e('Gallery Template', 'foogallery'); ?></label>
+					</th>
+					<td>
+						<select id="FooGallerySettings_GalleryTemplate" name="<?php echo FOOGALLERY_META_TEMPLATE; ?>">
+							<?php
+							foreach ( $available_templates as $template ) {
+								$selected = ($gallery_template === $template['slug']) ? 'selected' : '';
+								$preview_css = isset( $template['preview_css'] ) ? ' data-preview-css="' . $template['preview_css'] . '" ' : '';
+								echo "<option {$selected}{$preview_css} value=\"{$template['slug']}\">{$template['name']}</option>";
+							}
+							?>
+						</select>
+						<br />
+						<small><?php _e( 'The gallery template that will be used when the gallery is output to the frontend.', 'foogallery' ); ?></small>
+					</td>
+				</tr>
+				<?php
+				foreach ( $available_templates as $template ) {
+					$field_visibility = ($gallery_template !== $template['slug']) ? 'style="display:none"' : '';
+					$section          = '';
+					foreach ( $template['fields'] as $field ) {
+						if ( isset($field['section']) && $field['section'] !== $section ) {
+							$section = $field['section'];
+							?>
+							<tr class="gallery_template_field gallery_template_field-<?php echo $template['slug']; ?>" <?php echo $field_visibility; ?>>
+								<td colspan="2"><h4><?php echo $section; ?></h4></td>
+							</tr>
+						<?php
+						}
+						?>
+					<tr class="gallery_template_field gallery_template_field-<?php echo $template['slug']; ?>" <?php echo $field_visibility; ?>>
+						<th>
+							<label
+								for="FooGallerySettings_<?php echo $template['slug'] . '_' . $field['id']; ?>"><?php echo $field['title']; ?></label>
+						</th>
+						<td>
+							<?php do_action('foogallery_render_gallery_template_field', $field, $gallery, $template ); ?>
+						</td>
+						</tr><?php
+					}
+				}
+				?>
+				</tbody>
+			</table>
+		<?php
+		}
+
+		function render_gallery_shortcode_metabox($post) {
+			$gallery = $this->get_gallery( $post );
+			$shortcode = $gallery->shortcode();
+			if ( $gallery->is_published() ) {
+				?>
+				<p>
+					<code id="foogallery-copy-shortcode" data-clipboard-text="<?php echo htmlspecialchars( $shortcode ); ?>"
+						  title="<?php _e('Click to copy to your clipboard', 'foogallery'); ?>"
+						  class="foogallery-shortcode"><?php echo $shortcode; ?></code>
+				</p>
+				<p>
+					<?php _e( 'Paste the above shortcode into a post or page to show the gallery. Simply click the shortcode to copy it to your clipboard.', 'foogallery' ); ?>
+				</p>
+				<script>
+					jQuery(function($) {
+						var $el = $('#foogallery-copy-shortcode');
+						ZeroClipboard.config({ moviePath: "<?php echo FOOGALLERY_URL; ?>lib/zeroclipboard/ZeroClipboard.swf" });
+						var client = new ZeroClipboard($el);
+
+						client.on( "load", function(client) {
+							client.on( "complete", function(client, args) {
+								$('.foogallery-shortcode-message').remove();
+								$el.after('<p class="foogallery-shortcode-message"><?php _e( 'Shortcode copied to clipboard :)','foogallery' ); ?></p>');
+							} );
+						} );
+					});
+				</script>
+			<?php
+			}
+		}
+
+		function include_required_scripts() {
+
+			//zeroclipboard needed for copy to clipboard functionality
+			$url = FOOGALLERY_URL . 'lib/zeroclipboard/ZeroClipboard.min.js';
+			wp_enqueue_script( 'foogallery-zeroclipboard', $url, array('jquery'), FOOGALLERY_VERSION );
+
+			//include any admin js required for the templates
+			foreach ( foogallery_gallery_templates() as $template ) {
+				$admin_js = foo_safe_get( $template, 'admin_js' );
+				if ( $admin_js ) {
+					wp_enqueue_script( 'foogallery-gallery-admin-' . $template['slug'], $admin_js, array('jquery'), FOOGALLERY_VERSION );
+				}
+			}
+		}
+	}
+}
