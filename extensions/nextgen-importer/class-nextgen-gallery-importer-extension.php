d2309f17979a5c6543e5fdb9d7bfe8f1bd739fcc
Brad Vincent <bradvin@gmail.com>
2014-06-03 19:39:46 +0200

    nextgen extenstion

--- extensions/nextgen-importer/class-nextgen-gallery-importer-extension.php
+++ extensions/nextgen-importer/class-nextgen-gallery-importer-extension.php
@@ -4,9 +4,37 @@
  */
 if (!class_exists('FooGallery_Nextgen_Gallery_Importer_Extension')) {
 
-    class FooGallery_Nextgen_Gallery_Importer_Extension {
-        function __construct() {
+    require_once 'class-nextgen-helper.php';
 
+    class FooGallery_Nextgen_Gallery_Importer_Extension {
+
+        private $nextgen;
+
+        function __construct() {
+          $nextgen = new FooGallery_NextGen_Helper();
+
+
+
+          //only do anything if NextGen is installed
+          if ( $nextgen->is_nextgen_installed() ) {
+            //hook into the foogallery menu
+            add_action( 'foogallery_admin_menu_after', array( $this, 'add_menu' ) );
+            add_action( 'foogallery_extension_activated-nextgen',  array( $this, 'add_menu' ) );
+          }
+        }
+
+        function add_menu() {
+          $parent_slug = foogallery_admin_menu_parent_slug();
+          add_submenu_page( $parent_slug,
+            __('NextGen Gallery Importer', 'foogallery'),
+            __('NextGen Importer', 'foogallery'),
+            'manage_options',
+            'foogallery-nextgen-importer',
+            array($this, 'render_view') );
+        }
+
+        function render_view() {
+          require_once 'view-importer.php';
         }
     }
 }
