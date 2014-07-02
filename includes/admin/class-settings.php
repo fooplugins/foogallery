4912cb08f61155d1a9ae21dab434587946365b72
Brad Vincent <bradvin@gmail.com>
2014-07-02 23:25:00 +0200

    cleaned up settings

--- includes/admin/class-settings.php
+++ includes/admin/class-settings.php
@@ -2,7 +2,10 @@
 
 if (!class_exists('FooGallery_Admin_Settings')) {
 
-    class FooGallery_Admin_Settings {
+	/**
+	 * Class FooGallery_Admin_Settings
+	 */
+	class FooGallery_Admin_Settings {
 
 		function __construct() {
 			add_filter( 'foogallery-admin_settings', array($this, 'create_settings'), 10, 2 );
@@ -16,7 +19,7 @@ if (!class_exists('FooGallery_Admin_Settings')) {
 	        $gallery_templates = foogallery_gallery_templates();
 	        $gallery_templates_choices = array();
 	        foreach($gallery_templates as $template) {
-		        $gallery_templates_choices[$template['slug']] = $template['slug'];
+		        $gallery_templates_choices[$template['slug']] = $template['name'];
 	        }
 
 	        $settings[] = array(
@@ -31,29 +34,29 @@ if (!class_exists('FooGallery_Admin_Settings')) {
 
 	        //endregion General
 
-	        //region Advanced Tab
-	        $tabs['advanced'] = __('Advanced', 'foogallery');
-
-	        $example_url = '<br /><code>' . trailingslashit( site_url() ) . foogallery_permalink() . '/my-cool-gallery</code>';
-
-	        $settings[] = array(
-		        'id'      => 'gallery_permalinks_enabled',
-		        'title'   => __('Enable Friendly URL\'s', 'foogallery'),
-		        'desc'    => sprintf( __('If enabled, you will be able to access your galleries from friendly URL\'s, e.g. %s', 'foogallery'), $example_url ),
-		        'default' => foogallery_get_default( 'gallery_permalinks_enabled' ),
-		        'type'    => 'checkbox',
-		        'tab'     => 'advanced'
-	        );
-
-	        $settings[] = array(
-		        'id'      => 'gallery_permalink',
-		        'title'   => __('Gallery Permalink', 'foogallery'),
-		        'desc'    => __('The part used in building up friendly URL\'s', 'foogallery'),
-		        'default' => foogallery_get_default( 'gallery_permalink' ),
-		        'type'    => 'text',
-		        'tab'     => 'advanced'
-	        );
-	        //endregion Advanced
+//	        //region Advanced Tab
+//	        $tabs['advanced'] = __('Advanced', 'foogallery');
+//
+//	        $example_url = '<code>' . trailingslashit( site_url() ) . foogallery_permalink() . '/my-cool-gallery</code>';
+//
+//	        $settings[] = array(
+//		        'id'      => 'gallery_permalinks_enabled',
+//		        'title'   => __('Enable Friendly URL\'s', 'foogallery'),
+//		        'desc'    => sprintf( __('If enabled, you will be able to access your galleries from a friendly URL e.g. %s', 'foogallery'), $example_url ),
+//		        'default' => foogallery_get_default( 'gallery_permalinks_enabled' ),
+//		        'type'    => 'checkbox',
+//		        'tab'     => 'advanced'
+//	        );
+//
+//	        $settings[] = array(
+//		        'id'      => 'gallery_permalink',
+//		        'title'   => __('Gallery Permalink', 'foogallery'),
+//		        'desc'    => __('If friendly URL\'s are enabled, this is used in building up a friendly URL', 'foogallery'),
+//		        'default' => foogallery_get_default( 'gallery_permalink' ),
+//		        'type'    => 'text',
+//		        'tab'     => 'advanced'
+//	        );
+//	        //endregion Advanced
 
 			return array(
 				'tabs' => $tabs,
