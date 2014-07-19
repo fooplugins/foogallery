584b80932d08523312185d8d40b0c8f1fa95f4ce
Brad Vincent <bradvin@gmail.com>
2014-07-19 12:09:51 +0200

    Get styles working with no anchor. Fixes #26

--- extensions/default-templates/gallery-default.php
+++ extensions/default-templates/gallery-default.php
@@ -5,7 +5,8 @@
 global $current_foogallery;
 global $current_foogallery_arguments;
 $args = foogallery_gallery_template_setting( 'thumbnail_dimensions', array() );
-$args['link'] = foogallery_gallery_template_setting( 'thumbnail_link', 'image' );
+$link = foogallery_gallery_template_setting( 'thumbnail_link', 'image' );
+$args['link'] = $link;
 
 $lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
 $spacing = foogallery_gallery_template_setting( 'spacing', '' );
@@ -13,7 +14,7 @@ $hover_effect = foogallery_gallery_template_setting( 'hover-effect', 'hover-effe
 $border_style = foogallery_gallery_template_setting( 'border-style', 'border-style-square-white' );
 $alignment = foogallery_gallery_template_setting( 'alignment', 'alignment-center' );
 ?>
-<div class="<?php echo foogallery_build_class_attribute( $current_foogallery, 'foogallery-lightbox-' . $lightbox, $spacing, $hover_effect, $border_style, $alignment); ?>">
+<div class="<?php echo foogallery_build_class_attribute( $current_foogallery, 'foogallery-link-' . $link, 'foogallery-lightbox-' . $lightbox, $spacing, $hover_effect, $border_style, $alignment); ?>">
 	<?php foreach ( $current_foogallery->attachments() as $attachment ) {
 		echo $attachment->html( $args );
 	} ?>
