db5709d4c0dea7d5d4131de7a782821f5c6003d2
Brad Vincent <bradvin@gmail.com>
2014-07-20 10:40:59 +0200

    reordering of metaboxes

--- includes/admin/class-gallery-metaboxes.php
+++ includes/admin/class-gallery-metaboxes.php
@@ -67,7 +67,7 @@ if ( !class_exists( 'FooGallery_Admin_Gallery_MetaBoxes' ) ) {
 				array($this, 'render_gallery_shortcode_metabox'),
 				FOOGALLERY_CPT_GALLERY,
 				'side',
-				'high'
+				'default'
 			);
 
 			if ( 'publish' == $post->post_status ) {
@@ -76,7 +76,7 @@ if ( !class_exists( 'FooGallery_Admin_Gallery_MetaBoxes' ) ) {
 					array($this, 'render_gallery_usage_metabox'),
 					FOOGALLERY_CPT_GALLERY,
 					'side',
-					'high'
+					'default'
 				);
 			}
 		}
