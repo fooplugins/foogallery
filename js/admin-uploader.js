09b6bc838afe4966fb967782c33de8a92ecbc082
Brad Vincent <bradvin@gmail.com>
2014-07-26 08:14:42 +0200

    fixed jshint issues

--- js/admin-uploader.js
+++ js/admin-uploader.js
@@ -1,14 +1,13 @@
 jQuery(document).ready(function() {
     jQuery('.image-upload-button').click(function(e) {
         e.preventDefault();
-        targetfield = jQuery(this).prev('.image-upload-url');
         tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
         return false;
     });
-    
+
     window.send_to_editor = function(html) {
-        imgurl = jQuery('img',html).attr('src');
-        jQuery(targetfield).val(imgurl);
+        var img = jQuery('img',html).attr('src');
+        jQuery(targetfield).val(img);
         tb_remove();
-    }
+    };
 });
