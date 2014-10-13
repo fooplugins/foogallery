1be67f1b1fd40357071bc2c19b4a261ffd9d0df0
Brad Vincent <bradvin@gmail.com>
2014-10-13 23:39:09 +0200

    cleanup

--- extensions/albums/public/class-rewrite-rules.php
+++ extensions/albums/public/class-rewrite-rules.php
@@ -8,16 +8,10 @@ if (!class_exists('FooGallery_Album_Rewrite_Rules')) {
 
         function __construct() {
 	        add_action( 'init',  array( $this, 'add_gallery_endpoint' ) );
-	        //add_action( 'template_redirect', array( $this, 'template_redirect' ) );
         }
 
 	    function add_gallery_endpoint() {
 		    add_rewrite_endpoint( 'gallery', EP_ALL );
 	    }
-
-//	    function template_redirect() {
-//		    global $foogallery_album_gallery;
-//		    $foogallery_album_gallery = get_query_var( 'gallery' );
-//	    }
     }
 }
