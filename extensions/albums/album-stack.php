afedd42b9e809aee3aa6d5153178e141cc9d7234
Brad Vincent <bradvin@gmail.com>
2015-01-20 16:02:15 +0200

    album stack fix to get foobox working as expected

--- extensions/albums/album-stack.php
+++ extensions/albums/album-stack.php
@@ -11,7 +11,7 @@ $gutter = foogallery_album_template_setting( 'gutter', '40' );
 $delay = foogallery_album_template_setting( 'delay', '0' );
 $pile_angles = foogallery_album_template_setting( 'pile_angles', '2' );
 ?>
-<div id="foogallery-album-<?php echo $current_foogallery_album->ID; ?>" class="foogallery-container foogallery-stack-album foogallery-lightbox-<?php echo $lightbox; ?>">
+<div id="foogallery-album-<?php echo $current_foogallery_album->ID; ?>" class="foogallery-container foogallery-stack-album">
 	<div class="topbar">
 		<span id="foogallery-stack-album-back-<?php echo $current_foogallery_album->ID; ?>" class="back">&larr;</span>
 		<h2><?php echo $current_foogallery_album->name; ?></h2><h3 id="foogallery-stack-album-gallery-<?php echo $current_foogallery_album->ID; ?>"></h3>
@@ -22,7 +22,7 @@ $pile_angles = foogallery_album_template_setting( 'pile_angles', '2' );
 			foreach ( $gallery->attachments() as $attachment ) {
 				echo '<li data-pile="'. esc_attr($gallery->name) . '">';
 				$args['link_attributes']['rel'] = 'gallery[' . $gallery->ID . ']';
-				$args['link_attributes']['class'] = $lightbox;
+				$args['link_attributes']['class'] = apply_filters( 'foogallery_album_stack_link_class_name', $lightbox );
 				echo $attachment->html( $args, false, false );
 				if ( $attachment->caption ) {
 					echo '<span class="tp-info"><span>' . $attachment->caption . '</span></span>';
