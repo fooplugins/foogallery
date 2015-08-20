2ec690bcdeab1b5e6e866bb46e6a31645efec582
Brad Vincent <bradvin@gmail.com>
2015-08-20 22:04:51 +0200

    version bump

--- foogallery.php
+++ foogallery.php
@@ -14,7 +14,7 @@
  * Plugin Name: FooGallery
  * Plugin URI:  https://github.com/fooplugins/foogallery
  * Description: Better Image Galleries for WordPress
- * Version:     1.2.5
+ * Version:     1.2.6
  * Author:      FooPlugins
  * Author URI:  http://fooplugins.com
  * Text Domain: foogallery
@@ -32,7 +32,7 @@ define( 'FOOGALLERY_SLUG', 'foogallery' );
 define( 'FOOGALLERY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FOOGALLERY_URL', plugin_dir_url( __FILE__ ) );
 define( 'FOOGALLERY_FILE', __FILE__ );
-define( 'FOOGALLERY_VERSION', '1.2.5' );
+define( 'FOOGALLERY_VERSION', '1.2.6' );
 
 /**
  * FooGallery_Plugin class
