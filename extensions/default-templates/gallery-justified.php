7ea81370f198d14f475fb91f3cf3e07430906b0a
Brad Vincent <bradvin@gmail.com>
2015-09-24 07:07:18 +0200

    added 2 new settings to justified gallery - Caption Source and MaxRowHeight

--- extensions/default-templates/gallery-justified.php
+++ extensions/default-templates/gallery-justified.php
@@ -5,6 +5,10 @@
 global $current_foogallery;
 global $current_foogallery_arguments;
 $height = foogallery_gallery_template_setting( 'row_height', '150' );
+$max_row_height = foogallery_gallery_template_setting( 'max_row_height', '200%' );
+if ( strpos( $max_row_height, '%' ) !== false ) {
+	$max_row_height = '"' . $max_row_height . '"';
+}
 $margins = foogallery_gallery_template_setting( 'margins', '1' );
 $captions = foogallery_gallery_template_setting( 'captions', '' ) == 'on';
 $gutter_width = foogallery_gallery_template_setting( 'gutter_width', '10' );
@@ -13,10 +17,13 @@ $args = array(
 	'link' => foogallery_gallery_template_setting( 'thumbnail_link', 'image' )
 );
 $lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
+$caption_source = foogallery_gallery_template_setting( 'caption_source', 'title' );
 ?>
-<div data-justified-options='{ "rowHeight": <?php echo $height; ?>, "margins": <?php echo $margins; ?>, "captions": <?php echo $captions ? 'true' : 'false'; ?> }' id="foogallery-gallery-<?php echo $current_foogallery->ID; ?>" class="<?php echo foogallery_build_class_attribute( $current_foogallery, 'foogallery-lightbox-' . $lightbox, 'foogallery-justified-loading' ); ?>">
+<div data-justified-options='{ "rowHeight": <?php echo $height; ?>, "maxRowHeight": <?php echo $max_row_height; ?>, "margins": <?php echo $margins; ?>, "captions": <?php echo $captions ? 'true' : 'false'; ?> }' id="foogallery-gallery-<?php echo $current_foogallery->ID; ?>" class="<?php echo foogallery_build_class_attribute( $current_foogallery, 'foogallery-lightbox-' . $lightbox, 'foogallery-justified-loading' ); ?>">
 	<?php foreach ( $current_foogallery->attachments() as $attachment ) {
-		if ( empty( $attachment->alt ) ) {
+		if ( 'title' == $caption_source ) {
+			$attachment->alt = $attachment->title;
+		} else if ( 'caption' == $caption_source ) {
 			$attachment->alt = $attachment->caption;
 		}
 		echo $attachment->html( $args );
