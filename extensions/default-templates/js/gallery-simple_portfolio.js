4f4537085e8f41bc57d2f6b8ad29679fcf3712e0
Brad Vincent <bradvin@gmail.com>
2015-10-27 20:40:29 +0200

    allow for zero gutter if needed

--- extensions/default-templates/js/gallery-simple_portfolio.js
+++ extensions/default-templates/js/gallery-simple_portfolio.js
@@ -19,7 +19,8 @@ function FooGallery_Simple_Portfolio_Ready(func) {
 
 FooGallery_Simple_Portfolio_Ready(function () {
 	jQuery('.foogallery-simple_portfolio').each(function() {
-		var gutterValue = jQuery(this).data('brickfolio-gutter') || 40;
+		var gutterValue = jQuery(this).data('brickfolio-gutter');
+		if (isNaN(gutterValue)) gutterValue = 40;
 		jQuery(this).brickfolio( { imageSelector : "img.bf-img:first", gutter : gutterValue} );
 	});
 });
