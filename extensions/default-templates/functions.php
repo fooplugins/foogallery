26f2eda4244faa6e4bb96e68a11af3200a555f76
Brad Vincent <bradvin@gmail.com>
2016-08-23 20:32:07 +0200

    use WP 4.6's images loaded script

--- extensions/default-templates/functions.php
+++ extensions/default-templates/functions.php
@@ -7,6 +7,15 @@
  * Enqueue the imagesLoaded script file
  */
 function foogallery_enqueue_imagesloaded_script() {
-	$js = FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'js/imagesloaded.pkgd.min.js';
-	wp_enqueue_script( 'foogallery-imagesloaded', $js, array(), FOOGALLERY_VERSION );
+    global $wp_version;
+    if ( version_compare( $wp_version, '4.6' ) >= 0 ) {
+
+        wp_enqueue_script('imagesloaded');
+
+    } else {
+
+        //include our own version of imagesLoaded for <4.6
+        $js = FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'js/imagesloaded.pkgd.min.js';
+        wp_enqueue_script( 'foogallery-imagesloaded', $js, array(), FOOGALLERY_VERSION );
+    }
 }
