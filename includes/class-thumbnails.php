799b65b7c2b86b846d8ffdf6bc31a5c3bd7b38d3
Brad Vincent <bradvin@gmail.com>
2016-10-29 11:35:30 +0200

    introduce force_use_original_thumb into image resizer

--- includes/class-thumbnails.php
+++ includes/class-thumbnails.php
@@ -26,11 +26,23 @@ if ( !class_exists( 'FooGallery_Thumbnails' ) ) {
 
 			$args = wp_parse_args( $args, $arg_defaults );
 
-			if ( 'on' === foogallery_get_setting( 'use_original_thumbs' ) ) {
-				$width  = (int)$args['width'];
-				$height = (int)$args['height'];
-				$crop   = (bool)$args['crop'];
+			$width  = (int)$args['width'];
+			$height = (int)$args['height'];
+			$crop   = (bool)$args['crop'];
 
+			//we can force the use of the original WP icon by passing through args individually
+			$force_use_original_thumb = isset( $args['force_use_original_thumb'] ) && true === $args['force_use_original_thumb'];
+
+			if ( $force_use_original_thumb ) {
+				$thumbnail_icon = wp_get_attachment_image_src( $thumbnail_object->ID, array( $width, $height ) );
+
+				return $thumbnail_icon[0];
+			}
+
+			//we can force the use of original WP thumbs by passing through args individually, or by saved settings
+			$use_original_thumbs = ( isset( $args['use_original_thumbs'] ) && true === $args['use_original_thumbs'] ) || 'on' === foogallery_get_setting( 'use_original_thumbs' );
+
+			if ( $use_original_thumbs ) {
 				//check if we are trying to get back the default thumbnail that we already have
 				if ( $thumbnail_object->ID > 0 && $width == get_option( 'thumbnail_size_w' ) && $height == get_option( 'thumbnail_size_h' ) && $crop == get_option( 'thumbnail_crop' ) ) {
 					$thumbnail_attributes = wp_get_attachment_image_src( $thumbnail_object->ID );
