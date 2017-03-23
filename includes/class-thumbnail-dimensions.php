e9379c59dcaed2b590a8a8eeba4d579e7e8cabb7
Brad Vincent <bradvin@gmail.com>
2017-03-23 20:19:02 +0200

    reworked thumb calculation

--- includes/class-thumbnail-dimensions.php
+++ includes/class-thumbnail-dimensions.php
@@ -9,96 +9,58 @@ if ( ! class_exists( 'FooGallery_Thumbnail_Dimensions' ) ) {
 
 		function __construct() {
 			if ( is_admin() ) {
-				add_action( 'foogallery_before_save_gallery', array( $this, 'start_watching_for_thumbnail_generation_calls' ), 10, 2 );
-				add_action( 'foogallery_after_save_gallery', array( $this, 'stop_watching_for_thumbnail_generation_calls' ), 99, 2 );
+				add_action( 'foogallery_after_save_gallery', array( $this, 'calculate_thumbnail_dimensions' ), 9, 2 );
 			}
+
+			add_filter( 'foogallery_attachment_load', array( $this, 'load_thumbnail_dimensions' ), 10, 2 );
 		}
 
 		/**
-		 * Start watching for calls to the thumbnail generator so we can intercept and store info about the thumbs
-		 *
+		 * Calculate the exact thumb size for the gallery and save the meta data
 		 * @param $post_id
 		 * @param $form_post
 		 */
-		function start_watching_for_thumbnail_generation_calls( $post_id, $form_post ) {
-			global $foogallery_thumb_dimensions;
+		function calculate_thumbnail_dimensions( $post_id, $form_post ) {
+			$foogallery = FooGallery::get_by_id( $post_id );
 
-			//clear the array
-			if ( empty( $foogallery_thumb_dimensions ) ) {
-				$foogallery_thumb_dimensions[$post_id] = array();
+			$gallery_template = $foogallery->gallery_template;
+
+			$setting_key = "{$gallery_template}_thumbnail_dimensions";
+
+			$thumbnail_dimensions = apply_filters( 'foogallery_template_thumbnail_dimensions-' . $gallery_template, $foogallery->get_meta( $setting_key, false ), $foogallery );
+
+			foreach ( $foogallery->attachments() as $attachment ) {
+				//$thumbnail_dimensions
+				$thumb_width = (int) $thumbnail_dimensions['width'];
+				$thumb_height = (int) $thumbnail_dimensions['height'];
+				$thumb_crop = (bool) $thumbnail_dimensions['crop'];
+
+				$size_array = image_resize_dimensions( $attachment->width, $attachment->height, $thumb_width, $thumb_height, $thumb_crop );
+
+				$size = array(
+					'width'  => $size_array[4],
+					'height' => $size_array[5]
+				);
+
+				$existing_size = get_post_meta( $attachment->ID, FOOGALLERY_META_THUMB_DIMENSIONS, true );
+				$existing_size[$foogallery->ID] = $size;
+
+				//save the post meta against the attachment
+				update_post_meta( $attachment->ID, FOOGALLERY_META_THUMB_DIMENSIONS, $existing_size );
 			}
-
-			//and hook up actions
-			add_filter( 'wpthumb_image_post', array( $this, 'intercept_thumb_generation' ), 10, 2 );
-			add_filter( 'foogallery_thumbnail_resize_args', array( $this, 'change_thumbnail_resize_args' ), 10, 3 );
 		}
 
-		/**
-		 * Stop watching for calls to the thumbnail generator
-		 *
-		 * @param $post_id
-		 * @param $form_post
-		 */
-		function stop_watching_for_thumbnail_generation_calls( $post_id, $form_post ) {
-			global $foogallery_thumb_dimensions;
+		function load_thumbnail_dimensions( $foogallery_attachment, $foogallery ) {
+			$size = get_post_meta( $foogallery_attachment->ID, FOOGALLERY_META_THUMB_DIMENSIONS, true );
+			if ( $size && array_key_exists( $foogallery->ID, $size ) ) {
+				$size = $size[$foogallery->ID];
 
-			//if we have nothing to store then get out
-			if ( empty( $foogallery_thumb_dimensions ) || !array_key_exists( $post_id, $foogallery_thumb_dimensions ) ) {
-				return;
+				$foogallery_attachment->foogallery_id = $foogallery->ID;
+				$foogallery_attachment->thumb_width = $size['width'];
+				$foogallery_attachment->thumb_height = $size['width'];
 			}
 
-			//check if we have data and store it against the foogallery
-			update_post_meta( $post_id, FOOGALLERY_META_THUMB_DIMENSIONS, $foogallery_thumb_dimensions[$post_id] );
-
-			//unhook the action
-			remove_filter( 'wpthumb_image_post', array( $this, 'intercept_thumb_generation' ), 10 );
-			remove_filter( 'foogallery_thumbnail_resize_args', array( $this, 'change_thumbnail_resize_args' ), 10 );
-		}
-
-		/**
-		 * Intercept calls to the thumb generation and store dimensions values
-		 *
-		 * @param $editor
-		 * @param $args
-		 */
-		function intercept_thumb_generation( $editor, $args ) {
-			global $foogallery_thumb_dimensions;
-
-			//if we have nothing stored then get out
-			if ( empty( $foogallery_thumb_dimensions ) ) {
-				return;
-			}
-
-			//if we are not generating a thumb for a foogallery then get out
-			if ( !array_key_exists( 'foogallery_id', $args ) ) {
-				return;
-			}
-
-			//if we are not generating a thumb for a foogallery attachment then get out
-			if ( !array_key_exists( 'foogallery_attachment_id', $args ) ) {
-				return;
-			}
-
-			$foogallery_id = $args['foogallery_id'];
-			$foogallery_attachment_id = $args['foogallery_attachment_id'];
-			$size = $editor->get_size();
-
-			$foogallery_thumb_dimensions[$foogallery_id][$foogallery_attachment_id] = $size;
-		}
-
-		/**
-		 * Change any arguments passed to the thumb generation class
-		 *
-		 * @param $args
-		 * @param $original_image_src
-		 * @param $thumbnail_object
-		 *
-		 * @return mixed
-		 */
-		function change_thumbnail_resize_args($args, $original_image_src, $thumbnail_object) {
-			//we only want to force the thumb to be generated even if done so before
-			$args['cache'] = false;
-			return $args;
+			return $foogallery_attachment;
 		}
 	}
 }
