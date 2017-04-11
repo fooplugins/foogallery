ec440366c209e878b333203d5677f540561f5360
Brad Vincent <bradvin@gmail.com>
2017-04-11 14:14:28 +0200

    updated default gallery template to use new function and assets

--- extensions/default-templates/default/class-default-gallery-template.php
+++ extensions/default-templates/default/class-default-gallery-template.php
@@ -10,6 +10,12 @@ if ( !class_exists( 'FooGallery_Default_Gallery_Template' ) ) {
 		 */
 		function __construct() {
 			add_filter( 'foogallery_gallery_templates', array( $this, 'add_template' ) );
+
+			//add extra fields to the templates
+			add_filter( 'foogallery_override_gallery_template_fields-default', array( $this, 'add_common_thumbnail_fields' ), 10, 2 );
+
+			add_action( 'foogallery_located_template-default', array( $this, 'enqueue_dependencies' ) );
+
 			add_filter( 'foogallery_gallery_templates_files', array( $this, 'register_myself' ) );
 			add_action( 'foogallery_render_gallery_template_field_custom', array( $this, 'render_thumbnail_preview' ), 10, 3 );
 			add_filter( 'foogallery_located_template-default', array( $this, 'enqueue_dependencies' ) );
@@ -35,165 +41,104 @@ if ( !class_exists( 'FooGallery_Default_Gallery_Template' ) ) {
 		 */
 		function add_template( $gallery_templates ) {
 			$gallery_templates[] = array(
-					'slug'        => 'default',
-					'name'        => __( 'Responsive Image Gallery', 'foogallery' ),
-					'preview_css' => FOOGALLERY_DEFAULT_GALLERY_TEMPLATE_URL . 'css/gallery-default.css',
-					'admin_js'	  => FOOGALLERY_DEFAULT_GALLERY_TEMPLATE_URL . 'js/admin-gallery-default.js',
-					'fields'	  => array(
-							array(
-									'id'      => 'lightbox',
-									'title'   => __( 'Lightbox', 'foogallery' ),
-									'desc'    => __( 'Choose which lightbox you want to use. The lightbox will only work if you set the thumbnail link to "Full Size Image".', 'foogallery' ),
-									'type'    => 'lightbox',
-							),
-							array(
-									'id'      => 'spacing',
-									'title'   => __( 'Spacing', 'foogallery' ),
-									'desc'    => __( 'The spacing or gap between thumbnails in the gallery.', 'foogallery' ),
-									'type'    => 'select',
-									'default' => 'spacing-width-10',
-									'choices' => array(
-											'spacing-width-0' => __( '0 pixels', 'foogallery' ),
-											'spacing-width-5' => __( '5 pixels', 'foogallery' ),
-											'spacing-width-10' => __( '10 pixels', 'foogallery' ),
-											'spacing-width-15' => __( '15 pixels', 'foogallery' ),
-											'spacing-width-20' => __( '20 pixels', 'foogallery' ),
-											'spacing-width-25' => __( '25 pixels', 'foogallery' ),
-									),
-							),
-							array(
-									'id'      => 'alignment',
-									'title'   => __( 'Alignment', 'foogallery' ),
-									'desc'    => __( 'The horizontal alignment of the thumbnails inside the gallery.', 'foogallery' ),
-									'default' => 'alignment-center',
-									'type'    => 'select',
-									'choices' => array(
-											'alignment-left' => __( 'Left', 'foogallery' ),
-											'alignment-center' => __( 'Center', 'foogallery' ),
-											'alignment-right' => __( 'Right', 'foogallery' ),
-									)
-							),
-							array(
-									'id'      => 'loading_animation',
-									'title'   => __( 'Loading Indicator', 'foogallery' ),
-									'default' => 'yes',
-									'type'    => 'radio',
-									'choices' => array(
-											'yes'  => __( 'Show Thumbnail Loading Indicator', 'foogallery' ),
-											'no'   => __( 'Disabled', 'foogallery' )
-									),
-									'spacer'  => '<span class="spacer"></span>',
-									'desc'	  => __( 'By default, an animated loading animation indicator is shown before the thumbnails have loaded. You can disable the loader if you want.', 'foogallery' ),
-							),
-							array(
-									'id'      => 'thumbnail_dimensions',
-									'title'   => __( 'Size', 'foogallery' ),
-									'desc'    => __( 'Choose the size of your thumbnails.', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'type'    => 'thumb_size',
-									'default' => array(
-											'width' => get_option( 'thumbnail_size_w' ),
-											'height' => get_option( 'thumbnail_size_h' ),
-											'crop' => true,
-									),
-							),
-							array(
-									'id'      => 'thumbnail_link',
-									'title'   => __( 'Link', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'default' => 'image',
-									'type'    => 'thumb_link',
-									'spacer'  => '<span class="spacer"></span>',
-									'desc'	  => __( 'You can choose to link each thumbnail to the full size image, the image\'s attachment page, a custom URL, or you can choose to not link to anything.', 'foogallery' ),
-							),
-							array(
-									'id'      => 'border-style',
-									'title'   => __( 'Border Style', 'foogallery' ),
-									'desc'    => __( 'The border style for each thumbnail in the gallery.', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'type'    => 'icon',
-									'default' => 'border-style-square-white',
-									'choices' => array(
-											'border-style-square-white' => array( 'label' => __( 'Square white border with shadow' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-square-white.png' ),
-											'border-style-circle-white' => array( 'label' => __( 'Circular white border with shadow' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-circle-white.png' ),
-											'border-style-square-black' => array( 'label' => __( 'Square Black' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-square-black.png' ),
-											'border-style-circle-black' => array( 'label' => __( 'Circular Black' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-circle-black.png' ),
-											'border-style-inset' => array( 'label' => __( 'Square Inset' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-square-inset.png' ),
-											'border-style-rounded' => array( 'label' => __( 'Plain Rounded' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-plain-rounded.png' ),
-											'' => array( 'label' => __( 'Plain' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/border-style-icon-none.png' ),
-									)
-							),
-							array(
-									'id'      => 'hover-effect-type',
-									'title'   => __( 'Hover Effect Type', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'default' => '',
-									'type'    => 'radio',
-									'choices' => apply_filters( 'foogallery_gallery_template_hover-effect-types', array(
-											''  => __( 'Icon', 'foogallery' ),
-											'hover-effect-tint'   => __( 'Dark Tint', 'foogallery' ),
-											'hover-effect-color' => __( 'Colorize', 'foogallery' ),
-											'hover-effect-caption' => __( 'Caption', 'foogallery' ),
-											'hover-effect-none' => __( 'None', 'foogallery' )
-									) ),
-									'spacer'  => '<span class="spacer"></span>',
-									'desc'	  => __( 'The type of hover effect the thumbnails will use.', 'foogallery' ),
-							),
-							array(
-									'id'      => 'hover-effect',
-									'title'   => __( 'Icon Hover Effect', 'foogallery' ),
-									'desc'    => __( 'When the hover effect type of Icon is chosen, you can choose which icon is shown when you hover over each thumbnail.', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'type'    => 'icon',
-									'default' => 'hover-effect-zoom',
-									'choices' => array(
-											'hover-effect-zoom' => array( 'label' => __( 'Zoom' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-zoom.png' ),
-											'hover-effect-zoom2' => array( 'label' => __( 'Zoom 2' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-zoom2.png' ),
-											'hover-effect-zoom3' => array( 'label' => __( 'Zoom 3' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-zoom3.png' ),
-											'hover-effect-plus' => array( 'label' => __( 'Plus' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-plus.png' ),
-											'hover-effect-circle-plus' => array( 'label' => __( 'Cirlce Plus' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-circle-plus.png' ),
-											'hover-effect-eye' => array( 'label' => __( 'Eye' , 'foogallery' ), 'img' => FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'img/admin/hover-effect-icon-eye.png' )
-									),
-							),
-							array(
-									'id'      => 'caption-hover-effect',
-									'title'   => __( 'Caption Effect', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'default' => 'hover-caption-simple',
-									'type'    => 'radio',
-									'choices' => apply_filters( 'foogallery_gallery_template_caption-hover-effects', array(
-											'hover-caption-simple'  => __( 'Simple', 'foogallery' ),
-											'hover-caption-full-drop'   => __( 'Drop', 'foogallery' ),
-											'hover-caption-full-fade' => __( 'Fade In', 'foogallery' ),
-											'hover-caption-push' => __( 'Push', 'foogallery' ),
-											'hover-caption-simple-always' => __( 'Always Visible', 'foogallery' )
-									) ),
-									'spacer'  => '<span class="spacer"></span>'
-							),
-							array(
-									'id'      => 'caption-content',
-									'title'   => __( 'Caption Content', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'default' => 'title',
-									'type'    => 'radio',
-									'choices' => apply_filters( 'foogallery_gallery_template_caption-content', array(
-											'title'  => __( 'Title Only', 'foogallery' ),
-											'desc'   => __( 'Description Only', 'foogallery' ),
-											'both' => __( 'Title and Description', 'foogallery' )
-									) ),
-									'spacer'  => '<span class="spacer"></span>'
-							),
-							array(
-									'id' => 'thumb_preview',
-									'title' => __( 'Preview', 'foogallery' ),
-									'desc' => __( 'This is what your gallery thumbnails will look like.', 'foogallery' ),
-									'section' => __( 'Thumbnail Settings', 'foogallery' ),
-									'type' => 'default_thumb_preview',
-							)
+				'slug'        => 'default',
+				'name'        => __( 'Responsive Image Gallery', 'foogallery' ),
+				'lazyload_support' => true,
+				'preview_css' => FOOGALLERY_DEFAULT_GALLERY_TEMPLATE_URL . 'css/gallery-default.css',
+				'admin_js'	  => FOOGALLERY_DEFAULT_GALLERY_TEMPLATE_URL . 'js/admin-gallery-default.js',
+				'fields'	  => array(
+					array(
+						'id'      => 'lightbox',
+						'title'   => __( 'Lightbox', 'foogallery' ),
+						'desc'    => __( 'Choose which lightbox you want to use. The lightbox will only work if you set the thumbnail link to "Full Size Image".', 'foogallery' ),
+						'type'    => 'lightbox',
+					),
+					array(
+						'id'      => 'spacing',
+						'title'   => __( 'Spacing', 'foogallery' ),
+						'desc'    => __( 'The spacing or gap between thumbnails in the gallery.', 'foogallery' ),
+						'type'    => 'select',
+						'default' => 'spacing-width-10',
+						'choices' => array(
+							'spacing-width-0' => __( '0 pixels', 'foogallery' ),
+							'spacing-width-5' => __( '5 pixels', 'foogallery' ),
+							'spacing-width-10' => __( '10 pixels', 'foogallery' ),
+							'spacing-width-15' => __( '15 pixels', 'foogallery' ),
+							'spacing-width-20' => __( '20 pixels', 'foogallery' ),
+							'spacing-width-25' => __( '25 pixels', 'foogallery' ),
+						),
+					),
+					array(
+						'id'      => 'alignment',
+						'title'   => __( 'Alignment', 'foogallery' ),
+						'desc'    => __( 'The horizontal alignment of the thumbnails inside the gallery.', 'foogallery' ),
+						'default' => 'alignment-center',
+						'type'    => 'select',
+						'choices' => array(
+							'alignment-left' => __( 'Left', 'foogallery' ),
+							'alignment-center' => __( 'Center', 'foogallery' ),
+							'alignment-right' => __( 'Right', 'foogallery' ),
+						)
+					),
+					array(
+						'id'      => 'loading_animation',
+						'title'   => __( 'Loading Indicator', 'foogallery' ),
+						'default' => 'yes',
+						'type'    => 'radio',
+						'choices' => array(
+							'yes'  => __( 'Show Thumbnail Loading Indicator', 'foogallery' ),
+							'no'   => __( 'Disabled', 'foogallery' )
+						),
+						'spacer'  => '<span class="spacer"></span>',
+						'desc'	  => __( 'By default, an animated loading animation indicator is shown before the thumbnails have loaded. You can disable the loader if you want.', 'foogallery' ),
+					),
+					array(
+						'id'      => 'thumbnail_dimensions',
+						'title'   => __( 'Size', 'foogallery' ),
+						'desc'    => __( 'Choose the size of your thumbnails.', 'foogallery' ),
+						'section' => __( 'Thumbnail Settings', 'foogallery' ),
+						'type'    => 'thumb_size',
+						'default' => array(
+							'width' => get_option( 'thumbnail_size_w' ),
+							'height' => get_option( 'thumbnail_size_h' ),
+							'crop' => true,
+						),
+					),
+					array(
+						'id'      => 'thumbnail_link',
+						'title'   => __( 'Link', 'foogallery' ),
+						'section' => __( 'Thumbnail Settings', 'foogallery' ),
+						'default' => 'image',
+						'type'    => 'thumb_link',
+						'spacer'  => '<span class="spacer"></span>',
+						'desc'	  => __( 'You can choose to link each thumbnail to the full size image, the image\'s attachment page, a custom URL, or you can choose to not link to anything.', 'foogallery' ),
 					)
+//					array(
+//						'id' => 'thumb_preview',
+//						'title' => __( 'Preview', 'foogallery' ),
+//						'desc' => __( 'This is what your gallery thumbnails will look like.', 'foogallery' ),
+//						'section' => __( 'Thumbnail Settings', 'foogallery' ),
+//						'type' => 'default_thumb_preview',
+//					)
+				)
 			);
 
 			return $gallery_templates;
+		}
+
+		/**
+		 * Add thumbnail fields to the gallery template
+		 *
+		 * @uses "foogallery_override_gallery_template_fields"
+		 * @param $fields
+		 * @param $template
+		 *
+		 * @return array
+		 */
+		function add_common_thumbnail_fields( $fields, $template ) {
+			$fields = array_merge( $fields, foogallery_get_gallery_template_common_thumbnail_fields($template) );
+
+			return $fields;
 		}
 
 		/**
@@ -241,10 +186,22 @@ if ( !class_exists( 'FooGallery_Default_Gallery_Template' ) ) {
 		 * Enqueue scripts that the default gallery template relies on
 		 */
 		function enqueue_dependencies( $gallery ) {
-			if ( 'yes' === $gallery->get_meta( 'default_loading_animation', 'yes' ) ) {
-				wp_enqueue_script( 'jquery' );
-				foogallery_enqueue_imagesloaded_script();
-			}
+			wp_enqueue_script( 'jquery' );
+
+			//how do we handle loading animations
+//			if ( 'yes' === $gallery->get_meta( 'default_loading_animation', 'yes' ) ) {
+//				foogallery_enqueue_imagesloaded_script();
+//			}
+
+			//enqueue core files
+			foogallery_enqueue_core_gallery_template_style();
+			foogallery_enqueue_core_gallery_template_script();
+
+			$css = FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_URL . 'default/css/foogallery.responsive.min.css';
+			wp_enqueue_style( 'foogallery-default', $css, array(), FOOGALLERY_VERSION );
+
+			$js = FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_URL . 'default/js/foogallery.responsive.min.js';
+			wp_enqueue_script( 'foogallery-default', $js, array(), FOOGALLERY_VERSION );
 		}
 
 		/**
--- extensions/default-templates/default/gallery-default.php
+++ extensions/default-templates/default/gallery-default.php
@@ -16,17 +16,21 @@ $alignment = foogallery_gallery_template_setting( 'alignment', 'alignment-center
 $hover_effect_type = foogallery_gallery_template_setting( 'hover-effect-type', '' );
 $caption_hover_effect = foogallery_gallery_template_setting( 'caption-hover-effect', 'hover-caption-simple' );
 $caption_content = foogallery_gallery_template_setting( 'caption-content', 'title' );
-$loading_animation = 'yes' === foogallery_gallery_template_setting( 'loading_animation', 'yes' ) ? 'foogallery-default-loading' : '';
+$loading_animation = 'yes' === foogallery_gallery_template_setting( 'loading_animation', 'yes' ) ? 'loading-icon-default' : '';
+$lazyload = 'yes' === foogallery_gallery_template_setting( 'lazyload', 'yes' ) ? 'data-loader-options="{\'lazy\':true}"' : '';
 if ( 'hover-effect-caption' === $hover_effect_type ||
 		'hover-effect-none' === $hover_effect_type ) {
 	$hover_effect = '';
 }
-?><div id="foogallery-gallery-<?php echo $current_foogallery->ID; ?>" class="<?php foogallery_build_class_attribute_render_safe( $current_foogallery, 'foogallery-link-' . $link, 'foogallery-lightbox-' . $lightbox, $spacing, $hover_effect, $hover_effect_type, $border_style, $alignment, $caption_hover_effect, $loading_animation ); ?>">
+$foogallery_default_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'loaded-fade-in', 'foogallery-link-' . $link, 'foogallery-lightbox-' . $lightbox, $spacing, $hover_effect, $hover_effect_type, $border_style, $alignment, $caption_hover_effect, $loading_animation );
+?><div id="foogallery-gallery-<?php echo $current_foogallery->ID; ?>" <?php echo $lazyload; ?> class="<?php echo $foogallery_default_classes; ?>">
 	<?php foreach ( $current_foogallery->attachments() as $attachment ) {
+		echo '<div class="foogallery-item">';
 		echo $attachment->html( $args, true, false );
 		if ( 'hover-effect-caption' === $hover_effect_type ) {
 			echo $attachment->html_caption( $caption_content );
 		}
 		echo '</a>';
+		echo '</div>';
 	} ?>
 </div>
