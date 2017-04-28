50917bd012320e2a422e032e9774a1b6a723da88
Brad Vincent <bradvin@gmail.com>
2017-04-28 15:57:55 +0200

    refactored settings and settings helper in separate classes

--- includes/admin/class-gallery-metabox-settings.php
+++ includes/admin/class-gallery-metabox-settings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: brad
+ * User: bradvin
  * Date: 2017/04/19
  * Time: 1:19 PM
  */
@@ -12,192 +12,55 @@ if ( ! class_exists( 'FooGallery_Admin_Gallery_MetaBox_Settings' ) ) {
     class FooGallery_Admin_Gallery_MetaBox_Settings {
 
         /**
-         * @var FooGallery
-         */
-        private $gallery;
-
-        /**
-         * @var bool
-         */
-        private $hide_help;
-
-        /**
-         * @var array
-         */
-        public $gallery_templates;
-
-        /**
          * FooGallery_Admin_Gallery_MetaBox_Settings constructor.
-         * @param $gallery FooGallery
          */
-        function __construct($gallery) {
-            $this->gallery = $gallery;
-            $this->hide_help = 'on' == foogallery_get_setting( 'hide_gallery_template_help' );
+        function __construct() {
+            //enqueue assets for the new settings tabs
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 
-            $this->gallery_templates = foogallery_gallery_templates();
-
-            //check if we have no templates
-            if ( 0 === count( $this->gallery_templates ) ) {
-                //force the default template to activate if there are no other gallery templates
-                foogallery_activate_default_templates_extension();
-                $this->gallery_templates = foogallery_gallery_templates();
-            }
-
-            $this->current_gallery_template = foogallery_default_gallery_template();
-            if ( ! empty($this->gallery->gallery_template) ) {
-                $this->current_gallery_template = $this->gallery->gallery_template;
-            }
+            //set default settings tab icons
+            add_filter( 'foogallery_gallery_settings_metabox_section_icon', array( $this, 'add_section_icons') );
         }
 
-        private function render_gallery_template_settings_tabs( $template, $sections ) {
-            $tab_active = 'foogallery-tab-active';
-            foreach ( $sections as $section_slug => $section ) { ?>
-                <div class="foogallery-vertical-tab <?php echo $tab_active; ?>"
-                     data-name="<?php echo $template['slug']; ?>-<?php echo $section_slug; ?>">
-                    <span class="dashicons <?php echo $section['icon_class']; ?>"></span>
-                    <span class="foogallery-tab-text"><?php echo $section['name']; ?></span>
-                </div>
-            <?php
-                $tab_active = '';
+        /***
+         * Enqueue the assets needed by the settings
+         * @param $hook_suffix
+         */
+        function enqueue_assets( $hook_suffix ){
+            if( in_array( $hook_suffix, array( 'post.php', 'post-new.php' ) ) ) {
+                $screen = get_current_screen();
+
+                if ( is_object( $screen ) && FOOGALLERY_CPT_GALLERY == $screen->post_type ){
+
+                    // Register, enqueue scripts and styles here
+                    wp_enqueue_script( 'foogallery-admin-settings', FOOGALLERY_URL . '/js/foogallery.admin.min.js', FOOGALLERY_VERSION, 'jquery' );
+
+                    wp_enqueue_style( 'foogallery-admin-settings', FOOGALLERY_URL . '/css/foogallery.admin.min.css', FOOGALLERY_VERSION );
+                }
             }
         }
 
-        private function render_gallery_template_settings_tab_contents( $template, $sections ) {
-            $tab_active = 'foogallery-tab-active';
-            foreach ( $sections as $section_slug => $section ) { ?>
-                <div class="foogallery-tab-content <?php echo $tab_active; ?>"
-                     data-name="<?php echo $template['slug']; ?>-<?php echo $section_slug; ?>">
-                    <?php $this->render_gallery_template_settings_tab_contents_fields( $template, $section ); ?>
-                </div>
-            <?php
-                $tab_active = '';
-            }
-        }
-
-        private function render_gallery_template_settings_tab_contents_fields( $template, $section ) {
-            ?>
-            <table class="foogallery-metabox-settings">
-                <tbody>
-                <?php
-                foreach ( $section['fields'] as $field ) {
-                    $field_type = isset( $field['type'] ) ? $field['type'] : 'unknown';
-                    $field_class ="foogallery_template_field foogallery_template_field_type-{$field_type} foogallery_template_field-{$template['slug']} foogallery_template_field-{$template['slug']}-{$field['id']}";
-                    ?>
-                    <tr class="<?php echo $field_class; ?>">
-                        <?php if ( 'help' == $field_type ) { ?>
-                            <td colspan="2">
-                                <div class="foogallery-help">
-                                    <?php echo $field['desc']; ?>
-                                </div>
-                            </td>
-                        <?php } else { ?>
-                            <th>
-                                <label for="FooGallerySettings_<?php echo $template['slug'] . '_' . $field['id']; ?>"><?php echo $field['title']; ?></label>
-                                <?php if ( !empty( $field['desc'] ) ) { ?>
-                                <span data-balloon-length="large" data-balloon-pos="right" data-balloon="<?php echo esc_attr($field['desc']); ?>"><i class="dashicons dashicons-editor-help"></i></span>
-                                <?php } ?>
-                            </th>
-                            <td>
-                                <?php do_action( 'foogallery_render_gallery_template_field', $field, $this->gallery, $template ); ?>
-                            </td>
-                        <?php } ?>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-            <?php
-        }
-
-        private function render_gallery_template_settings( $template ) {
-            $sections = $this->build_model_for_template( $template );
-            ?>
-            <div class="foogallery-settings">
-                <div class="foogallery-vertical-tabs">
-                    <?php $this->render_gallery_template_settings_tabs( $template, $sections ); ?>
-                </div>
-                <div class="foogallery-tab-contents">
-                    <?php $this->render_gallery_template_settings_tab_contents( $template, $sections ); ?>
-                </div>
-            </div>
-            <?php
-        }
-
-        public function render_gallery_settings() {
-            foreach ( $this->gallery_templates as $template ) {
-                $field_visibility = ($this->current_gallery_template !== $template['slug']) ? 'style="display:none"' : '';
-                ?><div
-                    class="foogallery-settings-container foogallery-settings-container-<?php echo $template['slug']; ?>"
-                    <?php echo $field_visibility; ?>>
-                    <?php $this->render_gallery_template_settings( $template ); ?>
-                </div><?php
-            }
-        }
 
         /**
-         * build up and return a model that we can use to render the gallery settings
+         * Returns the Dashicon that can be used in the settings tabs
+         * @param $section_slug
+         * @return string
          */
-        private function build_model_for_template($template) {
-            //allow for extensions to override fields for every gallery template.
-            // Also passes the $template along so you can inspect and conditionally alter fields based on the template properties
-            $fields = apply_filters( 'foogallery_override_gallery_template_fields', $template['fields'], $template );
-
-            //allow for extensions to override fields for every gallery template.
-            // Also passes the $template along so you can inspect and conditionally alter fields based on the template properties
-            $fields = apply_filters( "foogallery_override_gallery_template_fields-{$template['slug']}", $fields, $template );
-
-            //create a sections array and fill it with fields
-            $sections = array();
-            foreach ( $fields as $field ) {
-                //allow for the field to be altered by extensions. Also used by the build-in fields, e.g. lightbox
-                $field = apply_filters( 'foogallery_alter_gallery_template_field', $field, $this->gallery );
-
-                if (isset($field['type']) && 'help' == $field['type'] && $this->hide_help) {
-                    continue; //skip help if the 'hide help' setting is turned on
-                }
-
-                $section_name = isset($field['section']) ? $field['section'] : __( 'General', 'foogallery' );
-
-                $section_slug = strtolower( $section_name );
-
-                if ( !isset( $sections[ $section_slug ] ) ) {
-                    $sections[ $section_slug ] = array (
-                        'name' => $section_name,
-                        'icon_class' => apply_filters( 'foogallery_gallery_settings_metabox_section_icon', $section_slug ),
-                        'fields' => array()
-                    );
-                }
-
-                $sections[ $section_slug ]['fields'][] = $field;
+        function add_section_icons( $section_slug ) {
+            switch ( $section_slug ) {
+                case 'general':
+                    return 'dashicons-admin-tools';
+                    break;
+                case 'advanced':
+                    return 'dashicons-admin-generic';
+                    break;
+                case 'look &amp; feel':
+                    return 'dashicons-images-alt2';
+                    break;
+                case 'video':
+                    return 'dashicons-format-video';
             }
-
-            return $sections;
-        }
-
-        public function render_hidden_gallery_template_selector() {
-            ?>
-            <span class="hidden foogallery-template-selector"> &mdash;
-                <select id="FooGallerySettings_GalleryTemplate" name="<?php echo FOOGALLERY_META_TEMPLATE; ?>">
-                    <?php
-                    foreach ( $this->gallery_templates as $template ) {
-                        $selected = ($this->current_gallery_template === $template['slug']) ? 'selected' : '';
-
-                        $preview_css = '';
-                        if ( isset( $template['preview_css'] ) ) {
-                            if ( is_array( $template['preview_css'] ) ) {
-                                //dealing with an array of css files to include
-                                $preview_css = implode( ',', $template['preview_css'] );
-                            } else {
-                                $preview_css = $template['preview_css'];
-                            }
-                        }
-                        $preview_css = empty( $preview_css ) ? '' : ' data-preview-css="' . $preview_css . '" ';
-
-                        echo "<option {$selected}{$preview_css} value=\"{$template['slug']}\">{$template['name']}</option>";
-                    }
-                    ?>
-                </select>
-            </span>
-            <?php
+            return 'dashicons-admin-tools';
         }
     }
 }
