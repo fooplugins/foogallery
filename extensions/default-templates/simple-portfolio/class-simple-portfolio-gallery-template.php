f0b48463fb5c2a86eb86f3b6cb93f10dba936ef4
Brad Vincent <bradvin@gmail.com>
2017-05-02 18:50:49 +0200

    simple portfolio settings changes for new settings tabs

--- extensions/default-templates/simple-portfolio/class-simple-portfolio-gallery-template.php
+++ extensions/default-templates/simple-portfolio/class-simple-portfolio-gallery-template.php
@@ -36,84 +36,87 @@ if ( !class_exists( 'FooGallery_Simple_Portfolio_Gallery_Template' ) ) {
 		 * @return array
 		 */
 		function add_template( $gallery_templates ) {
-
 			$gallery_templates[] = array(
-					'slug'        => 'simple_portfolio',
-					'name'        => __( 'Simple Portfolio', 'foogallery' ),
-					'lazyload_support' => true,
-					'fields'	  => array(
-							array(
-									'id'	  => 'help',
-									'title'	  => __( 'Tip', 'foogallery' ),
-									'type'	  => 'html',
-									'help'	  => true,
-									'desc'	  => __( 'The Simple Portfolio template works best when you have <strong>captions and descriptions</strong> set for every attachment in the gallery.<br />To change captions and descriptions, simply hover over the thumbnail above and click the "i" icon.', 'foogallery' ),
-							),
-							array(
-									'id'      => 'thumbnail_dimensions',
-									'title'   => __( 'Thumbnail Size', 'foogallery' ),
-									'desc'    => __( 'Choose the size of your thumbnails.', 'foogallery' ),
-									'type'    => 'thumb_size',
-									'default' => array(
-											'width' => 250,
-											'height' => 200,
-											'crop' => true,
-									),
-							),
-							array(
-									'id'      => 'thumbnail_link',
-									'title'   => __( 'Thumbnail Link', 'foogallery' ),
-									'default' => 'image',
-									'type'    => 'thumb_link',
-									'spacer'  => '<span class="spacer"></span>',
-									'desc'	  => __( 'You can choose to link each thumbnail to the full size image, or to the image\'s attachment page, or you can choose to not link to anything.', 'foogallery' )
-							),
-							array(
-									'id'      => 'lightbox',
-									'title'   => __( 'Lightbox', 'foogallery' ),
-									'desc'    => __( 'Choose which lightbox you want to display images with. The lightbox will only work if you set the thumbnail link to "Full Size Image".', 'foogallery' ),
-									'type'    => 'lightbox',
-							),
-							array(
-									'id'      => 'gutter',
-									'title'   => __( 'Gutter', 'foogallery' ),
-									'desc'    => __( 'The spacing between each thumbnail in the gallery.', 'foogallery' ),
-									'type'    => 'number',
-									'class'   => 'small-text',
-									'default' => 40,
-									'step'    => '1',
-									'min'     => '0',
-							),
-							array(
-								'id'      => 'caption_position',
-								'title' => __('Caption Position', 'foogallery'),
-								'desc' => __('Where the captions are displayed in relation to the thumbnail.', 'foogallery'),
-								'section' => __( 'Caption Settings', 'foogallery' ),
-								'default' => '',
-								'type'    => 'radio',
-								'spacer'  => '<span class="spacer"></span>',
-								'choices' => array(
-									'' => __( 'Below', 'foogallery' ),
-									'bf-captions-above' => __( 'Above', 'foogallery' )
-								)
-							),
-							array(
-								'id'      => 'caption_bg_color',
-								'title'   => __( 'Caption Background Color', 'foogallery' ),
-								'section' => __( 'Caption Settings', 'foogallery' ),
-								'type'    => 'colorpicker',
-								'default' => '#fff',
-								'opacity' => true
-							),
-							array(
-								'id'      => 'caption_text_color',
-								'title'   => __( 'Caption Text Color', 'foogallery' ),
-								'section' => __( 'Caption Settings', 'foogallery' ),
-								'type'    => 'colorpicker',
-								'default' => '#333',
-								'opacity' => true
-							)
-					),
+                'slug'        => 'simple_portfolio',
+                'name'        => __( 'Simple Portfolio', 'foogallery' ),
+                'lazyload_support' => true,
+                'fields'	  => array(
+                    array(
+                        'id'	  => 'help',
+                        'title'	  => __( 'Tip', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'	  => 'html',
+                        'help'	  => true,
+                        'desc'	  => __( 'The Simple Portfolio template works best when you have <strong>captions and descriptions</strong> set for every attachment in the gallery. To change captions and descriptions, simply hover over the thumbnail above and click the "i" icon.', 'foogallery' ),
+                    ),
+                    array(
+                        'id'      => 'thumbnail_dimensions',
+                        'title'   => __( 'Thumbnail Size', 'foogallery' ),
+                        'desc'    => __( 'Choose the size of your thumbnails.', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'thumb_size',
+                        'default' => array(
+                            'width' => 250,
+                            'height' => 200,
+                            'crop' => true,
+                        ),
+                    ),
+                    array(
+                        'id'      => 'thumbnail_link',
+                        'title'   => __( 'Thumbnail Link', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'default' => 'image',
+                        'type'    => 'thumb_link',
+                        'desc'	  => __( 'You can choose to link each thumbnail to the full size image, or to the image\'s attachment page, or you can choose to not link to anything.', 'foogallery' )
+                    ),
+                    array(
+                        'id'      => 'lightbox',
+                        'title'   => __( 'Lightbox', 'foogallery' ),
+                        'desc'    => __( 'Choose which lightbox you want to display images with. The lightbox will only work if you set the thumbnail link to "Full Size Image".', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'lightbox',
+                    ),
+                    array(
+                        'id'      => 'gutter',
+                        'title'   => __( 'Gutter', 'foogallery' ),
+                        'desc'    => __( 'The spacing between each thumbnail in the gallery.', 'foogallery' ),
+                        'section' => __( 'Look &amp; Feel', 'foogallery' ),
+                        'type'    => 'number',
+                        'class'   => 'small-text',
+                        'default' => 40,
+                        'step'    => '1',
+                        'min'     => '0',
+                    ),
+                    array(
+                        'id'      => 'caption_position',
+                        'title' => __('Caption Position', 'foogallery'),
+                        'desc' => __('Where the captions are displayed in relation to the thumbnail.', 'foogallery'),
+                        'section' => __( 'Captions', 'foogallery' ),
+                        'default' => '',
+                        'type'    => 'radio',
+                        'spacer'  => '<span class="spacer"></span>',
+                        'choices' => array(
+                            '' => __( 'Below', 'foogallery' ),
+                            'bf-captions-above' => __( 'Above', 'foogallery' )
+                        )
+                    ),
+                    array(
+                        'id'      => 'caption_bg_color',
+                        'title'   => __( 'Caption Background Color', 'foogallery' ),
+                        'section' => __( 'Captions', 'foogallery' ),
+                        'type'    => 'colorpicker',
+                        'default' => '#fff',
+                        'opacity' => true
+                    ),
+                    array(
+                        'id'      => 'caption_text_color',
+                        'title'   => __( 'Caption Text Color', 'foogallery' ),
+                        'section' => __( 'Captions', 'foogallery' ),
+                        'type'    => 'colorpicker',
+                        'default' => '#333',
+                        'opacity' => true
+                    )
+                ),
 			);
 
 			return $gallery_templates;
