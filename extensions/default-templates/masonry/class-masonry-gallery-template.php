7a416060ecc85f35084fff400ef176d7c595c2b8
Brad Vincent <bradvin@gmail.com>
2017-05-02 18:50:26 +0200

    masonry settings changes for new settings tabs

--- extensions/default-templates/masonry/class-masonry-gallery-template.php
+++ extensions/default-templates/masonry/class-masonry-gallery-template.php
@@ -38,86 +38,103 @@ if ( !class_exists( 'FooGallery_Masonry_Gallery_Template' ) ) {
 		 */
 		function add_template( $gallery_templates ) {
 			$gallery_templates[] = array(
-					'slug'        => 'masonry',
-					'name'        => __( 'Masonry Image Gallery', 'foogallery' ),
-					'lazyload_support' => true,
-					'admin_js'	  => FOOGALLERY_MASONRY_GALLERY_TEMPLATE_URL . 'js/admin-gallery-masonry.js',
-					'fields'	  => array(
-							array(
-									'id'      => 'thumbnail_width',
-									'title'   => __( 'Thumbnail Width', 'foogallery' ),
-									'desc'    => __( 'Choose the width of your thumbnails. Thumbnails will be generated on the fly and cached once generated.', 'foogallery' ),
-									'type'    => 'number',
-									'class'   => 'small-text',
-									'default' => 150,
-									'step'    => '1',
-									'min'     => '0',
-							),
-							array(
-									'id'      => 'layout',
-									'title'   => __( 'Masonry Layout', 'foogallery' ),
-									'desc'    => __( 'Choose a fixed width thumb layout, or responsive columns.', 'foogallery' ),
-									'type'    => 'radio',
-									'choices' => array(
-											'fixed'  => __( 'Fixed Width', 'foogallery' ),
-											'2col'   => __( '2 Columns', 'foogallery' ),
-											'3col'   => __( '3 Columns', 'foogallery' ),
-											'4col'   => __( '4 Columns', 'foogallery' ),
-											'5col'   => __( '5 Columns', 'foogallery' )
-									),
-									'spacer'  => '<span class="spacer"></span>',
-									'default' => 'fixed'
-							),
-							array(
-									'id'      => 'gutter_width',
-									'title'   => __( 'Gutter Width', 'foogallery' ),
-									'desc'    => __( 'The spacing between your thumbnails. Only applicable when using a fixed layout!', 'foogallery' ),
-									'type'    => 'number',
-									'class'   => 'small-text',
-									'default' => 10,
-									'step'    => '1',
-									'min'     => '0',
-							),
-							array(
-									'id'      => 'center_align',
-									'title'   => __( 'Image Alignment', 'foogallery' ),
-									'desc'    => __( 'You can choose to center align your images or leave them at the default. Only applicable when using a fixed layout!', 'foogallery' ),
-									'type'    => 'radio',
-									'choices' => array(
-											'default'  => __( 'Left Alignment', 'foogallery' ),
-											'center'   => __( 'Center Alignment', 'foogallery' )
-									),
-									'spacer'  => '<span class="spacer"></span>',
-									'default' => 'default'
-							),
-							array(
-									'id'      => 'gutter_percent',
-									'title'   => __( 'Gutter Size', 'foogallery' ),
-									'desc'    => __( 'Choose a gutter size when using responsive columns.', 'foogallery' ),
-									'type'    => 'radio',
-									'choices' => array(
-											'no-gutter'   => __( 'No Gutter', 'foogallery' ),
-											''  => __( 'Normal Size Gutter', 'foogallery' ),
-											'large-gutter'   => __( 'Larger Gutter', 'foogallery' )
-									),
-									'spacer'  => '<span class="spacer"></span>',
-									'default' => ''
-							),
-							array(
-									'id'      => 'thumbnail_link',
-									'title'   => __( 'Thumbnail Link', 'foogallery' ),
-									'default' => 'image' ,
-									'type'    => 'thumb_link',
-									'spacer'  => '<span class="spacer"></span>',
-									'desc'	  => __( 'You can choose to link each thumbnail to the full size image, or to the image\'s attachment page, or you can choose to not link to anything.', 'foogallery' ),
-							),
-							array(
-									'id'      => 'lightbox',
-									'title'   => __( 'Lightbox', 'foogallery' ),
-									'desc'    => __( 'Choose which lightbox you want to display images with. The lightbox will only work if you set the thumbnail link to "Full Size Image".', 'foogallery' ),
-									'type'    => 'lightbox',
-							),
-					),
+                'slug'        => 'masonry',
+                'name'        => __( 'Masonry Image Gallery', 'foogallery' ),
+                'lazyload_support' => true,
+                'admin_js'	  => FOOGALLERY_MASONRY_GALLERY_TEMPLATE_URL . 'js/admin-gallery-masonry.js',
+                'fields'	  => array(
+                    array(
+                        'id'      => 'thumbnail_width',
+                        'title'   => __( 'Thumb Width', 'foogallery' ),
+                        'desc'    => __( 'Choose the width of your thumbnails. Thumbnails will be generated on the fly and cached once generated', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'number',
+                        'class'   => 'small-text',
+                        'default' => 150,
+                        'step'    => '1',
+                        'min'     => '0',
+                    ),
+                    array(
+                        'id'      => 'layout',
+                        'title'   => __( 'Masonry Layout', 'foogallery' ),
+                        'desc'    => __( 'Choose a fixed width thumb layout, or responsive columns.', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'radio',
+                        'choices' => array(
+                            'fixed'  => __( 'Fixed Width', 'foogallery' ),
+                            '2col'   => __( '2 Columns', 'foogallery' ),
+                            '3col'   => __( '3 Columns', 'foogallery' ),
+                            '4col'   => __( '4 Columns', 'foogallery' ),
+                            '5col'   => __( '5 Columns', 'foogallery' )
+                        ),
+                        'default' => 'fixed',
+                        'row_data'=> array(
+                            'data-foogallery-change-selector' => 'input:radio',
+                            'data-foogallery-value-selector' => 'input:checked'
+                        )
+                    ),
+                    array(
+                        'id'      => 'gutter_width',
+                        'title'   => __( 'Gutter Width', 'foogallery' ),
+                        'desc'    => __( 'The spacing between your thumbnails. Only applicable when using a fixed layout!', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'number',
+                        'class'   => 'small-text',
+                        'default' => 10,
+                        'step'    => '1',
+                        'min'     => '0',
+                        'row_data'=> array(
+                            'data-foogallery-hidden' => true,
+                            'data-foogallery-show-when-field' => 'layout',
+                            'data-foogallery-show-when-field-value' => 'fixed'
+                        )
+                    ),
+                    array(
+                        'id'      => 'gutter_percent',
+                        'title'   => __( 'Gutter Size', 'foogallery' ),
+                        'desc'    => __( 'Choose a gutter size when using responsive columns.', 'foogallery' ),
+                        'section' => __( 'General', 'foogallery' ),
+                        'type'    => 'radio',
+                        'choices' => array(
+                            'no-gutter'   => __( 'No Gutter', 'foogallery' ),
+                            ''  => __( 'Normal Size Gutter', 'foogallery' ),
+                            'large-gutter'   => __( 'Larger Gutter', 'foogallery' )
+                        ),
+                        'default' => '',
+                        'row_data'=> array(
+                            'data-foogallery-hidden' => true,
+                            'data-foogallery-show-when-field' => 'layout',
+                            'data-foogallery-show-when-field-operator' => '!==',
+                            'data-foogallery-show-when-field-value' => 'fixed'
+                        )
+                    ),
+                    array(
+                        'id'      => 'center_align',
+                        'title'   => __( 'Alignment', 'foogallery' ),
+                        'desc'    => __( 'You can choose to center align your images or leave them at the default. Only applicable when using a fixed layout!', 'foogallery' ),
+                        'section' => __( 'Look &amp; Feel', 'foogallery' ),
+                        'type'    => 'radio',
+                        'choices' => array(
+                            'default'  => __( 'Left Alignment', 'foogallery' ),
+                            'center'   => __( 'Center Alignment', 'foogallery' ),
+                            'right'   => __( 'Right Alignment', 'foogallery' )
+                        ),
+                        'default' => 'default'
+                    ),
+                    array(
+                        'id'      => 'thumbnail_link',
+                        'title'   => __( 'Thumb Link', 'foogallery' ),
+                        'default' => 'image' ,
+                        'type'    => 'thumb_link',
+                        'desc'	  => __( 'You can choose to link each thumbnail to the full size image, or to the image\'s attachment page, or you can choose to not link to anything', 'foogallery' ),
+                    ),
+                    array(
+                        'id'      => 'lightbox',
+                        'title'   => __( 'Lightbox', 'foogallery' ),
+                        'desc'    => __( 'Choose which lightbox you want to display images with. The lightbox will only work if you set the thumbnail link to "Full Size Image"', 'foogallery' ),
+                        'type'    => 'lightbox',
+                    ),
+                ),
 			);
 
 
