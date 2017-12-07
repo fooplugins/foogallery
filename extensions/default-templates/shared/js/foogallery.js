35c526d255976fc5e90bb9dcc520e346530c366b
Brad Vincent <bradvin@gmail.com>
2017-12-07 23:01:48 +0200

    updated to 1.0.17 of client JS

--- extensions/default-templates/shared/js/foogallery.js
+++ extensions/default-templates/shared/js/foogallery.js
@@ -7604,10 +7604,10 @@
 		FooGallery,
 		FooGallery.utils.obj
 );
-(function($, _utils){
+(function($, _utils, _is){
 
-	function foobox_integration(e, tmpl){
-		if (tmpl.$el.is(".foogallery-lightbox-foobox, .foogallery-lightbox-foobox-free")){
+	function wp_integration(e, tmpl, current, prev){
+		if ((e.type === "after-page-change" && prev !== 0) || e.type === "ready"){
 			$("body").trigger("post-load");
 		}
 	}
@@ -7616,7 +7616,7 @@
 	$(function () {
 		$('[id^="foogallery-"]:not(.fg-ready)').foogallery({
 			on: {
-				"after-page-change.foogallery": foobox_integration
+				"ready.foogallery after-page-change.foogallery": wp_integration
 			}
 		});
 	});
@@ -7624,12 +7624,13 @@
 	_utils.ready(function(){
 		$('[id^="foogallery-"].fg-ready').foogallery({
 			on: {
-				"after-page-change.foogallery": foobox_integration
+				"ready.foogallery after-page-change.foogallery": wp_integration
 			}
 		});
 	});
 
 })(
 		FooGallery.$,
-		FooGallery.utils
+		FooGallery.utils,
+		FooGallery.utils.is
 );
