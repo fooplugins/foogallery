a1da2de0fc7b58c841cf37a3e1bb3012ff79669d
Brad Vincent <bradvin@gmail.com>
2018-05-22 20:33:40 +0200

    update to slider

--- pro/extensions/default-templates/slider/gallery-slider.php
+++ pro/extensions/default-templates/slider/gallery-slider.php
@@ -8,9 +8,10 @@ global $current_foogallery;
 
 $layout = foogallery_gallery_template_setting( 'layout', '' );
 $viewport = foogallery_gallery_template_setting( 'viewport', '' );
+$highlight = foogallery_gallery_template_setting( 'highlight', 'fgs-purple' );
 $thumbnail_captions = foogallery_gallery_template_setting( 'thumbnail_captions', '' );
 
-$foogallery_default_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'slider', $layout, $thumbnail_captions );
+$foogallery_default_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'slider', $layout, $highlight, $thumbnail_captions );
 $foogallery_default_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_default_classes ) );
 
 ?><div <?php echo $foogallery_default_attributes; ?>>
