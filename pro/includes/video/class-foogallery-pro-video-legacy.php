820fed363e42fb51d5e1db7d86dd5163da75d74e
Brad Vincent <bradvin@gmail.com>
2018-07-26 20:58:54 +0200

    remove legacy classes

--- pro/includes/video/class-foogallery-pro-video-legacy.php
+++ pro/includes/video/class-foogallery-pro-video-legacy.php
@@ -35,6 +35,12 @@ if ( ! class_exists( 'FooGallery_Pro_Video_Legacy' ) ) {
 
 				//short-circuit saving the post meta for video count on the gallery
 				add_filter( 'update_post_metadata', array( $this, 'short_circuit_legacy_video_count' ), 10, 5 );
+
+
+			}
+
+			if ( !is_admin() && class_exists( 'Foo_Video' ) ) {
+				add_filter( 'foogallery_build_class_attribute', array( $this, 'foogallery_build_class_attribute' ), 20 );
 			}
 
 			// Ajax calls for migrating
@@ -191,62 +197,20 @@ if ( ! class_exists( 'FooGallery_Pro_Video_Legacy' ) ) {
 		}
 
 		public function foogallery_build_class_attribute( $classes ) {
-			global $current_foogallery_template;
-
-			//first determine if the gallery has any videos
-
-			//get the selected video icon
-			$video_hover_icon = foogallery_gallery_template_setting( 'video_hover_icon', 'fg-video-default' );
-
-			//backwards compatible for the videoslider
-			if ( 'videoslider' === $current_foogallery_template ) {
-				switch ( $video_hover_icon ) {
-					case 'video-icon-default':
-						$video_hover_icon = 'rvs-flat-circle-play';
-						break;
-					case 'video-icon-1':
-						$video_hover_icon = 'rvs-plain-arrow-play';
-						break;
-					case 'video-icon-2':
-						$video_hover_icon = 'rvs-youtube-play';
-						break;
-					case 'video-icon-3':
-						$video_hover_icon = 'rvs-bordered-circle-play';
-						break;
-					default:
-						$video_hover_icon = '';
-				}
-			} else {
-				switch ( $video_hover_icon ) {
-					case 'video-icon-default':
-						$video_hover_icon = 'fg-video-default';
-						break;
-					case 'video-icon-1':
-						$video_hover_icon = 'fg-video-1';
-						break;
-					case 'video-icon-2':
-						$video_hover_icon = 'fg-video-2';
-						break;
-					case 'video-icon-3':
-						$video_hover_icon = 'fg-video-3';
-						break;
-					case 'video-icon-4':
-						$video_hover_icon = 'fg-video-4';
-						break;
-					default:
-						$video_hover_icon = '';
-				}
+			//remove any legacy classes
+			if ( ( $key = array_search( 'video-icon-1', $classes ) ) !== false ) {
+				unset( $classes[$key] );
+			}
+			if ( ( $key = array_search( 'video-icon-2', $classes ) ) !== false ) {
+				unset( $classes[$key] );
+			}
+			if ( ( $key = array_search( 'video-icon-3', $classes ) ) !== false ) {
+				unset( $classes[$key] );
+			}
+			if ( ( $key = array_search( 'video-icon-default', $classes ) ) !== false ) {
+				unset( $classes[$key] );
 			}
 
-			//include the video icon class
-			$classes[] = $video_hover_icon;
-			//get the video icon sticky state
-			$video_icon_sticky = foogallery_gallery_template_setting( 'foovideo_sticky_icon', '' );
-			if ( 'videoslider' === $current_foogallery_template && '' === $video_icon_sticky ) {
-				$video_icon_sticky = 'rvs-show-play-on-hover';
-			}
-			//include the video sticky class
-			$classes[] = $video_icon_sticky;
 			return $classes;
 		}
 
