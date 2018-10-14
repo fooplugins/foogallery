97ff258013952cc69e5d22a7c0696ba17e05a0f3
Brad Vincent <bradvin@gmail.com>
2018-10-14 15:28:14 +0200

    ElasticPress compatability Changed the way the styles were being persisted to page meta

--- includes/compatibility/class-foogallery-compatibility.php
+++ includes/compatibility/class-foogallery-compatibility.php
@@ -11,6 +11,7 @@ require_once( FOOGALLERY_PATH . 'includes/compatibility/class-polylang-compatibi
 require_once( FOOGALLERY_PATH . 'includes/compatibility/class-responsive-lightbox-dfactory-compatibility.php' );
 require_once( FOOGALLERY_PATH . 'includes/compatibility/class-wprocket-compatibility.php' );
 require_once( FOOGALLERY_PATH . 'includes/compatibility/class-foovideo-compatibility.php' );
+require_once( FOOGALLERY_PATH . 'includes/compatibility/class-elasticpress-compatibility.php' );
 
 if ( ! class_exists( 'FooGallery_Compatibility' ) ) {
 	class FooGallery_Compatibility {
@@ -20,6 +21,7 @@ if ( ! class_exists( 'FooGallery_Compatibility' ) ) {
 			new FooGallery_Polylang_Compatibility();
 			new FooGallery_Responsive_Lightbox_dFactory_Compatibility();
 			new FooGallery_FooVideo_Compatibility();
+			new FooGallery_ElasticPress_Compatibility();
 			//new FooGallery_WPRocket_Compatibility(); this has not been fully tested
 		}
 	}
