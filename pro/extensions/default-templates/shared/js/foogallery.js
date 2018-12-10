945882529ff7fd7308453d2ae92ad0b1c3952e62
Brad Vincent <bradvin@gmail.com>
2018-12-10 17:56:45 +0200

    updated to client side assets 1.1.5

--- pro/extensions/default-templates/shared/js/foogallery.js
+++ pro/extensions/default-templates/shared/js/foogallery.js
@@ -9187,6 +9187,7 @@
 			this.$el = $(element);
 			this.options = $.extend(true, {}, _.Portfolio.defaults, options);
 			this._items = [];
+			this._lastWidth = 0;
 		},
 		init: function(){
 			var self = this;
@@ -9197,21 +9198,25 @@
 			this.$el.removeAttr("style");
 		},
 		parse: function(){
-			var self = this, visible = self.$el.is(':visible'),
+			var self = this, visible = self.$el.is(':visible'), maxWidth = self.getContainerWidth(),
 					$test = $('<div/>', {'class': self.$el.attr('class')}).css({
 						position: 'absolute',
 						top: 0,
 						left: -9999,
 						visibility: 'hidden',
-						maxWidth: self.getContainerWidth()
+						maxWidth: maxWidth
 					}).appendTo('body');
 			self._items = self.$el.find(".fg-item").removeAttr("style").removeClass("fg-positioned").map(function(i, el){
 				var $item = $(el),
 						$thumb = $item.find(".fg-thumb"),
 						$img = $item.find(".fg-image"),
-						width = 0, height = 0;
-				$item.find(".fg-caption").css("max-width", parseFloat($img.attr("width")));
-				$img.css({ width: $img.attr("width"), height: $img.attr("height") });
+						width = parseFloat($img.attr("width")),
+						height = parseFloat($img.attr("height")),
+						iWidth = maxWidth < width ? maxWidth : width,
+						iHeight = maxWidth < width ? 'auto' : height;
+
+				$item.find(".fg-caption").css("max-width", iWidth);
+				$img.css({ width: iWidth, height: iHeight });
 				if (!visible){
 					var $clone = $item.clone();
 					$clone.appendTo($test);
@@ -9249,13 +9254,19 @@
 			refresh = _is.boolean(refresh) ? refresh : false;
 			autoCorrect = _is.boolean(autoCorrect) ? autoCorrect : true;
 
-			if (refresh || this._items.length === 0){
-				this.parse();
+			var self = this,
+					containerWidth = self.getContainerWidth();
+
+			if (self._lastWidth != 0 && Math.abs(containerWidth - self._lastWidth) > 0){
+				refresh = true;
+				self._lastWidth = containerWidth;
 			}
 
-			var self = this,
-					containerWidth = self.getContainerWidth(),
-					rows = self.rows(containerWidth),
+			if (refresh || self._items.length === 0){
+				self.parse();
+			}
+
+			var rows = self.rows(containerWidth),
 					offsetTop = 0;
 
 			for (var i = 0, l = rows.length, row; i < l; i++){
@@ -9264,6 +9275,9 @@
 				self.render(row);
 			}
 			self.$el.height(offsetTop);
+			if (self._lastWidth == 0){
+				self._lastWidth = containerWidth;
+			}
 			// if our layout caused the container width to get smaller
 			// i.e. makes a scrollbar appear then layout again to account for it
 			if (autoCorrect && self.getContainerWidth() < containerWidth){
