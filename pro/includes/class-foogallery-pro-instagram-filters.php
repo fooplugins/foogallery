8c0c8d9985728861119eb9a39dfcb29b69f6f1e7
Brad Vincent <bradvin@gmail.com>
2019-02-25 17:05:43 +0200

    updated filter setting updated to latest client side

--- pro/includes/class-foogallery-pro-instagram-filters.php
+++ pro/includes/class-foogallery-pro-instagram-filters.php
@@ -12,12 +12,10 @@ if ( ! class_exists( 'FooGallery_Pro_Instagram_Filters' ) ) {
 				add_filter( 'foogallery_override_gallery_template_fields', array( $this, 'add_instagram_fields' ), 10, 2 );
 			}
 
-			add_filter( 'foogallery_attachment_html_link_attributes', array( $this, 'append_instagram_filter' ), 10, 3 );
+			add_filter( 'foogallery_build_class_attribute', array( $this, 'append_instagram_filter_class' ), 10, 2 );
 
 			//build up any preview arguments
 			add_filter( 'foogallery_preview_arguments', array( $this, 'preview_arguments' ), 10, 3 );
-
-			add_action( 'foogallery_loaded_template', array( $this, 'enqueue_instagram_dependencies') );
 		}
 
 		/**
@@ -31,22 +29,6 @@ if ( ! class_exists( 'FooGallery_Pro_Instagram_Filters' ) ) {
 		function preview_arguments( $args, $post_data, $template ) {
 			$args['instagram'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_instagram'];
 			return $args;
-		}
-
-		/**
-		 * Enqueue the stylesheet file for instrgram filters
-		 *
-		 * @param $foogallery FooGallery
-		 */
-		function enqueue_instagram_dependencies( $foogallery ) {
-			if ( $foogallery ) {
-				$filter = foogallery_gallery_template_setting( 'instagram', '' );
-
-				if ( '' !== $filter ) {
-					$css = FOOGALLERY_PRO_URL . 'css/instagram.min.css';
-					foogallery_enqueue_style( "foogallery-instagram", $css, array(), FOOGALLERY_VERSION );
-				}
-			}
 		}
 
 		/**
@@ -70,54 +52,23 @@ if ( ! class_exists( 'FooGallery_Pro_Instagram_Filters' ) ) {
 				'choices'  => apply_filters(
 					'foogallery_gallery_template_instagram_choices', array(
 						''         => __( 'None', 'foogallery' ),
-						'filter-1977'   => __( '1977', 'foogallery' ),
-						//'filter-aden' => __( 'Aden', 'foogallery' ),
-						'filter-amaro' => __( 'Amaro', 'foogallery' ),
-
-						'filter-ashby' => __( 'Ashby', 'foogallery' ),
-						'filter-brannan' => __( 'Brannan', 'foogallery' ),
-						//'filter-brooklyn' => __( 'Brooklyn', 'foogallery' ),
-						//'filter-charmes' => __( 'Charmes', 'foogallery' ),
-
-						'filter-clarendon' => __( 'Clarendon', 'foogallery' ),
-						//'filter-crema' => __( 'Crema', 'foogallery' ),
-						'filter-dogpatch' => __( 'Dogpatch', 'foogallery' ),
-						'filter-earlybird' => __( 'Earlybird', 'foogallery' ),
-
-						'filter-gingham' => __( 'Gingham', 'foogallery' ),
-						//'filter-ginza' => __( 'Ginza', 'foogallery' ),
-						//'filter-hefe' => __( 'Hefe', 'foogallery' ),
-						'filter-helena' => __( 'Helena', 'foogallery' ),
-
-						//'filter-hudson' => __( 'Hudson', 'foogallery' ),
-						//'filter-inkwell' => __( 'Inkwell', 'foogallery' ),
-						//'filter-kelvin' => __( 'Kelvin', 'foogallery' ),
-						'filter-juno' => __( 'Juno', 'foogallery' ),
-
-						//'filter-lark' => __( 'Lark', 'foogallery' ),
-						'filter-lofi' => __( 'Lo-Fi', 'foogallery' ),
-						//'filter-kelvin' => __( 'Kelvin', 'foogallery' ),
-						'filter-juno' => __( 'Juno', 'foogallery' ),
-
-						//'filter-ludwig' => __( 'Ludwig', 'foogallery' ),
-						'filter-maven' => __( 'Maven', 'foogallery' ),
-						//'filter-mayfair' => __( 'Mayfair', 'foogallery' ),
-						'filter-moon' => __( 'Moon', 'foogallery' ),
-
-						//'filter-nashville' => __( 'Nashville', 'foogallery' ),
-						//'filter-perpetua' => __( 'Perpetua', 'foogallery' ),
-						'filter-poprocket' => __( 'Poprocket', 'foogallery' ),
-						'filter-reyes' => __( 'Reyes', 'foogallery' ),
-
-						//'filter-vesper' => __( 'Vesper', 'foogallery' ),
-						//'filter-walden' => __( 'Walden', 'foogallery' ),
-						'filter-willow' => __( 'Willow', 'foogallery' ),
-						'filter-xpro-ii' => __( 'X-Pro II', 'foogallery' ),
+						'fg-filter-1977'   => __( '1977', 'foogallery' ),
+						'fg-filter-amaro' => __( 'Amaro', 'foogallery' ),
+						'fg-filter-brannan' => __( 'Brannan', 'foogallery' ),
+						'fg-filter-clarendon' => __( 'Clarendon', 'foogallery' ),
+						'fg-filter-earlybird' => __( 'Earlybird', 'foogallery' ),
+						'fg-filter-lofi' => __( 'Lo-Fi', 'foogallery' ),
+						'fg-filter-poprocket' => __( 'PopRocket', 'foogallery' ),
+						'fg-filter-reyes' => __( 'Reyes', 'foogallery' ),
+						'fg-filter-toaster' => __( 'Toaster', 'foogallery' ),
+						'fg-filter-walden' => __( 'Walden', 'foogallery' ),
+						'fg-filter-xpro2' => __( 'X-Pro 2', 'foogallery' ),
+						'fg-filter-xtreme' => __( 'Xtreme', 'foogallery' ),
 					)
 				),
 				'row_data' => array(
 					'data-foogallery-change-selector'       => 'select',
-					'data-foogallery-preview'               => 'shortcode'
+					'data-foogallery-preview'               => 'class'
 				)
 			);
 
@@ -127,26 +78,20 @@ if ( ! class_exists( 'FooGallery_Pro_Instagram_Filters' ) ) {
 		/**
 		 * Adds the instagram filter onto the figure for the attachment
 		 *
-		 * @param $class
-		 * @param $foogallery_attachment
-		 * @param $args
+		 * @param $classes
+		 * @param $foogallery
 		 *
-		 * @return string
+		 * @return array
 		 */
-		function append_instagram_filter( $attr, $args, $foogallery_attachment ) {
-			global $current_foogallery;
+		function append_instagram_filter_class( $classes, $foogallery ) {
 
 			$filter = foogallery_gallery_template_setting( 'instagram', '' );
 
 			if ( $filter !== '' ) {
-				if ( array_key_exists( 'class', $attr ) ) {
-					$attr['class'] .= ' ' . $filter;
-				} else {
-					$attr['class'] = $filter;
-				}
+				$classes[] = $filter;
 			}
 
-			return $attr;
+			return $classes;
 		}
 	}
 }
