a3b47a83d4c68a20c74f2381563009e1af73ca18
Brad Vincent <bradvin@gmail.com>
2019-03-24 15:37:34 +0200

    optimized the settings checks for "Force HTTPS"

--- includes/class-foogallery-force-https.php
+++ includes/class-foogallery-force-https.php
@@ -9,9 +9,19 @@ if ( ! class_exists( 'FooGallery_ForceHttps' ) ) {
 
 		function __construct() {
 			add_filter( 'foogallery_admin_settings_override', array($this, 'add_settings' ) );
-			add_filter( 'foogallery_attachment_resize_thumbnail', array( $this, 'force_thumbnail_to_https' ), 99, 3 );
-			add_filter( 'foogallery_enqueue_style_src', array( $this, 'force_css_to_https'), 99, 2 );
-			add_filter( 'foogallery_core_gallery_script', array( $this, 'force_js_to_https'), 99 );
+			add_action( 'plugins_loaded', array( $this, 'enable_force' ) );
+		}
+
+		/**
+		 * Enables the force checks based on the setting. This is done here so that the setting is only checked once per page load, rather than multiple times
+		 *
+		 */
+		function enable_force() {
+			if ( 'on' === foogallery_get_setting( 'force_https' ) ) {
+				add_filter( 'foogallery_attachment_resize_thumbnail', array( $this, 'force_thumbnail_to_https' ), 99, 3 );
+				add_filter( 'foogallery_enqueue_style_src', array( $this, 'force_css_to_https'), 99, 2 );
+				add_filter( 'foogallery_core_gallery_script', array( $this, 'force_js_to_https'), 99 );
+			}
 		}
 
 		/**
@@ -52,9 +62,7 @@ if ( ! class_exists( 'FooGallery_ForceHttps' ) ) {
 		 * @return string
 		 */
 		function force_thumbnail_to_https( $original_image_src, $args, $thumbnail_object ) {
-			if ( 'on' === foogallery_get_setting( 'force_https' ) ) {
-				$original_image_src = $this->force_https( $original_image_src );
-			}
+			$original_image_src = $this->force_https( $original_image_src );
 			return $original_image_src;
 		}
 
@@ -67,9 +75,7 @@ if ( ! class_exists( 'FooGallery_ForceHttps' ) ) {
 		 * @return string
 		 */
 		function force_css_to_https( $src, $handle ) {
-			if ( 'on' === foogallery_get_setting( 'force_https' ) ) {
-				$src = $this->force_https( $src );
-			}
+			$src = $this->force_https( $src );
 			return $src;
 		}
 
@@ -81,9 +87,7 @@ if ( ! class_exists( 'FooGallery_ForceHttps' ) ) {
 		 * @return string
 		 */
 		function force_js_to_https( $src ) {
-			if ( 'on' === foogallery_get_setting( 'force_https' ) ) {
-				$src = $this->force_https( $src );
-			}
+			$src = $this->force_https( $src );
 			return $src;
 		}
 	}
