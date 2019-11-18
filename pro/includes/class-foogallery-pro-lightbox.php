e9a912fba10e6c88a45ad97aca8444e4937942bd
bradvin <bradvin@gmail.com>
2019-11-18 20:07:22 +0200

    added a lot of lightbox fields

--- pro/includes/class-foogallery-pro-lightbox.php
+++ pro/includes/class-foogallery-pro-lightbox.php
@@ -118,19 +118,21 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 			);
 
 			$field[] = array(
-				'id'      => 'lightbox_info',
-				'title'   => __( 'Show Caption', 'foogallery' ),
-				'desc'    => __( 'You can show or hide captions within the lightbox.', 'foogallery' ),
+				'id'      => 'lightbox_transition',
+				'title'   => __( 'Transition', 'foogallery' ),
+				'desc'    => __( 'The transition to apply to the main content area when switching between items.', 'foogallery' ),
 				'section' => $section,
 				'spacer'  => '<span class="spacer"></span>',
 				'type'    => 'radio',
-				'default' => 'yes',
-				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_info_choices', array(
-					'yes'  => __( 'Visible', 'foogallery' ),
-					'no'  => __( 'Hidden', 'foogallery' ),
+				'default' => 'fade',
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_button_highlight_choices', array(
+					'fade'       => __( 'Fade', 'foogallery' ),
+					'horizontal' => __( 'Horizontal', 'foogallery' ),
+					'vertical'   => __( 'Vertical', 'foogallery' ),
+					'none'       => __( 'None', 'foogallery' )
 				) ),
 				'row_data'=> array(
-					'data-foogallery-change-selector' => 'input:radio',
+					'data-foogallery-change-selector' => 'input',
 					'data-foogallery-preview' => 'shortcode',
 					'data-foogallery-value-selector' => 'input:checked',
 				)
@@ -144,20 +146,17 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 				'spacer'  => '<span class="spacer"></span>',
 				'type'    => 'radio',
 				'default' => 'bottom',
-				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_info_choices', array(
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_info_position_choices', array(
 					'bottom' => __( 'Bottom', 'foogallery' ),
 					'top'    => __( 'Top', 'foogallery' ),
 					'left'   => __( 'Left', 'foogallery' ),
 					'right'  => __( 'Right', 'foogallery' ),
+					'none'  => __( 'Hidden', 'foogallery' ),
 				) ),
 				'row_data'=> array(
-					'data-foogallery-hidden'                   => true,
-					'data-foogallery-show-when-field'          => 'lightbox_info',
-					'data-foogallery-show-when-field-operator' => 'indexOf',
-					'data-foogallery-show-when-field-value'    => 'yes',
-					'data-foogallery-change-selector'          => 'input:radio',
-					'data-foogallery-preview'                  => 'shortcode',
-					'data-foogallery-value-selector'           => 'input:checked',
+					'data-foogallery-change-selector' => 'input:radio',
+					'data-foogallery-preview'         => 'shortcode',
+					'data-foogallery-value-selector'  => 'input:checked'
 				)
 			);
 
@@ -175,9 +174,9 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 				) ),
 				'row_data'=> array(
 					'data-foogallery-hidden'                   => true,
-					'data-foogallery-show-when-field'          => 'lightbox_info',
-					'data-foogallery-show-when-field-operator' => 'indexOf',
-					'data-foogallery-show-when-field-value'    => 'yes',
+					'data-foogallery-show-when-field'          => 'lightbox_info_position',
+					'data-foogallery-show-when-field-operator' => '!==',
+					'data-foogallery-show-when-field-value'    => 'none',
 					'data-foogallery-change-selector'          => 'input:radio',
 					'data-foogallery-preview'                  => 'shortcode',
 					'data-foogallery-value-selector'           => 'input:checked',
@@ -187,15 +186,18 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 			if ( $use_lightbox ) {
 				$field[] = array(
 					'id'       => 'lightbox_thumbs',
-					'title'    => __( 'Show Thumbs', 'foogallery' ),
+					'title'    => __( 'Thumbnail Strip', 'foogallery' ),
 					'desc'     => __( 'You can enable or disable thumbnails within the lightbox.', 'foogallery' ),
 					'section'  => $section,
 					'spacer'   => '<span class="spacer"></span>',
 					'type'     => 'radio',
-					'default'  => 'enabled',
+					'default'  => 'bottom',
 					'choices'  => apply_filters( 'foogallery_gallery_template_lightbox_thumbs_choices', array(
-						'enabled'  => __( 'Enabled', 'foogallery' ),
-						'disabled' => __( 'Disabled', 'foogallery' )
+						'bottom' => __( 'Bottom', 'foogallery' ),
+						'top'    => __( 'Top', 'foogallery' ),
+						'left'   => __( 'Left', 'foogallery' ),
+						'right'  => __( 'Right', 'foogallery' ),
+						'none'  => __( 'Hidden', 'foogallery' ),
 					) ),
 					'row_data' => array(
 						'data-foogallery-change-selector' => 'input',
@@ -203,7 +205,146 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 						'data-foogallery-value-selector'  => 'input:checked',
 					)
 				);
+
+				$field[] = array(
+					'id'      => 'lightbox_thumbs_captions',
+					'title'   => __( 'Thumbnail Strip Captions', 'foogallery' ),
+					'desc'    => __( 'Whether or not the thumbnail strip should contain captions.', 'foogallery' ),
+					'section' => $section,
+					'spacer'  => '<span class="spacer"></span>',
+					'type'    => 'radio',
+					'default' => 'no',
+					'choices' => apply_filters( 'foogallery_gallery_template_lightbox_thumbs_captions_choices', array(
+						'yes' => __( 'Show Captions', 'foogallery' ),
+						'no'  => __( 'No Captions', 'foogallery' ),
+					) ),
+					'row_data'=> array(
+						'data-foogallery-hidden'                   => true,
+						'data-foogallery-show-when-field'          => 'lightbox_thumbs',
+						'data-foogallery-show-when-field-operator' => '!==',
+						'data-foogallery-show-when-field-value'    => 'none',
+						'data-foogallery-change-selector'          => 'input:radio',
+						'data-foogallery-preview'                  => 'shortcode',
+						'data-foogallery-value-selector'           => 'input:checked',
+					)
+				);
 			}
+
+			$field[] = array(
+				'id'      => 'lightbox_hover_buttons',
+				'title'   => __( 'Show Buttons On Hover', 'foogallery' ),
+				'desc'    => __( 'Only show the buttons when you hover the mouse over.', 'foogallery' ),
+				'section' => $section,
+				'spacer'  => '<span class="spacer"></span>',
+				'type'    => 'radio',
+				'default' => 'no',
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_hover_buttons_choices', array(
+					'yes' => __( 'Yes', 'foogallery' ),
+					'no'  => __( 'No', 'foogallery' ),
+				) ),
+				'row_data'=> array(
+					'data-foogallery-change-selector'          => 'input:radio',
+					'data-foogallery-preview'                  => 'shortcode',
+					'data-foogallery-value-selector'           => 'input:checked',
+				)
+			);
+
+			$field[] = array(
+				'id'      => 'lightbox_auto_progress',
+				'title'   => __( 'Auto Progress', 'foogallery' ),
+				'desc'    => __( 'Auto progress to the next item after a specified time.', 'foogallery' ),
+				'section' => $section,
+				'spacer'  => '<span class="spacer"></span>',
+				'type'    => 'radio',
+				'default' => 'no',
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_auto_progress_choices', array(
+					'yes' => __( 'Yes', 'foogallery' ),
+					'no'  => __( 'No', 'foogallery' ),
+				) ),
+				'row_data'=> array(
+					'data-foogallery-change-selector'          => 'input:radio',
+					'data-foogallery-preview'                  => 'shortcode',
+					'data-foogallery-value-selector'           => 'input:checked',
+				)
+			);
+
+			$field[] = array(
+				'id'      => 'lightbox_auto_progress_seconds',
+				'title'   => __( 'Auto Progress Seconds', 'foogallery' ),
+				'desc'    => __( 'The time in seconds to display content before auto progressing to the next item.', 'foogallery' ),
+				'section' => $section,
+				'type'    => 'number',
+				'default' => '10',
+				'row_data'=> array(
+					'data-foogallery-hidden'                   => true,
+					'data-foogallery-show-when-field'          => 'lightbox_auto_progress',
+					'data-foogallery-show-when-field-operator' => '===',
+					'data-foogallery-show-when-field-value'    => 'yes',
+					'data-foogallery-change-selector'          => 'input:radio',
+					'data-foogallery-preview'                  => 'shortcode',
+					'data-foogallery-value-selector'           => 'input:checked',
+				)
+			);
+
+			$field[] = array(
+				'id'      => 'lightbox_fit_media',
+				'title'   => __( 'Fit Media', 'foogallery' ),
+				'desc'    => __( 'Whether or not to force images to fill the content area. Aspect ratios are maintained, the image is simply scaled so it covers the entire available area.', 'foogallery' ),
+				'section' => $section,
+				'spacer'  => '<span class="spacer"></span>',
+				'type'    => 'radio',
+				'default' => 'no',
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_fit_media_choices', array(
+					'yes' => __( 'Yes', 'foogallery' ),
+					'no'  => __( 'No', 'foogallery' ),
+				) ),
+				'row_data'=> array(
+					'data-foogallery-change-selector'          => 'input:radio',
+					'data-foogallery-preview'                  => 'shortcode',
+					'data-foogallery-value-selector'           => 'input:checked',
+				)
+			);
+
+			$field[] = array(
+				'id'      => 'lightbox_no_scrollbars',
+				'title'   => __( 'Scroll Bars', 'foogallery' ),
+				'desc'    => __( 'Whether or not to hide the page scrollbars when maximizing.', 'foogallery' ),
+				'section' => $section,
+				'spacer'  => '<span class="spacer"></span>',
+				'type'    => 'radio',
+				'default' => 'no',
+				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_no_scrollbars_choices', array(
+					'yes' => __( 'Default', 'foogallery' ),
+					'no'  => __( 'Hidden', 'foogallery' ),
+				) ),
+				'row_data'=> array(
+					'data-foogallery-change-selector'          => 'input:radio',
+					'data-foogallery-preview'                  => 'shortcode',
+					'data-foogallery-value-selector'           => 'input:checked',
+				)
+			);
+
+//			$field[] = array(
+//				'id'      => 'lightbox_buttons',
+//				'title'   => __( 'Buttons', 'foogallery' ),
+//				'desc'    => __( 'Which buttons should be shown.', 'foogallery' ),
+//				'section' => $section,
+//				'type'    => 'checkboxlist',
+//				'default' => 'no',
+//				'choices' => apply_filters( 'foogallery_gallery_template_lightbox_thumbs_captions_choices', array(
+//					'close' => __( 'Close', 'foogallery' ),
+//					'no'  => __( 'No Captions', 'foogallery' ),
+//				) ),
+//				'row_data'=> array(
+//					'data-foogallery-hidden'                   => true,
+//					'data-foogallery-show-when-field'          => 'lightbox_thumbs',
+//					'data-foogallery-show-when-field-operator' => '!==',
+//					'data-foogallery-show-when-field-value'    => 'none',
+//					'data-foogallery-change-selector'          => 'input:radio',
+//					'data-foogallery-preview'                  => 'shortcode',
+//					'data-foogallery-value-selector'           => 'input:checked',
+//				)
+//			);
 
 			//find the index of the first Hover Effect field
 			$index = $this->find_index_of_section( $fields, __( 'Hover Effects', 'foogallery' ) );
@@ -275,6 +416,42 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 
 			if ( array_key_exists( $template . '_lightbox_thumbs', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
 				$args['lightbox_thumbs'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_thumbs'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_thumbs_captions', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_thumbs_captions'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_thumbs_captions'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_info_position', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_info_position'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_info_position'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_info_overlay', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_info_overlay'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_info_overlay'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_hover_buttons', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_hover_buttons'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_hover_buttons'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_transition', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_transition'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_transition'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_auto_progress', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_auto_progress'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_auto_progress'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_auto_progress_seconds', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_auto_progress_seconds'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_auto_progress_seconds'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_fit_media', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_fit_media'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_fit_media'];
+			}
+
+			if ( array_key_exists( $template . '_lightbox_no_scrollbars', $post_data[FOOGALLERY_META_SETTINGS] ) ) {
+				$args['lightbox_no_scrollbars'] = $post_data[FOOGALLERY_META_SETTINGS][$template . '_lightbox_no_scrollbars'];
 			}
 
 			return $args;
@@ -355,6 +532,8 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 		private function get_options_from_settings() {
 			$options = array();
 
+			//$options['hoverButtons'] = true;
+
 			$theme = foogallery_gallery_template_setting( 'lightbox_theme', '' );
 			if ( !empty( $theme ) ) {
 				$options['theme'] = $theme;
@@ -370,14 +549,29 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 				$options['highlight'] = $button_highlight;
 			}
 
-			$show_thumbs = foogallery_gallery_template_setting( 'lightbox_thumbs', 'enabled' ) === 'enabled';
-			$options['thumbsVisible'] = $show_thumbs;
-			$options['thumbs'] = 'bottom';
-			//$options['thumbsSmall'] = true;
-			$options['thumbsCaptions'] = false;
+			$thumbs = foogallery_gallery_template_setting( 'lightbox_thumbs', 'bottom' );
+			$options['thumbs'] = $thumbs;
+			if ( 'none' !== $thumbs ) {
+				$options['thumbsCaptions'] = foogallery_gallery_template_setting( 'lightbox_thumbs_captions', 'no' ) === 'yes';
+			}
 
-			$show_captions = foogallery_gallery_template_setting( 'lightbox_info', 'yes' ) === 'yes';
+			$info_position = foogallery_gallery_template_setting( 'lightbox_info_position', 'botton' );
+			$options['info'] = $info_position;
+			if ( 'none' !== $info_position ) {
+				$options['infoVisible'] = true;
+				$options['infoOverlay'] = foogallery_gallery_template_setting( 'lightbox_info_overlay', 'yes' ) === 'yes';
+			}
 
+			$options['transition'] = foogallery_gallery_template_setting( 'lightbox_transition', 'fade' );
+
+			$auto_progress = foogallery_gallery_template_setting( 'lightbox_auto_progress', 'no' ) === 'yes';
+			if ( $auto_progress ) {
+				$options['autoProgress'] = intval( foogallery_gallery_template_setting( 'lightbox_auto_progress_seconds', '10' ) );
+			}
+
+			$options['hoverButtons'] = foogallery_gallery_template_setting( 'lightbox_hover_buttons', 'no' ) === 'yes';
+			$options['fitMedia'] = foogallery_gallery_template_setting( 'lightbox_fit_media', 'no' ) === 'yes';
+			$options['noScrollbars'] = foogallery_gallery_template_setting( 'lightbox_no_scrollbars', 'no' ) !== 'yes';
 
 			return $options;
 		}
