57f89d585d7ac1fc5f46effc3772fd4b4170b45c
bradvin <bradvin@gmail.com>
2020-03-08 10:03:46 +0000

    slider pro - updated defaults from old layout

--- pro/extensions/default-templates/slider/class-slider-gallery-template.php
+++ pro/extensions/default-templates/slider/class-slider-gallery-template.php
@@ -19,7 +19,7 @@ if ( !class_exists( 'FooGallery_Slider_Gallery_Template' ) ) {
 			add_filter( 'foogallery_override_gallery_template_fields-slider', array( $this, 'change_common_thumbnail_fields' ), 10, 2 );
 
 			//add the data options needed for polaroid
-			add_filter( 'foogallery_build_container_data_options-slider', array( $this, 'add_data_options' ), 10, 3 );
+			add_filter( 'foogallery_build_container_data_options-slider', array( $this, 'add_data_options' ), 20, 3 );
 
 			//override specific settings when saving the gallery
 			add_filter( 'foogallery_save_gallery_settings-slider', array( $this, 'override_settings'), 10, 3 );
@@ -391,10 +391,17 @@ if ( !class_exists( 'FooGallery_Slider_Gallery_Template' ) ) {
 		function add_data_options($options, $gallery, $attributes) {
 
 			$old_layout = foogallery_gallery_template_setting( 'layout', false );
-			if ( 'fgs-horizontal' === $old_layout ) {
-				$options['template']['thumbs'] = 'bottom';
-			} else if ( '' === $old_layout ) {
-				$options['template']['thumbs'] = 'right';
+			if ( $old_layout !== false ) {
+				//we are dealing with an older version of the slider PRO, so we need to set some defaults
+				$options['template']['fitMedia'] = true;
+				$options['template']['preserveButtonSpace'] = false;
+				$options['template']['hoverButtons'] = true;
+
+				if ( 'fgs-horizontal' === $old_layout ) {
+					$options['template']['thumbs'] = 'bottom';
+				} else if ( '' === $old_layout ) {
+					$options['template']['thumbs'] = 'right';
+				}
 			}
 
 			$old_thumbnail_captions = foogallery_gallery_template_setting( 'thumbnail_captions', false );
