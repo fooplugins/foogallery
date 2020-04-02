54078a53c60aa0ffaa50c16b808cb64e1c2f0049
bradvin <bradvin@gmail.com>
2020-04-02 12:49:09 +0100

    added exclude featured image setting to thumbnail gallery (only works with PRO lightbox)

--- extensions/default-templates/thumbnail/class-thumbnail-gallery-template.php
+++ extensions/default-templates/thumbnail/class-thumbnail-gallery-template.php
@@ -115,18 +115,6 @@ if ( !class_exists( 'FooGallery_Thumbnail_Gallery_Template' ) ) {
                         'type'    => 'checkbox',
                         'desc'	  => __( 'You can link your thumbnails to Custom URL\'s (if they are set on your attachments). Fallback will be to the full size image.', 'foogallery' )
                     ),
-//	                array(
-//		                'id'      => 'exclude_featured_image',
-//		                'title'   => __( 'Exclude Featured Image', 'foogallery' ),
-//		                'section' => __( 'General', 'foogallery' ),
-//		                'default' => '',
-//		                'type'    => 'checkbox',
-//		                'desc'	  => __( 'You can exclude the featured image from the images shown in the lightbox.', 'foogallery' ),
-//		                'row_data'=> array(
-//			                'data-foogallery-change-selector' => 'input',
-//			                'data-foogallery-preview' => 'shortcode'
-//		                )
-//	                ),
                     array(
                         'id'      => 'lightbox',
                         'title'   => __( 'Lightbox', 'foogallery' ),
@@ -134,7 +122,29 @@ if ( !class_exists( 'FooGallery_Thumbnail_Gallery_Template' ) ) {
                         'desc'    => __( 'Choose which lightbox you want to use.', 'foogallery' ),
                         'type'    => 'lightbox',
                         'default' => 'none',
+                        'row_data'=> array(
+	                        'data-foogallery-change-selector' => 'select',
+	                        'data-foogallery-preview'         => 'shortcode',
+                            'data-foogallery-value-selector'  => 'select'
+                        )
                     ),
+	                array(
+		                'id'      => 'exclude_featured_image',
+		                'title'   => __( 'Exclude Featured Image', 'foogallery' ),
+		                'section' => __( 'General', 'foogallery' ),
+		                'default' => '',
+		                'type'    => 'checkbox',
+		                'desc'	  => __( 'You can exclude the featured image from the images shown in the lightbox.', 'foogallery' ),
+		                'row_data'=> array(
+			                'data-foogallery-hidden'                   => true,
+			                'data-foogallery-show-when-field'          => 'lightbox',
+			                'data-foogallery-show-when-field-operator' => '===',
+			                'data-foogallery-show-when-field-value'    => 'foogallery',
+			                'data-foogallery-change-selector'          => 'input',
+			                'data-foogallery-preview'                  => 'shortcode',
+			                'data-foogallery-value-selector'           => 'input:checked',
+		                )
+	                ),
                     array(
                         'id'      => 'caption_title',
                         'title'   => __('Override Title', 'foogallery'),
@@ -183,9 +193,9 @@ if ( !class_exists( 'FooGallery_Thumbnail_Gallery_Template' ) ) {
 			$args['thumbnail_dimensions'] = $post_data[FOOGALLERY_META_SETTINGS]['thumbnail_thumbnail_dimensions'];
 			$args['caption_title'] = $post_data[FOOGALLERY_META_SETTINGS]['thumbnail_caption_title'];
 			$args['caption_description'] = $post_data[FOOGALLERY_META_SETTINGS]['thumbnail_caption_description'];
-//			if ( isset( $post_data[FOOGALLERY_META_SETTINGS]['thumbnail_exclude_featured_image'] ) ) {
-//				$args['exclude_featured_image'] = $post_data[ FOOGALLERY_META_SETTINGS ]['thumbnail_exclude_featured_image'];
-//			}
+			if ( isset( $post_data[FOOGALLERY_META_SETTINGS]['thumbnail_exclude_featured_image'] ) ) {
+				$args['exclude_featured_image'] = $post_data[ FOOGALLERY_META_SETTINGS ]['thumbnail_exclude_featured_image'];
+			}
 			return $args;
 		}
 
