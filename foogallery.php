13341df96342b594a893d088d5fa992b7355d032
bradvin <bradvin@gmail.com>
2020-04-06 19:46:19 +0100

    fixed bug for not including promos

--- foogallery.php
+++ foogallery.php
@@ -188,8 +188,11 @@ if ( function_exists( 'foogallery_fs' ) ) {
 				if ( !$pro_code_included ) {
 					add_filter( 'foogallery_extensions_for_view', array( $this, 'add_foogallery_pro_extension' ) );
 
-					//include PRO promotion
-					new FooGallery_Pro_Promotion();
+					//only include if in admin
+					if ( is_admin() ) {
+						//include PRO promotion
+						new FooGallery_Pro_Promotion();
+					}
 				}
 
 				//init Gutenberg!
