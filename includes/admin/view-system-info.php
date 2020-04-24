b4228bfed734579a346be8cb33df9f09c77d2569
bradvin <bradvin@gmail.com>
2020-04-24 12:06:37 +0100

    added detected image editor to system info

--- includes/admin/view-system-info.php
+++ includes/admin/view-system-info.php
@@ -66,6 +66,8 @@ if ( current_user_can( 'activate_plugins' ) ) {
 	$test_image_url_scheme = parse_url( $test_image_url ,PHP_URL_SCHEME );
 	$home_url_scheme = parse_url( home_url() ,PHP_URL_SCHEME );
 
+	$image_editor = _wp_image_editor_choose( array( 'methods' => array( 'get_image' ) ) );
+
 	$debug_info = array(
 		__( 'FooGallery version', 'foogallery' )  			=> $info['version'],
 		__( 'WordPress version', 'foogallery' )   			=> $wp_version,
@@ -79,6 +81,7 @@ if ( current_user_can( 'activate_plugins' ) ) {
 		__( 'HTTPS Mismatch', 'foogallery' )      			=> $test_image_url_scheme === $home_url_scheme ? __( 'None', 'foogallery' ) : __( 'There is a protocol mismatch between your site URL and the actual URL!', 'foogallery' ),
 		__( 'PHP Config[allow_url_fopen]', 'foogallery' ) 	=> ini_get( 'allow_url_fopen' ),
 		__( 'PHP Config[allow_url_include]', 'foogallery' ) => ini_get( 'allow_url_fopen' ),
+		__( 'WP Image Editor', 'foogallery' )               => $image_editor,
 		__( 'Thumbnail Generation Test', 'foogallery') 		=> $test_image_url,
 		__( 'Extensions Endpoint', 'foogallery' ) 			=> $api->get_extensions_endpoint(),
 		__( 'Extensions Errors', 'foogallery' )   			=> $api->has_extension_loading_errors() == true ? $api->get_extension_loading_errors_response() : __( 'Nope, all good', 'foogallery' ),
