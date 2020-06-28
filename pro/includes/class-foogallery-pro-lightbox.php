086c97e8e9cd9971e4c761b6f55fd376a2606df3
bradvin <bradvin@gmail.com>
2020-06-28 16:57:53 +0100

    added json support for lightbox captions

--- pro/includes/class-foogallery-pro-lightbox.php
+++ pro/includes/class-foogallery-pro-lightbox.php
@@ -36,7 +36,22 @@ if ( ! class_exists( 'FooGallery_Pro_Lightbox' ) ) {
 
 			//cater for different captions sources
 			add_filter( 'foogallery_attachment_html_link_attributes', array( $this, 'add_caption_attributes' ), 10, 3 );
+
+			add_filter( 'foogallery_build_attachment_json', array( $this, 'add_captions_to_json' ), 10, 6 );
 		}
+
+		function add_captions_to_json( $json_object, $foogallery_attachment, $args, $anchor_attributes, $image_attributes, $captions ) {
+			if ( array_key_exists('data-lightbox-title', $anchor_attributes ) ) {
+				$json_object->attr->anchor->{'data-lightbox-title'} = $anchor_attributes['data-lightbox-title'];
+			}
+
+			if ( array_key_exists('data-lightbox-description', $anchor_attributes ) ) {
+				$json_object->attr->anchor->{'data-lightbox-description'} = $anchor_attributes['data-lightbox-description'];
+			}
+
+			return $json_object;
+		}
+
 
 		/**
 		 * Handle custom captions for the lightbox
