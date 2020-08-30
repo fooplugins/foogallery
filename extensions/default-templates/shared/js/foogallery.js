5117f18f0769b57b91ea35a7a32980424163da64
bradvin <bradvin@gmail.com>
2020-08-30 16:42:41 +0100

    updated client side to 1.4.17

--- extensions/default-templates/shared/js/foogallery.js
+++ extensions/default-templates/shared/js/foogallery.js
@@ -5824,11 +5824,13 @@
 			var current = element.data(_.DATA_TEMPLATE);
 			if (current instanceof _.Template) {
 				return current.destroy(true).then(function(){
-					return _.template.make(options, element).initialize();
+					var tmpl = _.template.make(options, element);
+					return tmpl instanceof _.Template ? tmpl.initialize() : _fn.rejected;
 				});
 			}
 		}
-		return _.template.make(options, element).initialize();
+		var tmpl = _.template.make(options, element);
+		return tmpl instanceof _.Template ? tmpl.initialize() : _fn.rejected;
 	};
 
 	_.initAll = function (options) {
@@ -6362,8 +6364,8 @@
 		},
 		type: function (options, element) {
 			element = _is.jq(element) ? element : $(element);
-			var self = this, type = _is.hash(options) && _is.hash(options) && _is.string(options.type) && self.contains(options.type) ? options.type : "core";
-			if (type === "core" && element.length > 0) {
+			var self = this, type = _is.hash(options) && _is.hash(options) && _is.string(options.type) && self.contains(options.type) ? options.type : null;
+			if (type !== null && element.length > 0) {
 				var reg = self.registered, names = self.names(true);
 				for (var i = 0, l = names.length; i < l; i++) {
 					if (!reg.hasOwnProperty(names[i])) continue;
@@ -11034,7 +11036,7 @@
 			}
 			self.fullWidth = maxWidth < width;
 			if (self.fullWidth){
-				rule = item + ' { max-width: 100%; margin: ' + gutter + 'px; }';
+				rule = item + ' { max-width: 100%; min-width: 100%; margin: ' + gutter + 'px; }';
 				sheet.insertRule(rule , 0);
 			} else {
 				rule = item + ' { max-width: ' + width + 'px; min-width: ' + width + 'px; margin: ' + gutter + 'px; }';
