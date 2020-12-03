370e5f33efb65e86ef70dc4b3f88f4eb3865f4c4
bradvin <bradvin@gmail.com>
2020-12-03 17:31:55 +0000

    fixes for album rendering

--- extensions/albums/album-stack.php
+++ extensions/albums/album-stack.php
@@ -10,6 +10,9 @@ global $current_foogallery;
 $args = foogallery_album_template_setting( 'thumbnail_dimensions', array() );
 $args['crop'] = true;
 $lightbox = foogallery_album_template_setting( 'lightbox', 'unknown' );
+if ( $lightbox === 'foogallery' ) {
+    $lightbox = 'unknown';
+}
 $random_angle = foogallery_album_template_setting( 'random_angle', 'false' );
 $gutter = foogallery_album_template_setting( 'gutter', '40' );
 $pile_angles = foogallery_album_template_setting( 'pile_angles', '2' );
@@ -32,8 +35,15 @@ if ( !function_exists( 'foogallery_album_all_in_one_stack_render_gallery_attachm
 	function foogallery_album_all_in_one_stack_render_gallery_attachment2( $gallery, $attachment, $args ) {
 		echo '<li class="fg-pile-item">';
 		$args['link_attributes']['class'] = apply_filters( 'foogallery_album_stack_link_class_name', 'fg-pile-item-thumb' );
-		echo foogallery_attachment_html_anchor_opening( $attachment, $args );
-		echo foogallery_attachment_html_image( $attachment, $args );
+
+		$anchor_html = foogallery_attachment_html_anchor_opening( $attachment, $args );
+        $anchor_html = str_replace( 'fg-thumb', '', $anchor_html );
+        echo $anchor_html;
+
+		$image_html = foogallery_attachment_html_image( $attachment, $args );
+		$image_html = str_replace( 'fg-image', 'fg-pile-item-image', $image_html );
+		echo $image_html;
+
 		$captions = foogallery_build_attachment_html_caption( $attachment, $args );
 		if ( $captions !== false ) {
 			echo '<span class="fg-pile-item-caption">';
