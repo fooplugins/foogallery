206e0b3b07aa693665bdf910d5fade57bb9405c1
bradvin <bradvin@gmail.com>
2021-01-10 12:56:53 +0000

    update imageviewer to use settings

--- extensions/default-templates/image-viewer/gallery-image-viewer.php
+++ extensions/default-templates/image-viewer/gallery-image-viewer.php
@@ -7,14 +7,19 @@
 global $current_foogallery;
 //the current shortcode args
 global $current_foogallery_arguments;
-//get our thumbnail sizing args
+
+$text_prev_default = foogallery_get_setting( 'language_imageviewer_prev_text', __('Prev', 'foogallery') );
+$text_prev = foogallery_gallery_template_setting( 'text-prev', $text_prev_default );
+
+$text_of_default = foogallery_get_setting( 'language_imageviewer_of_text', __('of', 'foogallery') );
+$text_of = foogallery_gallery_template_setting( 'text-of', $text_of_default );
+
+$text_next_default = foogallery_get_setting( 'language_imageviewer_next_text', __('Next', 'foogallery') );
+$text_next = foogallery_gallery_template_setting( 'text-next', $text_next_default );
 
 //get which lightbox we want to use
 $lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
 $alignment = foogallery_gallery_template_setting( 'alignment', 'fg-center' );
-$text_prev = foogallery_gallery_template_setting( 'text-prev', __('Prev', 'foogallery') );
-$text_of = foogallery_gallery_template_setting( 'text-of', __('of', 'foogallery') );
-$text_next = foogallery_gallery_template_setting( 'text-next', __('Next', 'foogallery') );
 $link = foogallery_gallery_template_setting( 'thumbnail_link', 'image' );
 $foogallery_imageviewer_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'foogallery-link-' . $link, 'foogallery-lightbox-' . $lightbox, $alignment );
 $foogallery_imageviewer_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_imageviewer_classes ) );
