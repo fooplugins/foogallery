838a6b2ff3e0dac462959694b049f49f2a9d0657
bradvin <bradvin@gmail.com>
2021-01-30 13:10:13 +0000

    added new justified css template

--- extensions/default-templates/class-default-templates.php
+++ extensions/default-templates/class-default-templates.php
@@ -16,6 +16,7 @@ if ( ! class_exists( 'FooGallery_Default_Templates' ) ) {
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'default/class-default-gallery-template.php' );
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'image-viewer/class-image-viewer-gallery-template.php' );
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'justified/class-justified-gallery-template.php' );
+	require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'justified-css/class-justified-css-gallery-template.php' );
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'masonry/class-masonry-gallery-template.php' );
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'simple-portfolio/class-simple-portfolio-gallery-template.php' );
     require_once( FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_PATH . 'thumbnail/class-thumbnail-gallery-template.php' );
@@ -26,6 +27,7 @@ if ( ! class_exists( 'FooGallery_Default_Templates' ) ) {
             new FooGallery_Default_Gallery_Template();
             new FooGallery_Image_Viewer_Gallery_Template();
             new FooGallery_Justified_Gallery_Template();
+	        new FooGallery_Justified_CSS_Gallery_Template();
             new FooGallery_Masonry_Gallery_Template();
             new FooGallery_Simple_Portfolio_Gallery_Template();
             new FooGallery_Thumbnail_Gallery_Template();
---
+++ extensions/default-templates/justified-css/gallery-justified-css.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FooGallery Justified CSS gallery template
+ */
+global $current_foogallery;
+global $current_foogallery_arguments;
+$lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
+$foogallery_justified_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'foogallery-lightbox-' . $lightbox );
+$foogallery_justified_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_justified_classes) );
+?>
+<div <?php echo $foogallery_justified_attributes; ?>>
+	<?php foreach ( foogallery_current_gallery_attachments_for_rendering() as $attachment ) {
+		echo foogallery_attachment_html( $attachment );
+	} ?>
+</div>
