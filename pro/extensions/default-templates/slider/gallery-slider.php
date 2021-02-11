349478edafb747110ff82111430695cb501f01bd
bradvin <bradvin@gmail.com>
2021-02-11 15:11:27 +0000

    updated slider template to use json item script block

--- pro/extensions/default-templates/slider/gallery-slider.php
+++ pro/extensions/default-templates/slider/gallery-slider.php
@@ -9,8 +9,5 @@ global $current_foogallery;
 $foogallery_default_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'slider' );
 $foogallery_default_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_default_classes ) );
 
-?><div <?php echo $foogallery_default_attributes; ?>>
-	<?php foreach ( foogallery_current_gallery_attachments_for_rendering() as $attachment ) {
-		echo foogallery_attachment_html( $attachment );
-	} ?>
-</div>
+?><div <?php echo $foogallery_default_attributes; ?>></div>
+<?php foogallery_render_script_block_for_json_items( $current_foogallery, $current_foogallery->attachments() );
