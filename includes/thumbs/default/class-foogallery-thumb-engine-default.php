33df448982277ae99c7c9df3d00bd141222483de
bradvin <bradvin@gmail.com>
2021-03-02 13:31:06 +0000

    change deleted_post filter to only take a single parameter

--- includes/thumbs/default/class-foogallery-thumb-engine-default.php
+++ includes/thumbs/default/class-foogallery-thumb-engine-default.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'FooGallery_Thumb_Engine_Default' ) ) {
 
 		public function init() {
 			add_filter( 'wp_image_editors', array( $this, 'override_image_editors' ), 999 );
-			add_filter( 'deleted_post', array( $this, 'delete_cache_folder_for_attachment' ), 10, 2 );
+			add_filter( 'deleted_post', array( $this, 'delete_cache_folder_for_attachment' ), 10, 1 );
 			add_action( 'foogallery_admin_menu_after', array( $this, 'add_test_thumb_menu' ) );
 
 			//add background fill functionality
@@ -187,7 +187,7 @@ if ( ! class_exists( 'FooGallery_Thumb_Engine_Default' ) ) {
 		 *
 		 * @return string
 		 */
-		function delete_cache_folder_for_attachment( $post_id, $post ) {
+		function delete_cache_folder_for_attachment( $post_id ) {
 			$url = wp_get_attachment_url( $post_id );
 
 			if ( $url !== false ) {
