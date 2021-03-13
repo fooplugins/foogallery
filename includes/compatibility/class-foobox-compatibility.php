3b8dbd308794035468cf57b94dcf7e6bc11dea11
bradvin <bradvin@gmail.com>
2021-03-13 16:18:43 +0000

    added gallery settings to change lightbox captinos when foobox is selected

--- includes/compatibility/class-foobox-compatibility.php
+++ includes/compatibility/class-foobox-compatibility.php
@@ -40,7 +40,8 @@ if ( !class_exists( 'FooGallery_FooBox_Compatibility' ) ) {
 			//add custom captions
 			add_filter( 'foogallery_build_attachment_html_caption_custom', array( &$this, 'customize_captions' ), 90, 3 );
 
-			add_filter( 'foogallery_override_gallery_template_fields', array( $this, 'add_foobox_help_field' ), 99, 2 );
+			//add fields for FooBox free captions
+			add_filter( 'foogallery_override_gallery_template_fields', array( $this, 'add_caption_fields' ), 20, 2 );
 		}
 
 		/**
@@ -74,84 +75,207 @@ if ( !class_exists( 'FooGallery_FooBox_Compatibility' ) ) {
 		function add_caption_attributes( $attr, $args, $foogallery_attachment ) {
 			global $current_foogallery;
 
-			//check if lightbox set to foogallery
+			$force_same = false;
+
+			//check if lightbox set to foobox
+			//Note that the $current_foogallery->lightbox property is only set if FooGallery PRO is running
 			if ( isset( $current_foogallery->lightbox ) && 'foobox' === $current_foogallery->lightbox ) {
 
-				//check lightbox caption source
-				$source = foogallery_gallery_template_setting( 'lightbox_caption_override', '' );
+				//check lightbox caption source field that is added in FooGallery PRO
+				$lightbox_caption_source = foogallery_gallery_template_setting( 'lightbox_caption_override', false );
 
-				if ( 'override' === $source ) {
-					$caption_title_source = foogallery_gallery_template_setting('lightbox_caption_override_title', '' );
+				if ( 'override' === $lightbox_caption_source ) {
+					$caption_title_source = foogallery_gallery_template_setting( 'lightbox_caption_override_title', '' );
 					if ( 'none' === $caption_title_source ) {
 						$attr['data-caption-title'] = ' ';
 					} else if ( '' !== $caption_title_source ) {
 						$attr['data-caption-title'] = foogallery_sanitize_html( foogallery_get_caption_by_source( $foogallery_attachment, $caption_title_source, 'title' ) );
 					}
 
-					$caption_desc_source = foogallery_gallery_template_setting('lightbox_caption_override_desc', '' );
+					$caption_desc_source = foogallery_gallery_template_setting( 'lightbox_caption_override_desc', '' );
 					if ( 'none' === $caption_desc_source ) {
 						$attr['data-caption-desc'] = ' ';
 					} else if ( '' !== $caption_desc_source ) {
 						$attr['data-caption-desc'] = foogallery_sanitize_html( foogallery_get_caption_by_source( $foogallery_attachment, $caption_desc_source, 'description' ) );
 					}
-				} else if ( 'custom' === $source ) {
+				} else if ( 'custom' === $lightbox_caption_source ) {
 
 					$template = foogallery_gallery_template_setting( 'lightbox_caption_custom_template', '' );
-					if ( !empty( $template ) ) {
+					if ( ! empty( $template ) ) {
 						$attr['data-caption-title'] = ' ';
-						$attr['data-caption-desc'] = foogallery_sanitize_html( FooGallery_Pro_Advanced_Captions::build_custom_caption( $template, $foogallery_attachment ) );
+						$attr['data-caption-desc']  = foogallery_sanitize_html( FooGallery_Pro_Advanced_Captions::build_custom_caption( $template, $foogallery_attachment ) );
 					}
+				} else if ( '' === $lightbox_caption_source ) {
+					//same as thumbnail
+					//either way, we need to force the lightbox captions to match the thumb captions
+					$force_same = true;
+				}
+
+			} else {
+				//we will get here if FooGallery FREE is running
+				$lightbox = foogallery_gallery_template_setting( 'lightbox', '' );
+
+				//we only want to make changes if the lightbox is set to foobox
+				if ( 'foobox' === $lightbox ) {
+					//check foobox caption source field that is only added if FooBox free is installed
+					$foobox_caption_source = foogallery_gallery_template_setting( 'foobox_caption_source', false );
+
+					if ( 'override' === $foobox_caption_source ) {
+						$caption_title_source = foogallery_gallery_template_setting( 'foobox_caption_override_title', '' );
+						if ( 'none' === $caption_title_source ) {
+							$attr['data-caption-title'] = ' ';
+						} else if ( '' !== $caption_title_source ) {
+							$attr['data-caption-title'] = foogallery_sanitize_html( foogallery_get_caption_by_source( $foogallery_attachment, $caption_title_source, 'title' ) );
+						}
+
+						$caption_desc_source = foogallery_gallery_template_setting( 'foobox_caption_override_desc', '' );
+						if ( 'none' === $caption_desc_source ) {
+							$attr['data-caption-desc'] = ' ';
+						} else if ( '' !== $caption_desc_source ) {
+							$attr['data-caption-desc'] = foogallery_sanitize_html( foogallery_get_caption_by_source( $foogallery_attachment, $caption_desc_source, 'description' ) );
+						}
+					} else if ( 'same' === $foobox_caption_source ) {
+						//same as thumbnail, or FooGallery FREE
+						$force_same = true;
+					}
+				}
+			}
+
+			//force the same captions as the thumbnail
+			if ( $force_same ) {
+				if ( isset( $foogallery_attachment->caption_title ) ) {
+					$attr['data-caption-title'] = foogallery_sanitize_html( $foogallery_attachment->caption_title );
+				} else {
+					$attr['data-caption-title'] = ' ';
+				}
+
+				if ( isset( $foogallery_attachment->caption_desc ) ) {
+					$attr['data-caption-desc'] = foogallery_sanitize_html( $foogallery_attachment->caption_desc );
+				} else {
+					$attr['data-caption-desc'] = ' ';
 				}
 			}
 
 			return $attr;
 		}
 
-		function add_foobox_help_field( $fields, $template ) {
+		/**
+		 * Add caption fields for FooBox FREE
+		 *
+		 * @param $fields
+		 * @param $template
+		 *
+		 * @return mixed
+		 */
+		function add_caption_fields( $fields, $template ) {
 			//see if the template has a lightbox field
 			$found_lightbox = false;
-			$position       = 0;
 			foreach ( $fields as $key => &$field ) {
-				$position ++;
 				if ( 'lightbox' === $field['id'] ) {
 					$found_lightbox = true;
 					break;
 				}
 			}
-			if ( $found_lightbox && ! $this->is_foobox_installed() ) {
-				$action      = 'install-plugin';
-				$slug        = 'foobox-image-lightbox';
-				$install_url = wp_nonce_url(
-					add_query_arg(
-						array(
-							'action' => $action,
-							'plugin' => $slug
-						),
-						admin_url( 'update.php' )
-					),
-					$action . '_' . $slug
-				);
 
-				$foobox_help_field = array(
-					array(
-						'id'      => 'lightbox_foobox_help',
-						'title'   => __( 'FooBox Help', 'foogallery' ),
-						'desc'    => sprintf( __( 'FooBox is our separate Lightbox plugin that enables your gallery images to open in a beautiful responsive lightbox. %s', 'foogallery' ), '<a href="' . $install_url . '" target="_blank">' . __( 'Install it now!', 'foogallery' ) . '</a>' ),
-						'section' => __( 'General', 'foogallery' ),
-						'type'    => 'help',
-						'row_data'=> array(
-							'data-foogallery-hidden' => true,
-							'data-foogallery-show-when-field' => 'lightbox',
-							'data-foogallery-show-when-field-value' => 'foobox',
-							'data-foogallery-change-selector' => 'input',
-						)
+			if ( $found_lightbox && $this->is_foobox_installed() && !foogallery_is_pro() ) {
+
+				$new_fields[] = array(
+					'id'      => 'foobox_caption_source',
+					'title'   => __( 'Lightbox Caption Source', 'foogallery' ),
+					'desc'    => __( 'The lightbox captions can be different to the thumbnail captions.', 'foogallery' ),
+					'section' => __( 'Lightbox', 'foogallery' ),
+					'type'    => 'radio',
+					'default' => '',
+					'choices' => array(
+						'' => __('Smart (try to show both caption titles and descriptions if available)', 'foogallery' ),
+						'same' => __( 'Same As Thumbnail', 'foogallery' ),
+						'override'  => __( 'Override', 'foogallery' ),
+					),
+					'row_data'=> array(
+						'data-foogallery-hidden' => true,
+						'data-foogallery-show-when-field' => 'lightbox',
+						'data-foogallery-show-when-field-value' => 'foobox',
+						'data-foogallery-change-selector' => 'input:radio',
+						'data-foogallery-value-selector'  => 'input:checked',
 					)
 				);
 
-				array_splice( $fields, $position, 0, $foobox_help_field );
+				$new_fields[] = array(
+					'id'      => 'foobox_caption_override_title',
+					'title'   => __( 'Override Caption Title', 'foogallery' ),
+					'desc'    => __( 'You can override the caption title to be different from the thumbnail caption title.', 'foogallery' ),
+					'section' => __( 'Lightbox', 'foogallery' ),
+					'type'    => 'radio',
+					'default' => '',
+					'choices' => array(
+						'' => __( 'Same As Thumbnail', 'foogallery' ),
+						'title'  => __( 'Attachment Title', 'foogallery' ),
+						'caption'  => __( 'Attachment Caption', 'foogallery' ),
+						'alt'  => __( 'Attachment Alt', 'foogallery' ),
+						'desc'  => __( 'Attachment Description', 'foogallery' ),
+						'none'  => __( 'None', 'foogallery' ),
+					),
+					'row_data'=> array(
+						'data-foogallery-hidden'                   => true,
+						'data-foogallery-show-when-field'          => 'foobox_caption_source',
+						'data-foogallery-show-when-field-operator' => '===',
+						'data-foogallery-show-when-field-value'    => 'override',
+						'data-foogallery-change-selector'          => 'input:radio',
+						'data-foogallery-value-selector'           => 'input:checked',
+					)
+				);
+
+				$new_fields[] = array(
+					'id'      => 'foobox_caption_override_desc',
+					'title'   => __( 'Override Caption Desc.', 'foogallery' ),
+					'desc'    => __( 'You can override the caption description to be different from the thumbnail caption description.', 'foogallery' ),
+					'section' => __( 'Lightbox', 'foogallery' ),
+					'type'    => 'radio',
+					'default' => '',
+					'choices' => array(
+						'' => __( 'Same As Thumbnail', 'foogallery' ),
+						'title'  => __( 'Attachment Title', 'foogallery' ),
+						'caption'  => __( 'Attachment Caption', 'foogallery' ),
+						'alt'  => __( 'Attachment Alt', 'foogallery' ),
+						'desc'  => __( 'Attachment Description', 'foogallery' ),
+						'none'  => __( 'None', 'foogallery' ),
+					),
+					'row_data'=> array(
+						'data-foogallery-hidden'                   => true,
+						'data-foogallery-show-when-field'          => 'foobox_caption_source',
+						'data-foogallery-show-when-field-operator' => '===',
+						'data-foogallery-show-when-field-value'    => 'override',
+						'data-foogallery-change-selector'          => 'input:radio',
+						'data-foogallery-value-selector'           => 'input:checked',
+					)
+				);
+
+				//find the index of the first Hover Effect field
+				$index = $this->find_index_of_section( $fields, __( 'Hover Effects', 'foogallery' ) );
+
+				array_splice( $fields, $index, 0, $new_fields );
 			}
 
 			return $fields;
+		}
+
+		/**
+		 * Return the index of the requested section
+		 *
+		 * @param $fields
+		 * @param $section
+		 *
+		 * @return int
+		 */
+		private function find_index_of_section( $fields, $section ) {
+			$index = 0;
+			foreach ( $fields as $field ) {
+				if ( isset( $field['section'] ) && $section === $field['section'] ) {
+					return $index;
+				}
+				$index++;
+			}
+			return $index;
 		}
 
         /***
@@ -194,8 +318,16 @@ if ( !class_exists( 'FooGallery_FooBox_Compatibility' ) ) {
         }
 
 		function is_foobox_installed() {
-		    return class_exists( 'FooBox' ) || class_exists( 'fooboxV2' );
+		    return $this->is_foobox_free_installed() || $this->is_foobox_pro_installed();
         }
+
+		function is_foobox_free_installed() {
+			return class_exists( 'FooBox' );
+		}
+
+		function is_foobox_pro_installed() {
+			return class_exists( 'fooboxV2' );
+		}
 
 		function ensure_outdated_foobox_extensions_never_run() {
 			global $foogallery_extensions;
