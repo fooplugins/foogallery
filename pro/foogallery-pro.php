63b31572a14fcddfb59731831615df34c4cb1382
bradvin <bradvin@gmail.com>
2021-03-29 11:53:36 +0100

    moved all loaded effects into free

--- pro/foogallery-pro.php
+++ pro/foogallery-pro.php
@@ -5,7 +5,6 @@
  */
 require_once( FOOGALLERY_PATH . 'pro/functions.php' );
 require_once( FOOGALLERY_PATH . 'pro/includes/class-foogallery-pro-presets.php' );
-require_once( FOOGALLERY_PATH . 'pro/includes/class-foogallery-pro-loaded-effects.php' );
 require_once( FOOGALLERY_PATH . 'pro/includes/class-foogallery-pro-paging.php' );
 require_once( FOOGALLERY_PATH . 'pro/extensions/default-templates/class-foogallery-pro-default-templates.php' );
 require_once( FOOGALLERY_PATH . 'pro/includes/class-foogallery-pro-bulk-copy.php' );
@@ -47,7 +46,6 @@ if ( ! class_exists( 'FooGallery_Pro' ) ) {
 
 		function __construct() {
 			new FooGallery_Pro_Hover_Presets();
-			new FooGallery_Pro_Loaded_Effects();
 			new FooGallery_Pro_Paging();
 			new FooGallery_Pro_Default_Templates();
 			new FooGallery_Pro_Bulk_Copy();
