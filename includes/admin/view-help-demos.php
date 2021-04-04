00cbbd61045a4e1c8bd72946bfbc3a6fdac0b14c
steveush <steveush@gmail.com>
2021-04-04 10:18:54 +0200

    Initial checkin for the demo on the help page.

--- includes/admin/view-help-demos.php
+++ includes/admin/view-help-demos.php
@@ -1,13 +1,278 @@
-<div id="demos_section" class="foogallery-admin-help-section foogallery-admin-help-centered" style="display: none">
-    <section class="fgah-feature">
-        <header>
-            <h3><?php _e( 'FooGallery Demo 😎', 'foogallery' );?></h3>
-            <p><?php _e( 'Demo coming soon...', 'foogallery' );?></p>
-        </header>
-        <div>
-            <div>
-                <!-- Demo Content -->
-            </div>
+<div id="demos_section" class="foogallery-admin-help-section fgah-demo" style="display: none">
+
+    <header>
+        <h3><?php _e( 'FooGallery Demo 😎', 'foogallery' );?></h3>
+        <p><?php _e( 'The below shows our default Responsive gallery template.', 'foogallery' );?></p>
+    </header>
+
+    <div id="foogallery-gallery-0" class="foogallery fg-default fg-center fg-gutter-10 fg-m-col2 fg-loading-default fg-loaded-fade-in fg-light fg-border-medium fg-shadow-outline fg-shadow-inset-small fg-caption-hover fg-hover-fade fg-c-l"
+         data-foogallery="{&quot;lazy&quot;:true}"
+         data-foogallery-lightbox="{&quot;exif&quot;:&quot;auto&quot;}">
+
+
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/1.jpg' ?>"
+                   data-type="image"
+                   data-title="Sunset"
+                   data-description="Amazing sunset seen through the branches of a tree overlooking the ocean."
+                   data-exif="{&quot;camera&quot;:&quot;Nikon D70&quot;,&quot;aperture&quot;:&quot;f/2.0&quot;,&quot;shutter_speed&quot;:&quot;1/697 s&quot;,&quot;focal_length&quot;:&quot;18 mm&quot;,&quot;iso&quot;:&quot;57&quot;,&quot;created_timestamp&quot;:&quot;2/1/2021, 8:53:06 PM&quot;,&quot;orientation&quot;:&quot;Right Side&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/1.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/1@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Sunset"
+                                     alt="Amazing sunset seen through the branches of a tree overlooking the ocean.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Sunset</div>
+                        <div class="fg-caption-desc">Amazing sunset seen through the branches of a tree overlooking the ocean.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
         </div>
-    </section>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/2.jpg' ?>"
+                   data-type="image"
+                   data-title="Autumn"
+                   data-description="Autumn leaves on a country path."
+                   data-exif="{&quot;camera&quot;:&quot;Olympus C8080WZ&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/101 s&quot;,&quot;focal_length&quot;:&quot;29 mm&quot;,&quot;iso&quot;:&quot;52&quot;,&quot;created_timestamp&quot;:&quot;2/4/2021, 8:58:44 PM&quot;,&quot;orientation&quot;:&quot;Bottom&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/2.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/2@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Autumn"
+                                     alt="Autumn leaves on a country path.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Autumn</div>
+                        <div class="fg-caption-desc">Autumn leaves on a country path.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/3.jpg' ?>"
+                   data-type="image"
+                   data-title="Flowers"
+                   data-description="White flowers in the shade of a tree."
+                   data-exif="{&quot;camera&quot;:&quot;Nikon COOLPIX P1&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/66 s&quot;,&quot;focal_length&quot;:&quot;12 mm&quot;,&quot;iso&quot;:&quot;99&quot;,&quot;created_timestamp&quot;:&quot;2/3/2021, 8:18:34 PM&quot;,&quot;orientation&quot;:&quot;Right Side&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/3.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/3@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Flowers"
+                                     alt="White flowers in the shade of a tree.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Flowers</div>
+                        <div class="fg-caption-desc">White flowers in the shade of a tree.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/4.jpg' ?>"
+                   data-type="image"
+                   data-title="Misty Forest"
+                   data-description="Peering through the mist up a hillside in a forest."
+                   data-exif="{&quot;camera&quot;:&quot;Kodak CX7530&quot;,&quot;aperture&quot;:&quot;f/1.0&quot;,&quot;shutter_speed&quot;:&quot;1/897 s&quot;,&quot;focal_length&quot;:&quot;5 mm&quot;,&quot;iso&quot;:&quot;67&quot;,&quot;created_timestamp&quot;:&quot;2/4/2021, 8:57:43 PM&quot;,&quot;orientation&quot;:&quot;Top&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/4.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/4@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Misty Forest"
+                                     alt="Peering through the mist up a hillside in a forest.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Misty Forest</div>
+                        <div class="fg-caption-desc">Peering through the mist up a hillside in a forest.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/5.jpg' ?>"
+                   data-type="image"
+                   data-title="Country Road"
+                   data-description="Shafts of sunlight filtering through the trees over a country road."
+                   data-exif="{&quot;camera&quot;:&quot;Kodak CX7530&quot;,&quot;aperture&quot;:&quot;f/1.0&quot;,&quot;shutter_speed&quot;:&quot;1/897 s&quot;,&quot;focal_length&quot;:&quot;5 mm&quot;,&quot;iso&quot;:&quot;67&quot;,&quot;created_timestamp&quot;:&quot;2/4/2021, 8:57:43 PM&quot;,&quot;orientation&quot;:&quot;Top&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/5.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/5@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Country Road"
+                                     alt="Shafts of sunlight filtering through the trees over a country road.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Country Road</div>
+                        <div class="fg-caption-desc">Shafts of sunlight filtering through the trees over a country road.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/6.jpg' ?>"
+                   data-type="image"
+                   data-title="Stone Path"
+                   data-description="A stone path in the foothills with a mountain in the background."
+                   data-exif="{&quot;camera&quot;:&quot;Nikon D70&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/416 s&quot;,&quot;focal_length&quot;:&quot;25 mm&quot;,&quot;iso&quot;:&quot;93&quot;,&quot;created_timestamp&quot;:&quot;2/1/2021, 8:49:57 PM&quot;,&quot;orientation&quot;:&quot;Bottom&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/6.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/6@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Stone Path"
+                                     alt="A stone path in the foothills with a mountain in the background.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Stone Path</div>
+                        <div class="fg-caption-desc">A stone path in the foothills with a mountain in the background.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/7.jpg' ?>"
+                   data-type="image"
+                   data-title="Mountain Peak"
+                   data-description="The sunset from a mountain peak above the clouds."
+                   data-exif="{&quot;camera&quot;:&quot;Canon PowerShot S40&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/210 s&quot;,&quot;focal_length&quot;:&quot;26 mm&quot;,&quot;iso&quot;:&quot;51&quot;,&quot;created_timestamp&quot;:&quot;2/6/2021, 8:26:16 PM&quot;,&quot;orientation&quot;:&quot;Right Side&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/7.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/7@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Mountain Peak"
+                                     alt="The sunset from a mountain peak above the clouds.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Mountain Peak</div>
+                        <div class="fg-caption-desc">The sunset from a mountain peak above the clouds.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/8.jpg' ?>"
+                   data-type="image"
+                   data-title="Mountain Village"
+                   data-description="A mountainside village with some morning mist."
+                   data-exif="{&quot;camera&quot;:&quot;Konica Minolta DiMAGE Z3&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/845 s&quot;,&quot;focal_length&quot;:&quot;14 mm&quot;,&quot;iso&quot;:&quot;71&quot;,&quot;created_timestamp&quot;:&quot;2/7/2021, 8:40:03 PM&quot;,&quot;orientation&quot;:&quot;Left Side&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/8.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/8@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Mountain Village"
+                                     alt="A mountainside village with some morning mist.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Mountain Village</div>
+                        <div class="fg-caption-desc">A mountainside village with some morning mist.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+        <div class="fg-item fg-type-image fg-item-exif fg-idle">
+            <figure class="fg-item-inner">
+                <a class="fg-thumb"
+                   href="<?php echo FOOGALLERY_URL . 'assets/demo/9.jpg' ?>"
+                   data-type="image"
+                   data-title="Valley"
+                   data-description="The view down a valley as lightning strikes in the background."
+                   data-exif="{&quot;camera&quot;:&quot;Konica Minolta DiMAGE Z3&quot;,&quot;aperture&quot;:&quot;f/4.0&quot;,&quot;shutter_speed&quot;:&quot;1/845 s&quot;,&quot;focal_length&quot;:&quot;14 mm&quot;,&quot;iso&quot;:&quot;71&quot;,&quot;created_timestamp&quot;:&quot;2/7/2021, 8:40:03 PM&quot;,&quot;orientation&quot;:&quot;Left Side&quot;}">
+                            <span class="fg-image-wrap">
+                                <img class="fg-image"
+                                     src="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20150%20150%22%3E%3C/svg%3E"
+                                     data-src-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/9.jpg' ?>"
+                                     data-srcset-fg="<?php echo FOOGALLERY_URL . 'assets/demo/thumbs/9@2x.jpg' ?> 2x"
+                                     width="150"
+                                     height="150"
+                                     title="Valley"
+                                     alt="The view down a valley as lightning strikes in the background.">
+                            </span>
+                    <span class="fg-image-overlay"></span>
+                </a>
+                <figcaption class="fg-caption">
+                    <div class="fg-caption-inner">
+                        <div class="fg-caption-title">Valley</div>
+                        <div class="fg-caption-desc">The view down a valley as lightning strikes in the background.</div>
+                    </div>
+                </figcaption>
+            </figure>
+            <div class="fg-loader"></div>
+        </div>
+
+
+    </div>
+
 </div>
