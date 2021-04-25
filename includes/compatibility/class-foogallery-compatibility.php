631c1b59dabaea19441420d4b3bed104fe3d1d0b
bradvin <bradvin@gmail.com>
2021-04-25 09:34:07 +0100

    added compatability for Jetpack Lazyload module and Jetpack Boost

--- includes/compatibility/class-foogallery-compatibility.php
+++ includes/compatibility/class-foogallery-compatibility.php
@@ -5,18 +5,27 @@
  * Date: 23/03/2018
  */
 
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-autoptimize-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-foobox-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-polylang-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-responsive-lightbox-dfactory-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-wprocket-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-foovideo-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-elasticpress-compatibility.php' );
-require_once( FOOGALLERY_PATH . 'includes/compatibility/class-elementor-compatibility.php' );
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-autoptimize-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-foobox-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-polylang-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-responsive-lightbox-dfactory-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-wprocket-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-foovideo-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-elasticpress-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-elementor-compatibility.php';
+require_once FOOGALLERY_PATH . 'includes/compatibility/class-jetpack-compatibility.php';
 
 if ( ! class_exists( 'FooGallery_Compatibility' ) ) {
+
+	/**
+	 * Class FooGallery_Compatibility
+	 */
 	class FooGallery_Compatibility {
-		function __construct() {
+
+		/**
+		 * FooGallery_Compatibility constructor.
+		 */
+		public function __construct() {
 			new FooGallery_Autoptimize_Compatibility();
 			new FooGallery_FooBox_Compatibility();
 			new FooGallery_Polylang_Compatibility();
@@ -25,6 +34,7 @@ if ( ! class_exists( 'FooGallery_Compatibility' ) ) {
 			new FooGallery_ElasticPress_Compatibility();
 			new FooGallery_Elementor_Compatibility();
 			new FooGallery_WPRocket_Compatibility();
+			new FooGallery_Jetpack_Compatibility();
 		}
 	}
 }
