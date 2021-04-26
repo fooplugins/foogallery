89f6d3b57f0d85696668fea661ad28e3d8ab3117
bradvin <bradvin@gmail.com>
2021-04-26 20:17:03 +0100

    fixed bug where custom url was not opening in lightbox in single thumbnail gallery

--- extensions/default-templates/thumbnail/gallery-thumbnail.php
+++ extensions/default-templates/thumbnail/gallery-thumbnail.php
@@ -4,15 +4,20 @@
  */
 global $current_foogallery;
 
-$lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
-$position = foogallery_gallery_template_setting( 'position', 'fg-center' );
+$lightbox        = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
+$position        = foogallery_gallery_template_setting( 'position', 'fg-center' );
+$link_custom_url = foogallery_gallery_template_setting( 'link_custom_url' );
 
 $featured_attachment = $current_foogallery->featured_attachment();
 $featured_attachment->featured = true;
 
 $args = foogallery_gallery_template_arguments();
 $args['override_caption_title'] = foogallery_gallery_template_setting( 'caption_title', '' );
-$args['override_caption_desc'] = foogallery_gallery_template_setting( 'caption_description', '' );
+$args['override_caption_desc']  = foogallery_gallery_template_setting( 'caption_description', '' );
+
+if ( 'on' === $link_custom_url && '' !== $lightbox && ! empty( $featured_attachment->custom_url ) ) {
+	$featured_attachment->type = 'iframe';
+}
 
 $foogallery_single_thumbnail_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'foogallery-single-thumbnail', 'foogallery-lightbox-' . $lightbox, $position );
 $foogallery_single_thumbnail_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_single_thumbnail_classes ) );
