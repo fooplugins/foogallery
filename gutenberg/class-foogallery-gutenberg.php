ac9405aca0fcf97e6c1743d845fdf066b697d87f
bradvin <bradvin@gmail.com>
2021-05-08 12:17:30 +0100

    updated how gallery usage is determined

--- gutenberg/class-foogallery-gutenberg.php
+++ gutenberg/class-foogallery-gutenberg.php
@@ -4,47 +4,53 @@
  * Date: 28/10/2018
  */
 
-require_once( FOOGALLERY_PATH . 'gutenberg/class-foogallery-blocks.php' );
-require_once( FOOGALLERY_PATH . 'gutenberg/class-foogallery-rest-routes.php' );
+require_once FOOGALLERY_PATH . 'gutenberg/class-foogallery-blocks.php';
+require_once FOOGALLERY_PATH . 'gutenberg/class-foogallery-rest-routes.php';
 
 if ( ! class_exists( 'FooGallery_Gutenberg' ) ) {
 
+	/**
+	 * Class FooGallery_Gutenberg
+	 */
 	class FooGallery_Gutenberg {
 
-		function __construct() {
+		/**
+		 * FooGallery_Gutenberg constructor.
+		 */
+		public function __construct() {
 			new FooGallery_Blocks();
 			new FooGallery_Rest_Routes();
 
-			add_action( 'foogallery_attach_gallery_to_post', array( $this, 'attach_gallery_to_post' ), 10, 2 );
+			add_filter( 'foogallery_find_galleries_in_post', array( $this, 'find_galleries_in_post' ), 10, 2 );
 		}
 
 		/**
-		 * Use the built-in Block Parser to "attach" a gallery to a post
+		 * Use the built-in Block Parser to find all foogallery blocks in post content.
 		 *
-		 * @param $post_id
-		 * @param $post
+		 * @param array   $galleries the galleries found in the post.
+		 * @param WP_Post $post the post we are checking.
 		 */
-		function attach_gallery_to_post( $post_id, $post ) {
-			if ( !class_exists( 'WP_Block_Parser' ) ) {
-				return;
+		public function find_galleries_in_post( $galleries, $post ) {
+			if ( ! class_exists( 'WP_Block_Parser' ) ) {
+				return $galleries;
 			}
 
-			if ( !is_object( $post ) ) {
-				return;
+			if ( ! is_object( $post ) ) {
+				return $galleries;
 			}
 
 			$parser = new WP_Block_Parser();
 			$blocks = $parser->parse( $post->post_content );
 
 			foreach ( $blocks as $block ) {
-				if ( array_key_exists( 'id', $block['attrs'] ) ) {
-					$gallery_id = $block['attrs']['id'];
-
-					add_post_meta( $post_id, FOOGALLERY_META_POST_USAGE, $gallery_id, false );
-
-					do_action( 'foogallery_attach_gallery_to_post', $post_id, $gallery_id );
+				if ( array_key_exists( 'blockName', $block ) && 'fooplugins/foogallery' === $block['blockName'] ) {
+					if ( array_key_exists( 'id', $block['attrs'] ) ) {
+						$galleries[] = $block['attrs']['id'];
+					}
 				}
 			}
+
+			return $galleries;
 		}
 	}
 }
