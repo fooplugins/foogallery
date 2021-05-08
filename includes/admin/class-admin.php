9b7381ab5fe9e862dcb5c723f25a38487c761628
bradvin <bradvin@gmail.com>
2021-05-08 12:27:12 +0100

    do not instantiate class that does not exist

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -29,7 +29,6 @@ if ( ! class_exists( 'FooGallery_Admin' ) ) {
 			new FooGallery_Admin_Columns();
 			new FooGallery_Admin_Extensions();
 			new FooGallery_Attachment_Fields();
-            new FooGallery_Admin_CSS_Load_Optimizer();
 			new FooGallery_Admin_Notices();
 			$foogallery_admin_datasource_instance = new FooGallery_Admin_Gallery_Datasources();
 		}
