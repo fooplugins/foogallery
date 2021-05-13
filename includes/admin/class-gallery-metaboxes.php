5991417d74dca3a2f4c0f77e806c1eb77f3a2073
bradvin <bradvin@gmail.com>
2021-05-13 16:33:17 +0100

    sanitize custom css captured by admin

--- includes/admin/class-gallery-metaboxes.php
+++ includes/admin/class-gallery-metaboxes.php
@@ -171,8 +171,8 @@ if ( ! class_exists( 'FooGallery_Admin_Gallery_MetaBoxes' ) ) {
 					update_post_meta( $post_id, FOOGALLERY_META_SORT, $_POST[FOOGALLERY_META_SORT] );
 				}
 
-				$custom_css = isset($_POST[FOOGALLERY_META_CUSTOM_CSS]) ?
-					$_POST[FOOGALLERY_META_CUSTOM_CSS] : '';
+				$custom_css = foogallery_sanitize_html( isset( $_POST[FOOGALLERY_META_CUSTOM_CSS] ) ?
+					$_POST[FOOGALLERY_META_CUSTOM_CSS] : '' );
 
 				if ( empty( $custom_css ) ) {
 					delete_post_meta( $post_id, FOOGALLERY_META_CUSTOM_CSS );
