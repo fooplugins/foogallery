dbce4acc6821cf9868d81a1b64dbeaf74c8c14f5
Oleg Shumar <oleg.shumar@gmail.com>
2021-07-04 19:50:28 +0300

    Swiper.js carousel template added

---
+++ pro/extensions/default-templates/swiper/class-swiper-gallery-template.php
@@ -0,0 +1,412 @@
+<?php
+
+if ( !class_exists( 'FooGallery_Swiper_Template' ) ) {
+
+    class FooGallery_Swiper_Template {
+        /**
+         * Wire up everything we need to run the extension
+         */
+        function __construct() {
+            add_filter( 'foogallery_gallery_templates', array( $this, 'add_template' ), 101 );
+
+            add_filter( 'foogallery_gallery_templates_files', array( $this, 'register_myself' ) );
+
+            //set the settings icon for lightbox
+            add_filter( 'foogallery_gallery_settings_metabox_section_icon', array( $this, 'add_section_icons' ) );
+
+            //change fields for the template
+            add_filter( 'foogallery_override_gallery_template_fields-swiper', array( $this, 'change_common_thumbnail_fields' ), 10, 2 );
+        }
+
+        /**
+         * Returns the Dashicon that can be used in the settings tabs
+         *
+         * @param $section_slug
+         *
+         * @return string
+         */
+        function add_section_icons( $section_slug ) {
+            if ( 'swiper' === $section_slug ) {
+                return 'dashicons-align-left';
+            }
+
+            return $section_slug;
+        }
+
+        /**
+         * Add the video gallery template to the list of templates available
+         * @param $gallery_templates
+         *
+         * @return array
+         */
+        function add_template( $gallery_templates ) {
+
+            $gallery_templates[] = array(
+                'slug'        => 'swiper',
+                'name'        => __( 'Swiper PRO', 'foogallery'),
+                'preview_support' => true,
+                'common_fields_support' => true,
+                'lazyload_support' => false,
+                'paging_support' => false,
+                'thumbnail_dimensions' => false,
+                'filtering_support' => true,
+                'mandatory_classes' => 'fg-swiper',
+                'embed_support' => true,
+                'panel_support' => true,
+                'enqueue_core' => true,
+                'fields'	  => array(
+                    array(
+                        'id'      => 'aspect-ratio',
+                        'section' => __( 'Swiper', 'foogallery' ),
+                        'subsection' => array( 'lightbox-general' => __( 'General', 'foogallery' ) ),
+                        'title'   => __('Aspect Ratio', 'foogallery'),
+                        'desc' => __('Select the aspect ratio the slider will use, to best suit your content.', 'foogallery'),
+                        'default' => 'fg-16-9',
+                        'type'    => 'radio',
+                        'spacer'  => '<span class="spacer"></span>',
+                        'choices' => array(
+                            'fg-16-9' => __( '16:9', 'foogallery' ),
+                            'fg-16-10' => __( '16:10', 'foogallery' ),
+                            'fg-4-3' => __( '4:3', 'foogallery' ),
+                        ),
+                        'row_data'=> array(
+                            'data-foogallery-change-selector' => 'input',
+                            'data-foogallery-value-selector' => 'input:checked',
+                            'data-foogallery-preview' => 'shortcode'
+                        )
+                    ),
+                    array(
+                        'id'      => 'swiper_help',
+                        'title'   => __('Help', 'foogallery'),
+                        'desc'    => __( 'You can change the layout of the slider by changing the position of the thumbnails.', 'foogallery' ),
+                        'section' => __( 'Swiper', 'foogallery' ),
+                        'subsection' => array( 'lightbox-general' => __( 'General', 'foogallery' ) ),
+                        'type'    => 'help',
+                    ),
+                )
+            );
+
+            return $gallery_templates;
+        }
+
+        /**
+         * Register myself so that all associated JS and CSS files can be found and automatically included
+         * @param $extensions
+         *
+         * @return array
+         */
+        function register_myself( $extensions ) {
+            $extensions[] = __FILE__;
+            return $extensions;
+        }
+
+        /**
+         * Remove some common fields
+         *
+         * @uses "foogallery_override_gallery_template_fields"
+         * @param $fields
+         * @param $template
+         *
+         * @return array
+         */
+        function change_common_thumbnail_fields( $fields, $template ) {
+
+            $fields_to_remove = array();
+            $fields_to_remove[] = 'border_size';
+            $fields_to_remove[] = 'rounded_corners';
+            $fields_to_remove[] = 'drop_shadow';
+            $fields_to_remove[] = 'loaded_effect';
+            $fields_to_remove[] = 'hover_effect_help';
+            $fields_to_remove[] = 'theme_custom_help';
+            $fields_to_remove[] = 'hover_effect_preset_size';
+            $fields_to_remove[] = 'hover_effect_caption_visibility';
+            $fields_to_remove[] = 'captions_help';
+            $fields_to_remove[] = 'video_size_help';
+            $fields_to_remove[] = 'video_size';
+            $fields_to_remove[] = 'lightbox_theme';
+            $fields_to_remove[] = 'lightbox_no_scrollbars';
+            $fields_to_remove[] = 'lightbox_caption_override';
+            $fields_to_remove[] = 'captions_help';
+            $fields_to_remove[] = 'caption_title_source';
+            $fields_to_remove[] = 'caption_desc_source';
+            $fields_to_remove[] = 'captions_limit_length';
+            $fields_to_remove[] = 'caption_title_length';
+            $fields_to_remove[] = 'caption_desc_length';
+            $fields_to_remove[] = 'captions_type';
+            $fields_to_remove[] = 'caption_custom_template';
+            $fields_to_remove[] = 'caption_custom_help';
+            $fields_to_remove[] = 'caption_alignment';
+            $fields_to_remove[] = 'lightbox_caption_override';
+            $fields_to_remove[] = 'aspect-ratio';
+            $fields_to_remove[] = 'lightbox_auto_progress_start';
+            $fields_to_remove[] = 'lightbox_mobile_layout';
+            $fields_to_remove[] = 'lightbox_button_theme';
+            $fields_to_remove[] = 'lightbox_button_highlight';
+            $fields_to_remove[] = 'lightbox_buttons_display';
+            $fields_to_remove[] = 'lightbox_hover_buttons';
+            $fields_to_remove[] = 'lightbox_show_fullscreen_button';
+            $fields_to_remove[] = 'lightbox_thumbs_captions';
+            $fields_to_remove[] = 'lightbox_thumbs_captions_alignment';
+            $fields_to_remove[] = 'lightbox_thumbs_bestfit';
+            $fields_to_remove[] = 'lightbox_thumbs_size';
+
+            $indexes_to_remove = array();
+
+            $settings_link = sprintf( '<a target="blank" href="%s">%s</a>', foogallery_admin_settings_url(), __( 'settings', 'foogallery' ) );
+            $captions_choices = array(
+                'none'    => __( 'None', 'foogallery' ),
+                ''        => sprintf( __( 'Default (as per %s)', 'foogallery' ), $settings_link ),
+                'title'   => foogallery_get_attachment_field_friendly_name( 'title' ),
+                'caption' => foogallery_get_attachment_field_friendly_name( 'caption' ),
+                'alt'     => foogallery_get_attachment_field_friendly_name( 'alt' ),
+                'desc'    => foogallery_get_attachment_field_friendly_name( 'desc' ),
+            );
+
+            $fields[] = [
+                "id" => "slides_per_view",
+                "title" => "Slides Per View",
+                "desc" => "Number of slides per view (slides visible at the same time on slider's container).",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-general" => "General"
+                ],
+                "row_data" => [
+                    "data-foogallery-hidden" => 1,
+                    "data-foogallery-show-when-field" => "effect",
+                    "data-foogallery-show-when-field-operator" => "regex",
+                    "data-foogallery-show-when-field-value" => "default|coverflow",
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input"
+                ],
+                "type" => "text",
+                "default" => 1,
+
+            ];
+
+            $fields[] = [
+                "id" => "space_between",
+                "title" => "Space Between",
+                "desc" => "Distance between slides in px.",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-general" => "General"
+                ],
+                "type" => "text",
+                "default" => 10,
+                "row_data" => [
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input"
+                ]
+            ];
+
+            $fields[] = [
+                "id" => "effect",
+                "title" => "Animation Effect",
+                "desc" => "Animation on change slide",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-general" => "General"
+                ],
+                "type" => "radio",
+                "default" => "fade",
+                "spacer" => '<span class="spacer"></span>',
+                "choices" =>  [
+                    "default" => "Default",
+                    "fade" => "Fade",
+                    "coverflow" => "Coverflow",
+                    "flip" => "Flip",
+                    "cube" => "Cube"
+                ],
+                "row_data" => [
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input:checked"
+                ]
+            ];
+
+            $fields[] = [
+                "id" => "height_container",
+                "title" => "Swiper Height (vertical)",
+                "desc" => "Height size of container Swiper in px",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-general" => "General"
+                ],
+                "type" => "text",
+                "default" => 400,
+                "row_data" => [
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input"
+                ]
+            ];
+
+            $fields[] = [
+                "id" => "height_image_container",
+                "title" => "Image Height",
+                "desc" => "Height size images slides in px",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-thumbnails" => "Thumbnails"
+                ],
+                "type" => "text",
+                "default" => 200,
+                "row_data" => [
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input"
+                ]
+            ];
+
+            $fields[] = [
+                "id" => "width_image_container",
+                "title" => "Image Width",
+                "desc" => "Width size images slides in px",
+                "section" => "Swiper",
+                "subsection" => [
+                    "lightbox-thumbnails" => "Thumbnails"
+                ],
+                "type" => "text",
+                "default" => 400,
+                "row_data" => [
+                    "data-foogallery-change-selector" => "input",
+                    "data-foogallery-preview" => "shortcode",
+                    "data-foogallery-value-selector" => "input"
+                ]
+            ];
+
+            foreach ($fields as $key => &$field) {
+                if ( $field['section'] === __( 'Panel', 'foogallery' ) ) {
+                    $field['section'] = __( 'Swiper', 'foogallery' );
+                }
+
+                /*if (isset($field['subsection']['lightbox-thumbnails'])) {
+                    $fields_to_remove[] = $field['id'];
+                }*/
+
+                if (isset($field['subsection']['lightbox-captions'])) {
+                    $fields_to_remove[] = $field['id'];
+                }
+
+
+                if ( 'lightbox_auto_progress' === $field['id'] ) {
+                    $field['subsection'] = ['lightbox-controls' => 'Controls'];
+                } elseif ( 'lightbox_auto_progress_seconds' === $field['id'] ) {
+                    $field['subsection'] = ['lightbox-controls' => 'Controls'];
+                    $field["row_data"] = [
+                        "data-foogallery-change-selector" => "input",
+                        "data-foogallery-preview" => "shortcode",
+                        "data-foogallery-value-selector" => "input"
+                    ];
+                } elseif ( 'hover_effect_preset' === $field['id'] ) {
+                    $field['default'] = 'fg-custom';
+                    $field['choices'] = array(
+                        'fg-custom'  => __( 'Swiper', 'foogallery' )
+                    );
+                    $field['row_data'] = array(
+                        'data-foogallery-hidden' => true,
+                        'data-foogallery-change-selector' => 'input:radio',
+                        'data-foogallery-value-selector' => 'input:checked',
+                        'data-foogallery-preview' => 'class'
+                    );
+                } elseif( 'lightbox_transition' === $field['id'] ) {
+                    $field['default'] = 'horizontal';
+                    $field['choices'] = array(
+                        'horizontal'  => __( 'Horizontal', 'foogallery' ),
+                        'vertical'  => __( 'Vertical', 'foogallery' )
+                    );
+
+                } elseif( 'lightbox_show_maximize_button' === $field['id'] ) {
+                    $field['title'] = 'Unlimited Scroll';
+                    $field['desc'] = 'Loop slider for unlimited scroll after end';
+                    $field['default'] = 'yes';
+                    $field['choices'] = array(
+                        'yes'  => __( 'Yes', 'foogallery' ),
+                        'no'  => __( 'No', 'foogallery' )
+                    );
+                    $field['row_data'] = [
+                        'data-foogallery-hidden' => 1,
+                        'data-foogallery-show-when-field' => 'lightbox_auto_progress',
+                        'data-foogallery-show-when-field-operator' => '===',
+                        'data-foogallery-show-when-field-value' => 'no',
+                        "data-foogallery-change-selector" => "input",
+                        "data-foogallery-preview" => "shortcode",
+                        "data-foogallery-value-selector" => "input:checked"
+                    ];
+                } elseif( 'lightbox_show_caption_button' === $field['id'] ) {
+                    $field['title'] = 'Grab Cursor';
+                    $field['desc'] = 'Show hand cursor on slides';
+                    $field['default'] = 'yes';
+                    $field['choices'] = array(
+                        'yes'  => __( 'Yes', 'foogallery' ),
+                        'no'  => __( 'No', 'foogallery' ),
+                    );
+                    $field["row_data"] = [
+                        "data-foogallery-change-selector" => "input",
+                        "data-foogallery-preview" => "shortcode",
+                        "data-foogallery-value-selector" => "input:checked"
+                    ];
+                } elseif( 'lightbox_show_thumbstrip_button' === $field['id'] ) {
+                    $field['title'] = 'Pagination';
+                    $field['desc'] = 'Show pagination dots';
+                } else if ( 'video_autoplay' === $field['id'] ) {
+                    $field['title'] = __( 'Autoplay', 'foogallery' );
+                    $field['desc'] = __( 'Try to autoplay the video when selected. This will only work with videos hosted on Youtube or Vimeo.', 'foogallery' );
+                } else if ( 'lightbox_buttons_display' === $field['id'] ) {
+                    $field['default'] = 'yes';
+                } else if ( 'lightbox_thumbs' === $field['id'] ) {
+                    $field['title'] = __( 'Image Feet', 'foogallery' );
+                    $field['default'] = 'none';
+                    $field['desc'] = __( 'Change size image in box by fill the box or show default image sizing', 'foogallery' );
+                    $field['choices'] = array(
+                        'none'  => __( 'None', 'foogallery' ),
+                        'cover'  => __( 'Cover', 'foogallery' ),
+                        'contain'  => __( 'Contain', 'foogallery' ),
+                        'fill'  => __( 'Fill', 'foogallery' )
+                    );
+                } else if ( 'lightbox_thumbs_captions' === $field['id'] ) {
+                    $field['title'] = __( 'Thumbnail Captions', 'foogallery' );
+                    $field['default'] = 'yes';
+                } else if ( 'lightbox_hover_buttons' === $field['id'] ) {
+                    $field['default'] = 'yes';
+                } else if ( 'lightbox_caption_override_title' === $field['id'] ) {
+                    $field['title'] = __( 'Caption Title', 'foogallery' );
+                    unset( $field['desc'] );
+                    $field['default'] = 'caption';
+
+                    $field['choices'] = $captions_choices;
+                    $field['row_data'] = array(
+                        'data-foogallery-show-when-field-value'    => 'override',
+                        'data-foogallery-change-selector'          => 'input:radio',
+                        'data-foogallery-preview'                  => 'shortcode',
+                        'data-foogallery-value-selector'           => 'input:checked',
+                    );
+                } else if ( 'lightbox_caption_override_desc' === $field['id'] ) {
+                    $field['title'] = __( 'Caption Description', 'foogallery' );
+                    unset( $field['desc'] );
+                    $field['default'] = 'desc';
+                    $field['choices'] = $captions_choices;
+                    $field['row_data'] = array(
+                        'data-foogallery-show-when-field-value'    => 'override',
+                        'data-foogallery-change-selector'          => 'input:radio',
+                        'data-foogallery-preview'                  => 'shortcode',
+                        'data-foogallery-value-selector'           => 'input:checked',
+                    );
+                }
+
+
+                if ( in_array( $field['id'], $fields_to_remove ) ) {
+                    $indexes_to_remove[] = $key;
+                }
+            }
+
+            foreach ($indexes_to_remove as $index) {
+                unset($fields[$index]);
+            }
+
+            return $fields;
+        }
+    }
+}
---
+++ pro/extensions/default-templates/swiper/gallery-swiper.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * FooGallery Swiper PRO gallery template
+ */
+global $current_foogallery;
+global $current_foogallery_arguments;
+
+$columns = foogallery_gallery_template_setting( 'columns', 'swiper-cols-4' );
+
+$foogallery_default_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'swiper', $columns );
+$foogallery_default_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_default_classes ) );
+
+if($_POST['action'] == 'foogallery_preview') {
+    //$current_foogallery->settings = $_POST['_foogallery_settings'];
+}
+    ?><div <?php echo $foogallery_default_attributes; ?>>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <?php foreach(foogallery_current_gallery_attachments_for_rendering() as $attachment) { ?>
+                <div class="swiper-slide" role="group" style="width: <?= $current_foogallery->settings['swiper_width_image_container'] ?>px !important; height: <?= $current_foogallery->settings['swiper_height_image_container'] ?>px !important;">
+                    <div class="card-image">
+                        <img src="<?= $attachment->url ?>" alt="<?= $attachment->title ?>" style="max-width: 100%;object-fit: <?= $current_foogallery->settings['swiper_lightbox_thumbs'] ?>; height: <?= $current_foogallery->settings['swiper_height_image_container'] ?>px;">
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+        <?php if($current_foogallery->settings['swiper_lightbox_show_nav_buttons'] == "yes") { ?>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+        <?php } ?>
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
+
+<script>
+    var swiper_slides_per_view = "<?= $current_foogallery->settings['swiper_slides_per_view'] ?>";
+    var swiper_space_between = "<?= $current_foogallery->settings['swiper_space_between'] ?>";
+    var swiper_direction = "<?= $current_foogallery->settings['swiper_lightbox_transition'] ?>";
+    var swiper_effect = "<?= $current_foogallery->settings['swiper_effect'] ?>";
+    var swiper_autoHeight = "<?php if($current_foogallery->settings['swiper_lightbox_fit_media'] == 'yes') {echo 'true';} else {echo 'false';} ?>";
+    var swiper_height = "<?= $current_foogallery->settings['swiper_height_container'] ?>";
+    <?php if($current_foogallery->settings['swiper_lightbox_auto_progress'] == "yes") { ?>
+        var swiper_autoplay = {
+            delay: <?= intval($current_foogallery->settings['swiper_lightbox_auto_progress_seconds']) * 1000 ?>,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+            stopOnLastSlide: false,
+            waitForTransition: false
+        }
+    <?php } else { ?>
+        var swiper_autoplay = false;
+    <?php } ?>
+    <?php if($current_foogallery->settings['swiper_lightbox_show_thumbstrip_button'] == "yes") { ?>
+        var swiper_pagination = {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    <?php } else { ?>
+        var swiper_pagination = false;
+    <?php } ?>
+    <?php if($current_foogallery->settings['swiper_lightbox_show_nav_buttons'] == "yes") { ?>
+    var swiper_navigation = {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+    <?php } else { ?>
+    var swiper_navigation = false;
+    <?php } ?>
+    <?php if($current_foogallery->settings['swiper_lightbox_show_caption_button'] == "yes") { ?>
+    var swiper_grabcursor = true;
+    <?php } else { ?>
+    var swiper_grabcursor = false;
+    <?php } ?>
+    <?php if($current_foogallery->settings['swiper_lightbox_show_maximize_button'] == "yes") { ?>
+    var swiper_loop = true;
+    <?php } else { ?>
+    var swiper_loop = false;
+    <?php } ?>
+</script>
+
+<?php
+// For Preview in Edit Gallery
+if($_POST['action'] == 'foogallery_preview') { ?>
+    <style>
+        .postbox-container .foogallery_preview_container {
+            height: 100% !important;
+            transform: unset !important;
+        }
+        .postbox-container .swiper-wrapper .swiper-slide {
+            width: <?= $current_foogallery->settings['swiper_width_image_container'] ?>px !important;
+        }
+        .postbox-container .swiper-container-autoheight, .swiper-container-autoheight .swiper-slide {
+            height: 100% !important;
+        }
+        .swiper-slide-active {
+            z-index: 99;
+        }
+    </style>
+    <script>
+        if (typeof swiper_grabcursor !== 'undefined') {
+            // Swiper Configuration
+            var swiper = new Swiper(".swiper-container", {
+                autoHeight: swiper_autoHeight,
+                height: swiper_height,
+                slidesPerView: swiper_slides_per_view,
+                
+            });
+        }
+    </script>
+<?php } ?>
