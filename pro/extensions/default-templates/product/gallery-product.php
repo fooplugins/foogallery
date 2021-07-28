0bd46f2aab19d5bf11f783e0666be6ff98d3daed
bradvin <bradvin@gmail.com>
2021-07-28 19:55:15 +0100

    always show caption for product gallery

--- pro/extensions/default-templates/product/gallery-product.php
+++ pro/extensions/default-templates/product/gallery-product.php
@@ -9,7 +9,7 @@ $lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
 if ( 'none' == foogallery_gallery_template_setting( 'caption_title_source' ) && 'none' == foogallery_gallery_template_setting( 'caption_desc_source' ) ) {
 	$current_foogallery_arguments['caption_preset'] = 'none';
 }
-$foogallery_portfolio_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'fg-simple_portfolio', 'foogallery-lightbox-' . $lightbox );
+$foogallery_portfolio_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'fg-simple_portfolio fg-caption-always', 'foogallery-lightbox-' . $lightbox );
 $foogallery_portfolio_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_portfolio_classes ) );
 
 ?><div <?php echo $foogallery_portfolio_attributes; ?>>
