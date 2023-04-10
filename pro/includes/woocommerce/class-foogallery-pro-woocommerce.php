79dc44a2f60949e9b982010ac506147a91da8fb6
Brad Vincent <bradvin@gmail.com>
2023-04-10 10:39:34 +0100

    reordered ribbon priority

--- pro/includes/woocommerce/class-foogallery-pro-woocommerce.php
+++ pro/includes/woocommerce/class-foogallery-pro-woocommerce.php
@@ -536,6 +536,15 @@ if ( ! class_exists( 'FooGallery_Pro_Woocommerce' ) ) {
 				}
 			}
 
+            // Do we need to add a featured ribbon?
+			$featured_ribbon_type = foogallery_gallery_template_setting( 'ecommerce_featured_ribbon_type', '' );
+			if ( '' !== $featured_ribbon_type ) {
+				if ( $product->is_featured() ) {
+					$attachment->ribbon_type = $featured_ribbon_type;
+					$attachment->ribbon_text = foogallery_gallery_template_setting( 'ecommerce_featured_ribbon_text', __( 'Featured!', 'foogallery' ) );
+				}
+			}
+
             // Do we need to add an outofstock ribbon?
 			$out_of_stock_ribbon_type = foogallery_gallery_template_setting( 'ecommerce_outofstock_ribbon_type', '' );
 			if ( '' !== $out_of_stock_ribbon_type ) {
@@ -551,15 +560,6 @@ if ( ! class_exists( 'FooGallery_Pro_Woocommerce' ) ) {
 				if ( $product->is_on_backorder() ) {
 					$attachment->ribbon_type = $backorder_ribbon_type;
 					$attachment->ribbon_text = foogallery_gallery_template_setting( 'ecommerce_backorder_ribbon_text', __( 'On Backorder', 'foogallery' ) );
-				}
-			}
-
-            // Do we need to add a featured ribbon?
-			$featured_ribbon_type = foogallery_gallery_template_setting( 'ecommerce_featured_ribbon_type', '' );
-			if ( '' !== $featured_ribbon_type ) {
-				if ( $product->is_featured() ) {
-					$attachment->ribbon_type = $featured_ribbon_type;
-					$attachment->ribbon_text = foogallery_gallery_template_setting( 'ecommerce_featured_ribbon_text', __( 'Featured!', 'foogallery' ) );
 				}
 			}
 
@@ -925,6 +925,41 @@ if ( ! class_exists( 'FooGallery_Pro_Woocommerce' ) ) {
 				);
 
                 $new_fields[] = array(
+					'id'       => 'ecommerce_featured_ribbon_type',
+					'title'    => __( 'Featured Ribbon', 'foogallery' ),
+					'desc'     => __( 'The type of ribbon to display for products that are featured.', 'foogallery' ),
+					'section'  => __( 'Ecommerce', 'foogallery' ),
+					'subsection' => array( 'ecommerce-ribbons' => __( 'Ribbons', 'foogallery' ) ),
+					'type'     => 'select',
+					'default'  => '',
+					'choices'  => FooGallery_Pro_Ribbons::get_ribbon_choices(),
+					'row_data' => array(
+						'data-foogallery-change-selector'          => 'select',
+						'data-foogallery-preview'                  => 'shortcode',
+						'data-foogallery-value-selector'           => 'select :selected',
+					),
+				);
+
+				$new_fields[] = array(
+					'id'       => 'ecommerce_featured_ribbon_text',
+					'title'    => __( 'Featured Ribbon Text', 'foogallery' ),
+					'desc'     => __( 'The text inside the ribbon to display for products that are featured.', 'foogallery' ),
+					'section'  => __( 'Ecommerce', 'foogallery' ),
+					'subsection' => array( 'ecommerce-ribbons' => __( 'Ribbons', 'foogallery' ) ),
+					'type'     => 'text',
+					'default'  => 'icon-star',
+					'row_data' => array(
+						'data-foogallery-hidden'                   => true,
+						'data-foogallery-show-when-field'          => 'ecommerce_featured_ribbon_type',
+						'data-foogallery-show-when-field-operator' => '!==',
+						'data-foogallery-show-when-field-value'    => '',
+						'data-foogallery-change-selector'          => 'input',
+						'data-foogallery-preview'                  => 'shortcode',
+						'data-foogallery-value-selector'           => 'input',
+					),
+				);
+
+                $new_fields[] = array(
 					'id'       => 'ecommerce_outofstock_ribbon_type',
 					'title'    => __( 'Out Of Stock Ribbon', 'foogallery' ),
 					'desc'     => __( 'The type of ribbon to display for products that are out of stock.', 'foogallery' ),
@@ -986,41 +1021,6 @@ if ( ! class_exists( 'FooGallery_Pro_Woocommerce' ) ) {
 					'row_data' => array(
 						'data-foogallery-hidden'                   => true,
 						'data-foogallery-show-when-field'          => 'ecommerce_backorder_ribbon_type',
-						'data-foogallery-show-when-field-operator' => '!==',
-						'data-foogallery-show-when-field-value'    => '',
-						'data-foogallery-change-selector'          => 'input',
-						'data-foogallery-preview'                  => 'shortcode',
-						'data-foogallery-value-selector'           => 'input',
-					),
-				);
-
-                $new_fields[] = array(
-					'id'       => 'ecommerce_featured_ribbon_type',
-					'title'    => __( 'Featured Ribbon', 'foogallery' ),
-					'desc'     => __( 'The type of ribbon to display for products that are featured.', 'foogallery' ),
-					'section'  => __( 'Ecommerce', 'foogallery' ),
-					'subsection' => array( 'ecommerce-ribbons' => __( 'Ribbons', 'foogallery' ) ),
-					'type'     => 'select',
-					'default'  => '',
-					'choices'  => FooGallery_Pro_Ribbons::get_ribbon_choices(),
-					'row_data' => array(
-						'data-foogallery-change-selector'          => 'select',
-						'data-foogallery-preview'                  => 'shortcode',
-						'data-foogallery-value-selector'           => 'select :selected',
-					),
-				);
-
-				$new_fields[] = array(
-					'id'       => 'ecommerce_featured_ribbon_text',
-					'title'    => __( 'Featured Ribbon Text', 'foogallery' ),
-					'desc'     => __( 'The text inside the ribbon to display for products that are featured.', 'foogallery' ),
-					'section'  => __( 'Ecommerce', 'foogallery' ),
-					'subsection' => array( 'ecommerce-ribbons' => __( 'Ribbons', 'foogallery' ) ),
-					'type'     => 'text',
-					'default'  => 'icon-star',
-					'row_data' => array(
-						'data-foogallery-hidden'                   => true,
-						'data-foogallery-show-when-field'          => 'ecommerce_featured_ribbon_type',
 						'data-foogallery-show-when-field-operator' => '!==',
 						'data-foogallery-show-when-field-value'    => '',
 						'data-foogallery-change-selector'          => 'input',
