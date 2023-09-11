e3940b13aac4942bc1ebd5dd7f8f33e36788e96c
frenziecodes <frenziecodes@gmail.com>
2023-09-12 00:57:19 +0300

    add pop up

--- pro/includes/frontend-uploads/class-foogallery-frontend-uploads.php
+++ pro/includes/frontend-uploads/class-foogallery-frontend-uploads.php
@@ -7,9 +7,19 @@ if ( ! class_exists( 'FooGallery_Image_Upload_Form_Shortcode' ) ) {
     class FooGallery_Image_Upload_Form_Shortcode {
 
         function __construct() {
+            add_action( 'wp_enqueue_scripts', array($this,'frontendEnqueueScripts') );
             add_shortcode('foogallery_image_upload_form', array($this, 'render_image_upload_form'));
             add_action('init', array($this, 'handle_image_upload'));
         }
+    
+        /**
+         * Function to enqueue scripts and styles.
+         */
+        public function frontendEnqueueScripts() {
+            $directory = plugin_dir_url(__FILE__);
+        
+            wp_enqueue_style('frontend-uploads', $directory . 'foogallery-frontend-uploads.css', array(), '1.0');
+        }    
 
         /**
          * Render the image upload form shortcode
@@ -21,56 +31,103 @@ if ( ! class_exists( 'FooGallery_Image_Upload_Form_Shortcode' ) ) {
             $output = '';
 
             // Check if the gallery_id attribute is provided
-            if (!$gallery_id) {
+            if ( ! $gallery_id) {
                 $output = 'Gallery ID not specified.';
             } else {
-                // Check if there's a success message in the transient
                 $upload_success_transient = get_transient('foogallery_upload_success_' . $gallery_id);
                 if ($upload_success_transient) {
                     $output = 'Image uploaded successfully.';
-                    // Delete the transient to ensure it's displayed only once
                     delete_transient('foogallery_upload_success_' . $gallery_id);
-                }
-
-                // Display the image upload form here
+                }                
                 ob_start();
                 ?>
                 <form method="post" enctype="multipart/form-data">
-                    <div>
+                    <div style="max-width: 500px; max-height: 200px; border: 1px dashed #999; text-align: center; padding: 20px; margin-top: 10px;">
                         <input type="hidden" name="gallery_id" value="<?php echo esc_attr($gallery_id); ?>" />
-                        <input type="file" name="foogallery_image" />
+                        <input type="file" name="foogallery_image" id="image-upload" accept="image/*" style="display: none;" />
+                        <label for="image-upload" style="cursor: pointer;">
+                            <p>Click to <span style="text-decoration: underline;">browse</span> or drag & drop image here</p>
+                        </label>
                     </div>
 
-                    <div style="margin-top: 10px;" >
-                        <label for="caption">Caption:</label>
-                        <input type="text" name="caption" id="caption" />
+                    <!-- Pop-up content -->
+                    <div class="popup-overlay" id="popup">
+                        <div class="popup-content">
+                            <span class="close-button" id="close-popup">&times;</span>
+                            <div class="popup-inner">
+                                <div class="left-column">
+                                    <div class="image-grid" id="uploaded-images">
+                                        <!-- Image displayed here -->
+                                    </div>
+                                </div>
+                                <div class="right-column">
+                                    <div>
+                                        <label for="caption">Caption:</label>
+                                        <input type="text" name="caption" id="caption" />
+                                    </div>
+                                    <div>
+                                        <label for="description">Description:</label>
+                                        <textarea name="description" id="description"></textarea>
+                                    </div>
+                                    <div>
+                                        <label for="alt">Alt Text:</label>
+                                        <input type="text" name="alt" id="alt" />
+                                    </div>
+                                    <div>
+                                        <label for="custom_url">Custom URL:</label>
+                                        <input type="text" name="custom_url" id="custom_url" />
+                                    </div>
+                                    <div>
+                                        <label for="custom_target">Custom Target:</label>
+                                        <input type="text" name="custom_target" id="custom_target" />
+                                    </div>
+                                    <div style="margin-top: 10px;">
+                                        <input type="submit" name="foogallery_image_upload" value="Upload Image" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    
-                    <div style="margin-top: 10px;" >
-                        <label for="description">Description:</label>
-                        <textarea name="description" id="description"></textarea>
-                    </div>
-                    
-                    <div style="margin-top: 10px;" >
-                        <label for="alt">Alt Text:</label>
-                        <input type="text" name="alt" id="alt" />
-                    </div>
+                </form>               
 
-                    <div style="margin-top: 10px;" >
-                        <label for="custom_url">Custom URL:</label>
-                        <input type="text" name="custom_url" id="custom_url" />
-                    </div>                    
-                    
-                    <div style="margin-top: 10px;" >
-                        <label for="custom_target">Custom Target:</label>
-                        <input type="text" name="custom_target" id="custom_target" />
-                    </div>
 
-                    <div style="margin-top: 10px;" >
-                        <input type="submit" name="foogallery_image_upload" value="Upload Image" />
-                    </div>
-                    
-                </form>
+                <script>
+                    const imageUploadInput = document.getElementById('image-upload');
+                    const popup = document.getElementById('popup');
+                    const closePopupButton = document.getElementById('close-popup');
+                    const uploadedImagesContainer = document.getElementById('uploaded-images');
+
+                    imageUploadInput.addEventListener('change', function () {
+                        if (this.files.length > 0) {
+                            displayPopup();
+                            displayUploadedImages(this.files);
+                        }
+                    });
+
+                    closePopupButton.addEventListener('click', function () {
+                        closePopup();
+                    });
+
+                    function displayPopup() {
+                        popup.style.display = 'flex';
+                    }
+
+                    function closePopup() {
+                        popup.style.display = 'none';
+                    }
+
+                    function displayUploadedImages(files) {
+                        uploadedImagesContainer.innerHTML = '';
+
+                        for (const file of files) {
+                            if (file.type.startsWith('image/')) {
+                                const img = document.createElement('img');
+                                img.src = URL.createObjectURL(file);
+                                uploadedImagesContainer.appendChild(img);
+                            }
+                        }
+                    }
+                </script>
 
                 <?php
                 $output .= ob_get_clean();
