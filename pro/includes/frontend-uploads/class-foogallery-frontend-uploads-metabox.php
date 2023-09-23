4e8b4ce8452d1f8f8344df174f7936256a43a555
frenziecodes <frenziecodes@gmail.com>
2023-09-23 11:35:22 +0300

    passing the gallery id to the metabox

--- pro/includes/frontend-uploads/class-foogallery-frontend-uploads-metabox.php
+++ pro/includes/frontend-uploads/class-foogallery-frontend-uploads-metabox.php
@@ -2,13 +2,16 @@
 
 class FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes extends FooGallery_Admin_Gallery_MetaBoxes {
 
+    private $gallery_id;
+
     public function __construct() {
         parent::__construct();
+        $this->gallery_id = isset($_POST['gallery_id']) ? intval($_POST['gallery_id']) : null;
     }
 
     public function add_meta_boxes_to_gallery($post) {
         parent::add_meta_boxes_to_gallery($post);
-       
+
         add_meta_box(
             'custom_metabox_id',
             __('Front End Upload', 'foogallery'),
@@ -18,10 +21,12 @@ class FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes extends FooGallery_Admi
             'low'
         );
     }
-    
+
     public function render_frontend_upload_metabox($post) {
-        // Metabox content here
+        // You can now access $this->gallery_id to use the gallery ID in your metabox.
     }
 }
+
+
 
 $custom_foogallery_meta_boxes = new FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes();
