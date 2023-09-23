68092009e373940d0de4cbd59e72b119d992e940
frenziecodes <frenziecodes@gmail.com>
2023-09-23 20:15:18 +0300

    add metafields

--- pro/includes/frontend-uploads/class-foogallery-frontend-uploads-metabox.php
+++ pro/includes/frontend-uploads/class-foogallery-frontend-uploads-metabox.php
@@ -30,45 +30,72 @@ class FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes extends FooGallery_Admi
     
         // Use preg_match to find the ID within the shortcode
         if (preg_match('/\[foogallery id="(\d+)"\]/', $shortcode, $matches)) {
-            
             $gallery_id = $matches[1];
             ?>
-			<p class="foogallery-shortcode">
-                <input type="text" id="Upload_Form_copy_shortcode" size="<?php echo strlen( $shortcode ) + 2; ?>" value="<?php echo htmlspecialchars('[Upload_Form id="' . $gallery_id . '"]'); ?>" readonly="readonly" />
+            <p class="foogallery-shortcode">
+                <input type="text" id="Upload_Form_copy_shortcode" size="<?php echo strlen($shortcode) + 2; ?>" value="<?php echo htmlspecialchars('[Upload_Form id="' . $gallery_id . '"]'); ?>" readonly="readonly" />
                 <input type="hidden" id="gallery_id" value="<?php echo esc_attr($gallery_id); ?>" />
             </p>
-
-			<p>
-				<?php _e( 'Paste the above shortcode into a post or page to show the Image Upload Form.', 'foogallery' ); ?>
-			</p>
-			<script>
-				jQuery(function($) {
-					var shortcodeInput = document.querySelector('#Upload_Form_copy_shortcode');
-					shortcodeInput.addEventListener('click', function () {
-						try {
-							// select the contents
-							shortcodeInput.select();
-							//copy the selection
-							document.execCommand('copy');
-							//show the copied message
-							$('.foogallery-shortcode-message').remove();
-							$(shortcodeInput).after('<p class="foogallery-shortcode-message"><?php _e( 'Shortcode copied to clipboard :)','foogallery' ); ?></p>');
-						} catch(err) {
-							console.log('Oops, unable to copy!');
-						}
-					}, false);
-				});
-                
-                const galleryIdInput = document.getElementById('gallery_id');
-                const shortcodeInput = document.getElementById('Upload_Form_copy_shortcode');
-
-                galleryIdInput.addEventListener('change', function () {
-                    const newGalleryId = galleryIdInput.value;
-                    const newShortcode = `[Upload_Form id="${newGalleryId}"]`;
-                    shortcodeInput.value = newShortcode;
+    
+            <p>
+                <?php _e('Paste the above shortcode into a post or page to show the Image Upload Form.', 'foogallery'); ?>
+            </p>
+    
+            <div id="metadata-settings">
+                <h4><?php _e('Check to display the metadata fields in the upload form.', 'foogallery'); ?></h4>
+                <label>
+                    <input type="checkbox" id="display_caption" name="display_caption" <?php checked(get_post_meta($post->ID, '_display_caption', true), 'on'); ?> />
+                    <?php _e('Display Caption', 'foogallery'); ?>
+                </label>
+                <br />
+                <label>
+                    <input type="checkbox" id="display_description" name="display_description" <?php checked(get_post_meta($post->ID, '_display_description', true), 'on'); ?> />
+                    <?php _e('Display Description', 'foogallery'); ?>
+                </label>
+                <br />
+                <label>
+                    <input type="checkbox" id="display_alt" name="display_alt" <?php checked(get_post_meta($post->ID, '_display_alt', true), 'on'); ?> />
+                    <?php _e('Display Alt Text', 'foogallery'); ?>
+                </label>
+                <br />
+                <label>
+                    <input type="checkbox" id="display_custom_url" name="display_custom_url" <?php checked(get_post_meta($post->ID, '_display_custom_url', true), 'on'); ?> />
+                    <?php _e('Display Custom URL', 'foogallery'); ?>
+                </label>
+                <br />
+                <label>
+                    <input type="checkbox" id="display_custom_target" name="display_custom_target" <?php checked(get_post_meta($post->ID, '_display_custom_target', true), 'on'); ?> />
+                    <?php _e('Display Custom Target', 'foogallery'); ?>
+                </label>
+            </div>
+    
+            <script>
+                jQuery(function($) {
+                    var shortcodeInput = document.querySelector('#Upload_Form_copy_shortcode');
+                    shortcodeInput.addEventListener('click', function () {
+                        try {
+                            // select the contents
+                            shortcodeInput.select();
+                            //copy the selection
+                            document.execCommand('copy');
+                            //show the copied message
+                            $('.foogallery-shortcode-message').remove();
+                            $(shortcodeInput).after('<p class="foogallery-shortcode-message"><?php _e( 'Shortcode copied to clipboard :)','foogallery' ); ?></p>');
+                        } catch(err) {
+                            console.log('Oops, unable to copy!');
+                        }
+                    }, false);
+    
+                    const galleryIdInput = document.getElementById('gallery_id');
+                    const metadataSettings = document.getElementById('metadata-settings');
+    
+                    galleryIdInput.addEventListener('change', function () {
+                        const newGalleryId = galleryIdInput.value;
+                        const newShortcode = `[Upload_Form id="${newGalleryId}"]`;
+                        shortcodeInput.value = newShortcode;
+                    });
                 });
-
-			</script>
+            </script>
             <?php
         } else {
             // No ID found
@@ -76,9 +103,6 @@ class FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes extends FooGallery_Admi
         }
     }
     
-    
-    
 }
-
 
 $custom_foogallery_meta_boxes = new FrontEnd_Upload_FooGallery_Admin_Gallery_MetaBoxes();
