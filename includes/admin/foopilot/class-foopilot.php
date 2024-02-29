92be31001576d244edc913c7f6e1f0c1e1dc06aa
lewis ushindi <frenziecodes@gmail.com>
2024-02-29 07:38:19 +0300

    sanitize

--- includes/admin/foopilot/class-foopilot.php
+++ includes/admin/foopilot/class-foopilot.php
@@ -237,7 +237,7 @@
                             <p><?php _e( 'Unlock the power of FooPilot! Sign up for free and get 20 credits to explore our service.', 'foogallery' ); ?></p>
                             <form class="foogallery-foopilot-signup-form-inner-content">
                                 <div style="margin-bottom: 20px;">
-                                <input type="email" id="foopilot-email" name="email" placeholder="<?php _e( 'Enter your email', 'foogallery'); ?>" value="<?php echo esc_attr(wp_get_current_user()->user_email); ?>" style="padding: 10px; border: 1px solid #ccc; border-radius: 5px; width: 250px;">
+                                <input type="email" id="foopilot-email" name="email" placeholder="<?php echo esc_attr( __( 'Enter your email', 'foogallery' ) ); ?>" value="<?php echo esc_attr( foogallery_sanitize_javascript( wp_get_current_user()->user_email ) ); ?>" style="padding: 10px; border: 1px solid #ccc; border-radius: 5px; width: 250px;">
                                 </div>
                                 <button class="foogallery-foopilot-signup-form-inner-content-button button button-primary button-large" type="submit" style="padding: 10px 20px; background-color: #0073e6; color: #fff; border: none; border-radius: 5px; cursor: pointer;"><?php _e( 'Sign Up for free', 'foogallery'); ?></button>
                             </form>
