b68b988f968675c10d696b4fc44423cea861ab51
Frenziecodes <coderflame3@gmail.com>
2024-08-20 13:08:31 +0300

    initial styling

--- pro/extensions/default-templates/boxslider/js/boxslider.js
+++ pro/extensions/default-templates/boxslider/js/boxslider.js
@@ -1,45 +1,45 @@
 import '@boxslider/components';
 
 class FooGalleryBoxSlider extends HTMLElement {
-	connectedCallback() {
-		this.setupSlider();
-		this.setupControls();
-	}
+    connectedCallback() {
+        this.setupSlider();
+        this.setupControls();
+    }
 
-	setupSlider() {
-		this.slider = this.querySelector('bs-carousel, bs-fade, bs-tile, bs-cube');
-		if (this.slider) {
-			this.slider.addEventListener('before', (ev) => console.log('Transition starting', ev.detail));
-			this.slider.addEventListener('after', (ev) => console.log('Transition complete', ev.detail));
-		}
-	}
+    setupSlider() {
+        this.slider = this.querySelector('bs-carousel, bs-fade, bs-tile, bs-cube');
+        if (this.slider) {
+            this.slider.addEventListener('before', (ev) => console.log('Transition starting', ev.detail));
+            this.slider.addEventListener('after', (ev) => console.log('Transition complete', ev.detail));
+        }
+    }
 
-	setupControls() {
-		const controls = this.querySelector('.boxslider-controls');
-		if (controls) {
-			controls.querySelector('.boxslider-prev').addEventListener('click', () => this.slider.slider.prev());
-			controls.querySelector('.boxslider-next').addEventListener('click', () => this.slider.slider.next());
-			controls.querySelector('.boxslider-play').addEventListener('click', () => this.slider.slider.play());
-			controls.querySelector('.boxslider-pause').addEventListener('click', () => this.slider.slider.pause());
+    setupControls() {
+        const controls = this.querySelector('.boxslider-controls');
+        if (controls) {
+            controls.querySelector('.boxslider-prev')?.addEventListener('click', () => this.slider.slider.prev());
+            controls.querySelector('.boxslider-next')?.addEventListener('click', () => this.slider.slider.next());
+            controls.querySelector('.boxslider-play')?.addEventListener('click', () => this.slider.slider.play());
+            controls.querySelector('.boxslider-pause')?.addEventListener('click', () => this.slider.slider.pause());
 
-			this.updatePlayPauseButtonState();
-		}
-	}
+            this.updatePlayPauseButtonState();
+        }
+    }
 
-	updatePlayPauseButtonState() {
-		const playButton = this.querySelector('.boxslider-play');
-		const pauseButton = this.querySelector('.boxslider-pause');
+    updatePlayPauseButtonState() {
+        const playButton = this.querySelector('.boxslider-play');
+        const pauseButton = this.querySelector('.boxslider-pause');
 
-		this.slider.slider.addEventListener('play', () => this.togglePlayPauseButtons(playButton, pauseButton, true));
-		this.slider.slider.addEventListener('pause', () => this.togglePlayPauseButtons(playButton, pauseButton, false));
+        this.slider.slider.addEventListener('play', () => this.togglePlayPauseButtons(playButton, pauseButton, true));
+        this.slider.slider.addEventListener('pause', () => this.togglePlayPauseButtons(playButton, pauseButton, false));
 
-		this.togglePlayPauseButtons(playButton, pauseButton, this.slider.hasAttribute('auto-scroll'));
-	}
+        this.togglePlayPauseButtons(playButton, pauseButton, this.slider.hasAttribute('auto-scroll'));
+    }
 
-	togglePlayPauseButtons(playButton, pauseButton, isPlaying) {
-		if (playButton) playButton.style.display = isPlaying ? 'none' : 'inline-block';
-		if (pauseButton) pauseButton.style.display = isPlaying ? 'inline-block' : 'none';
-	}
+    togglePlayPauseButtons(playButton, pauseButton, isPlaying) {
+        if (playButton) playButton.style.display = isPlaying ? 'none' : 'inline-block';
+        if (pauseButton) pauseButton.style.display = isPlaying ? 'inline-block' : 'none';
+    }
 }
 
 customElements.define('foogallery-boxslider', FooGalleryBoxSlider);
