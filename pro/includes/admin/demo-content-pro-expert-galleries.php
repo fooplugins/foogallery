0009e90df19a3986e997790f349801550b21b1bb
Brad Vincent <bradvin@gmail.com>
2025-10-15 09:43:15 +0100

    added filter demo content

--- pro/includes/admin/demo-content-pro-expert-galleries.php
+++ pro/includes/admin/demo-content-pro-expert-galleries.php
@@ -71,63 +71,67 @@ return array(
 				'default_video_icon_size'   => '',
 			),
 		),
-	), //Demo : Hover Effect Presets Showcase
+	), //Demo : Video Gallery
 	array(
-		'key'         => 'instagram-filters',
-		'post_title'  => 'Demo : Instagram Filters Showcase',
+		'key'         => 'tag-filters',
+		'post_title'  => 'Demo : Tag Filters Showcase',
 		'post_status' => 'publish',
 		'post_type'   => FOOGALLERY_CPT_GALLERY,
-		'items'       => array( 'demo5', 'demo6', 'demo7', 'demo8', 'demo9', 'demo10' , 'demo3' , 'demo2' , 'demo1' , 'demo4'),
+		'items'       => array( 'demo2', 'demo5', 'video-sunrise', 'demo1', 'video-mountain', 'video-lavender', 'demo7', 'demo8', 'demo9', 'video-ocean' ),
 		'meta_input'  => array(
+			FOOGALLERY_META_NOTICE => '<strong><i class="dashicons dashicons-star-filled"></i>This is a PRO demo gallery!</strong> It showcases the following PRO features: <ul class="ul-disc"><li>Tag filtering - tag filtering in the "Pill Block" style.</li><li>Gallery Search - the search box is shown at the top merged with the tag filters.</li><li>Video galleries - you can mix and match video and image in the same gallery.</li></ul>',
+			'_foogallery_video_count' => 4,
 			FOOGALLERY_META_TEMPLATE => 'default',
 			FOOGALLERY_META_SETTINGS => array(
 				'foogallery_items_view'         => 'preview',
 
-				//General
 				'default_thumbnail_dimensions'  => array(
-					'width'  => '250',
-					'height' => '250',
+					'width'  => '150',
+					'height' => '150',
 				),
 				'default_thumbnail_link'        => 'image',
 				'default_lightbox'              => 'foogallery',
-				'default_spacing'               => 'fg-gutter-10',
+				'default_spacing'               => '10',
 				'default_alignment'             => 'fg-center',
-				'default_layout'                => 'fixed',
-				'default_columns'               => '4',
 
 				//Appearance
-				'default_theme'                 => 'fg-light',
+				'default_theme'                 => 'fg-dark',
 				'default_border_size'           => 'fg-border-thin',
-				'default_rounded_corners'       => 'fg-round-medium',
+				'default_rounded_corners'       => 'fg-round-small',
 				'default_drop_shadow'           => 'fg-shadow-medium',
 				'default_inner_shadow'          => '',
 				'default_loading_icon'          => 'fg-loading-pulse',
-
 				'default_loaded_effect'         => 'fg-loaded-fade-in',
-				'default_instagram'             => 'sepia', // PRO Instagram filter
+				'default_instagram'             => '',
 
 				//Captions
-				'default_captions_type'         => 'fg-captions-bottom',
-				'default_caption_title_source'  => 'title',
-				'default_caption_desc_source'   => 'desc',
-				'default_captions_limit_length' => 'yes',
+				'default_captions_type'         => '',
+				'default_caption_title_source'  => 'none',
+				'default_caption_desc_source'   => 'none',
+				'default_captions_limit_length' => '',
 				'default_caption_alignment'     => 'fg-c-c',
+				'default_caption_invert_color'  => 'fg-transparent-overlays',
 
-				//Hover Effects
-				'default_hover_effect_caption_visibility' => 'fg-caption-hover',
-				'default_hover_effect_color'              => 'fg-hover-grayscale',
-				'default_hover_effect_icon'               => 'fg-hover-plus',
-				'default_hover_effect_preset'             => 'fg-custom',
-				'default_hover_effect_scale'              => 'fg-hover-zoomed',
-				'default_hover_effect_transition'         => 'fg-hover-fade',
+				//Hover Effects - Showcasing PRO presets
+				'default_hover_effect_caption_visibility' => '',
+				'default_hover_effect_color'              => '',
+				'default_hover_effect_icon'               => 'fg-hover-zoom5',
+				'default_hover_effect_preset'             => '',
+				'default_hover_effect_scale'              => 'fg-hover-semi-zoomed',
+				'default_hover_effect_transition'         => 'fg-hover-instant',
+                'default_hover_effect_icon_size'          => '48',
 				'default_hover_effect_type'               => 'normal',
-				'default_caption_invert_color'            => 'fg-transparent-overlays',
 
 				//Filtering
-				'default_filtering_type' => 'tags',
-
-				//Paging
-				'default_paging_type' => 'load_more',
+				'default_filtering_theme' => 'fg-dark',
+				'default_filtering_type'  => 'simple',
+                'default_filtering_limit' => 8,
+                'default_filtering_style' => 'pill-block',
+				'default_filtering_hideall' => 'hide',
+				'default_filtering_autoSelected' => 'true',
+				'default_filtering_search' => 'true',
+				'default_filtering_search_position' => 'after-merged',
+				'default_filtering_sort' => 'count_inverse',
 
 				//Advanced
 				'default_state' => 'no',
@@ -136,10 +140,15 @@ return array(
 
 				//Video
 				'default_video_autoplay'    => 'yes',
-				'default_video_hover_icon'  => 'fg-video-default',
-				'default_video_size'        => '250x250',
-				'default_video_sticky_icon' => '',
+				'default_video_hover_icon'  => 'fg-video-1',
+				'default_video_size'        => '1280x720',
+				'default_video_sticky_icon' => 'fg-video-sticky',
+				'default_video_enabled'     => '',
+				'default_video_icon_size'   => '',
+
+				//Lightbox
+				'default_lightbox_thumbs_bestfit' => 'yes',
 			),
 		),
-	), //Demo : Instagram Filters Showcase
+	), //Demo : Tag Filters Demo
 );
