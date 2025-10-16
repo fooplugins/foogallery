811cbb18e1ff6250dc55f5f6529ef32602a10184
Michael Pham <michael.pham@aveo-global.com>
2025-10-17 01:54:12 +0700

    Fix WordPress.Security.EscapeOutput errors in help PRO view
    Fixed 19 escaping violations:
    - Changed _e() to esc_html_e() for all text content
    - Escaped $foogallery_current_plan with esc_attr() in class
    - Escaped $feature['plan'] with esc_attr() in class
    - Used esc_html__() for sprintf
    - Used wp_kses_post() for imploded HTML spans
    
    Indentation preserved.

--- includes/admin/view-help-pro.php
+++ includes/admin/view-help-pro.php
@@ -13,35 +13,35 @@ $foogallery_pro_features = foogallery_pro_features();
 <div id="pro_section" class="foogallery-admin-help-section" style="display: none">
 	<section class="fgah-feature">
 		<header>
-			<h3><?php _e( 'FooGallery PRO Plans', 'foogallery' );?></h3>
-			<p><?php _e( 'Choose from a PRO Plan that suits your requirements and budget : ', 'foogallery' );?>
-				<span class="fgah-plan-prostarter"><?php _e( 'PRO Starter', 'foogallery' );?></span>,
-				<span class="fgah-plan-pro"><?php _e( 'PRO Expert', 'foogallery' );?></span> <?php _e( 'or', 'foogallery' );?>
-				<span class="fgah-plan-commerce"><?php _e( 'PRO Commerce', 'foogallery' );?></span>
+			<h3><?php esc_html_e( 'FooGallery PRO Plans', 'foogallery' );?></h3>
+			<p><?php esc_html_e( 'Choose from a PRO Plan that suits your requirements and budget : ', 'foogallery' );?>
+				<span class="fgah-plan-prostarter"><?php esc_html_e( 'PRO Starter', 'foogallery' );?></span>,
+				<span class="fgah-plan-pro"><?php esc_html_e( 'PRO Expert', 'foogallery' );?></span> <?php esc_html_e( 'or', 'foogallery' );?>
+				<span class="fgah-plan-commerce"><?php esc_html_e( 'PRO Commerce', 'foogallery' );?></span>
 			</p>
 		</header>
 		<footer>
-			<a class="foogallery-admin-help-button-cta" href="<?php echo esc_url ( $plans_url ); ?>"><?php _e( 'Compare FooGallery PRO Plans', 'foogallery' ); ?></a>
+			<a class="foogallery-admin-help-button-cta" href="<?php echo esc_url ( $plans_url ); ?>"><?php esc_html_e( 'Compare FooGallery PRO Plans', 'foogallery' ); ?></a>
 		</footer>
 	</section>
     <section class="fgah-feature">
         <?php if ( $show_trial_message ) { ?>
             <header>
-                <h3><?php _e( 'FooGallery PRO Free Trial 🤩', 'foogallery' );?></h3>
-                <p><?php _e( 'Want to test out all the PRO features below? No problem! You can start a 7-day free trial immediately!', 'foogallery' );?></p>
+                <h3><?php esc_html_e( 'FooGallery PRO Free Trial 🤩', 'foogallery' );?></h3>
+                <p><?php esc_html_e( 'Want to test out all the PRO features below? No problem! You can start a 7-day free trial immediately!', 'foogallery' );?></p>
             </header>
             <footer>
-                <a class="foogallery-admin-help-button-cta" href="<?php echo esc_url ( foogallery_admin_freetrial_url() ); ?>"><?php _e( 'Start Your 7-day Free Trial', 'foogallery' ); ?></a>
+                <a class="foogallery-admin-help-button-cta" href="<?php echo esc_url ( foogallery_admin_freetrial_url() ); ?>"><?php esc_html_e( 'Start Your 7-day Free Trial', 'foogallery' ); ?></a>
             </footer>
         <?php } else if ( $show_thanks_for_pro ) { ?>
             <header>
-                <h3><?php echo sprintf( __( 'Thanks for your support by purchasing a %s license 😍', 'foogallery' ), '<span class="fgah-plan-' . $foogallery_current_plan . '">' . $foogallery_plans[ $foogallery_current_plan ] . '</span>' );?></h3>
-                <p><?php _e( 'Check out the PRO features you can start using immediately...', 'foogallery' );?></p>
+                <h3><?php echo sprintf( esc_html__( 'Thanks for your support by purchasing a %s license 😍', 'foogallery' ), '<span class="fgah-plan-' . esc_attr( $foogallery_current_plan ) . '">' . esc_html( $foogallery_plans[ $foogallery_current_plan ] ) . '</span>' );?></h3>
+                <p><?php esc_html_e( 'Check out the PRO features you can start using immediately...', 'foogallery' );?></p>
             </header>
         <?php } else if ( $is_trial ) { ?>
             <header>
-                <h3><?php _e( 'Thanks for trying out PRO 😍', 'foogallery' );?></h3>
-                <p><?php _e( 'Check out the PRO features you can try out immediately...', 'foogallery' );?></p>
+                <h3><?php esc_html_e( 'Thanks for trying out PRO 😍', 'foogallery' );?></h3>
+                <p><?php esc_html_e( 'Check out the PRO features you can try out immediately...', 'foogallery' );?></p>
             </header>
         <?php } ?>
     </section>
@@ -54,7 +54,7 @@ $foogallery_pro_features = foogallery_pro_features();
         }
         $i++;
         ?>
-    <section class="fgah-feature fgah-feature-pro<?php echo ( $i % 2 === 0 ) ? " fgah-feature-right" : ""; ?> fgah-feature-plan-<?php echo $feature['plan']; ?>">
+    <section class="fgah-feature fgah-feature-pro<?php echo ( $i % 2 === 0 ) ? " fgah-feature-right" : ""; ?> fgah-feature-plan-<?php echo esc_attr( $feature['plan'] ); ?>">
         <div>
             <figure>
                 <a href="<?php echo esc_url( foogallery_admin_url( $feature['link'], 'help', $feature['utm_content'] ) ); ?>" target="_blank">
@@ -65,13 +65,13 @@ $foogallery_pro_features = foogallery_pro_features();
                 <dt><?php echo esc_html( $feature['title']); ?></dt>
                 <dd>
 	                <div class="fgah-feature-sub-title">
-		                <h4><?php _e( 'Available in:', 'foogallery' ); ?></h4>
+		                <h4><?php esc_html_e( 'Available in:', 'foogallery' ); ?></h4>
 		                <?php
 		                $available_plans = array();
 		                foreach ( $feature['plans'] as $plan ) {
 			                $available_plans[] = '<span class="fgah-plan-' . esc_attr( $plan ) . '">' . esc_html( $foogallery_plans[ $plan ] ) . '</span>';
 		                }
-		                echo implode( ' ', $available_plans );
+		                echo wp_kses_post( implode( ' ', $available_plans ) );
 	                    ?>
 	                </div>
 	                <p>
