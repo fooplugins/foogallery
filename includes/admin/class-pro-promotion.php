477fea43c8bcfd08ae1c245c4da0b03ef8223b84
Michael Pham <michael.pham@aveo-global.com>
2025-10-17 11:25:44 +0700

    Fix WordPress.Security.EscapeOutput errors in PRO promotion
    Fixed 9 escaping violations:
    - Escaped variables with esc_attr() and esc_html()
    - Changed _e() to esc_html_e()
    - Changed __() to esc_html__()
    - Added phpcs:ignore for HTML builder method
    
    Indentation preserved.

--- includes/admin/class-pro-promotion.php
+++ includes/admin/class-pro-promotion.php
@@ -324,16 +324,16 @@ if ( ! class_exists( 'FooGallery_Pro_Promotion' ) ) {
 				$plan = __('PRO Expert', 'foogallery');
 			}
 ?>
-			<div class="foogallery_template_field_type-promo <?php echo $class; ?>">
+			<div class="foogallery_template_field_type-promo <?php echo esc_attr( $class ); ?>">
 				<div class="foogallery-promo">
-					<strong><?php echo $plan; ?> <?php _e('Feature', 'foogallery' ); ?> : <?php echo $datasouce_title; ?></strong>
+					<strong><?php echo esc_attr( $plan ); ?> <?php esc_html_e('Feature', 'foogallery' ); ?> : <?php echo esc_html( $datasouce_title ); ?></strong>
 					<br><br>
-					<?php echo $datasource_desc; ?>
+					<?php echo esc_html( $datasource_desc ); ?>
 					<br><br>
-					<?php echo $this->build_promo_trial_html( 'datasources', $plan ); ?>
+					<?php echo $this->build_promo_trial_html( 'datasources', $plan ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					<br><br>
-					<a class="button-primary" href="<?php echo esc_url( $this->build_url( $datasource_url_name ) ); ?>" target="_blank"><?php echo __( 'Learn More', 'foogallery' ); ?></a>
-					<a class="button-secondary" href="<?php echo esc_url( $this->build_url( 'foogallery-plans' ) ); ?>" target="_blank"><?php echo __( 'Compare Plans', 'foogallery' ); ?></a>
+					<a class="button-primary" href="<?php echo esc_url( $this->build_url( $datasource_url_name ) ); ?>" target="_blank"><?php echo esc_html__( 'Learn More', 'foogallery' ); ?></a>
+					<a class="button-secondary" href="<?php echo esc_url( $this->build_url( 'foogallery-plans' ) ); ?>" target="_blank"><?php echo esc_html__( 'Compare Plans', 'foogallery' ); ?></a>
 				</div>
 			</div>
 <?php
