5a09e03c0d404a1065597e0d55e799679d889877
Michael Pham <michael.pham@aveo-global.com>
2025-10-20 10:57:46 +0700

    Fix WordPress.Security.EscapeOutput errors in cache class

--- includes/class-foogallery-cache.php
+++ includes/class-foogallery-cache.php
@@ -149,7 +149,7 @@ if ( ! class_exists( 'FooGallery_Cache' ) ) {
 
 			if ( !empty( $gallery_cache ) && is_string( $gallery_cache ) ) {
 				//output the cached gallery html
-				echo $gallery_cache;
+				echo $gallery_cache; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Cached HTML output
 				return true; //return that we will override
 			} else {
 				//we should cache the result for next time
@@ -198,7 +198,7 @@ if ( ! class_exists( 'FooGallery_Cache' ) ) {
 		function render_settings( $args ) {
 			if ('clear_gallery_cache_button' === $args['type'] ) { ?>
 				<div id="foogallery_clear_html_cache_container">
-					<input type="button" data-nonce="<?php echo esc_attr( wp_create_nonce( 'foogallery_clear_html_cache' ) ); ?>" class="button-primary foogallery_clear_html_cache" value="<?php _e( 'Clear Gallery HTML Cache', 'foogallery' ); ?>">
+					<input type="button" data-nonce="<?php echo esc_attr( wp_create_nonce( 'foogallery_clear_html_cache' ) ); ?>" class="button-primary foogallery_clear_html_cache" value="<?php esc_attr_e( 'Clear Gallery HTML Cache', 'foogallery' ); ?>">
 					<span id="foogallery_clear_html_cache_spinner" style="position: absolute" class="spinner"></span>
 				</div>
 			<?php }
@@ -212,7 +212,7 @@ if ( ! class_exists( 'FooGallery_Cache' ) ) {
 
 				$this->clear_all_gallery_caches();
 
-				_e('The cache for all galleries has been cleared!', 'foogallery' );
+				esc_html_e('The cache for all galleries has been cleared!', 'foogallery' );
 				die();
 			}
 		}
