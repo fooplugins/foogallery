5257cc1e8be59e178dd2cede0cab70f3dcfbf453
Michael Pham <michael.pham@aveo-global.com>
2025-10-20 11:22:58 +0700

    Fix WordPress.Security.EscapeOutput errors in carousel template

--- extensions/default-templates/carousel/gallery-carousel.php
+++ extensions/default-templates/carousel/gallery-carousel.php
@@ -13,13 +13,13 @@ $foogallery_default_classes = foogallery_build_class_attribute_safe( $current_fo
 $foogallery_default_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_default_classes ) );
 $foogallery_active_class = 'fg-item-active';
 $args = foogallery_gallery_template_arguments();
-?><div <?php echo $foogallery_default_attributes; ?>>
+?><div <?php echo $foogallery_default_attributes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
 	<button type="button" class="fg-carousel-prev"></button>
 	<div class="fg-carousel-inner">
 		<div class="fg-carousel-center"></div>
 		<?php foreach ( foogallery_current_gallery_attachments_for_rendering() as $attachment ) {
 			$args['class'] = $foogallery_active_class;
-			echo foogallery_attachment_html( $attachment, $args );
+			echo foogallery_attachment_html( $attachment, $args ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			$foogallery_active_class = '';
 		} ?>
 	</div>
