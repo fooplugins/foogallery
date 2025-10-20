97fa741c07a096e8de2de9ac77af4aee49ad901b
Michael Pham <michael.pham@aveo-global.com>
2025-10-20 11:28:14 +0700

    Fix WordPress.Security.EscapeOutput errors in justified template

--- extensions/default-templates/justified/gallery-justified.php
+++ extensions/default-templates/justified/gallery-justified.php
@@ -8,8 +8,8 @@ $lightbox = foogallery_gallery_template_setting_lightbox();
 $foogallery_justified_classes = foogallery_build_class_attribute_safe( $current_foogallery, 'foogallery-lightbox-' . $lightbox );
 $foogallery_justified_attributes = foogallery_build_container_attributes_safe( $current_foogallery, array( 'class' => $foogallery_justified_classes) );
 ?>
-<div <?php echo $foogallery_justified_attributes; ?>>
+<div <?php echo $foogallery_justified_attributes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
 	<?php foreach ( foogallery_current_gallery_attachments_for_rendering() as $attachment ) {
-		echo foogallery_attachment_html( $attachment );
+		echo foogallery_attachment_html( $attachment ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	} ?>
 </div>
