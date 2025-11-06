56084837ca48213ab00d4c90c25ea90301431505
Brad Vincent <bradvin@gmail.com>
2025-11-06 17:06:24 +0000

    updated the client asset enqueue process
    introduced new filters for injecting dependancies for foogallery assets
    split the core from ready, so that core is loaded first, followed by others, then custom (if applicable) and then lastly ready

--- extensions/default-templates/functions.php
+++ extensions/default-templates/functions.php
@@ -11,10 +11,12 @@ function foogallery_enqueue_core_gallery_template_style() {
 	$css = apply_filters( 'foogallery_core_gallery_style', FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'css/foogallery' . $filename . '.css' );
 	foogallery_enqueue_style( 'foogallery-core', $css, array(), FOOGALLERY_VERSION );
 
+	$feature_deps = apply_filters( 'foogallery_feature_style_deps', array( 'foogallery-core' ) );
+
 	if ( foogallery_get_setting( 'custom_css', '' ) !== '' ) {
 		$custom_assets = get_option( FOOGALLERY_OPTION_CUSTOM_ASSETS );
 		if ( is_array( $custom_assets ) && array_key_exists( 'style', $custom_assets ) ) {
-			foogallery_enqueue_style( 'foogallery-custom', $custom_assets['style'], array('foogallery-core'), FOOGALLERY_VERSION );
+			foogallery_enqueue_style( 'foogallery-custom', $custom_assets['style'], $feature_deps, FOOGALLERY_VERSION );
 		}
 	}
 }
@@ -34,8 +36,9 @@ function foogallery_enqueue_core_gallery_template_script( $deps = null ) {
 		$deps = array( 'jquery' );
 	}
 
-	$filename = foogallery_is_debug() ? '' : '.min';
-	$js = apply_filters( 'foogallery_core_gallery_script', FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'js/foogallery' . $filename . '.js' );
+	//enqueue the core script
+	$filename_suffix = foogallery_is_debug() ? '' : '.min';
+	$js = apply_filters( 'foogallery_core_gallery_script', FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'js/foogallery' . $filename_suffix . '.js' );
 	$deps = apply_filters( 'foogallery_core_gallery_script_deps', $deps );
 
     if ( foogallery_get_setting( 'enqueue_polyfills', false ) ) {
@@ -45,13 +48,22 @@ function foogallery_enqueue_core_gallery_template_script( $deps = null ) {
 
 	wp_enqueue_script( 'foogallery-core', $js, $deps, FOOGALLERY_VERSION );
 	do_action( 'foogallery_enqueue_script-core', $js );
+	$feature_deps = array( 'foogallery-core' );
 
+	//apply filters to the custom deps, so scripts are loaded in correct order.
+	$feature_deps = apply_filters( 'foogallery_feature_script_deps', $feature_deps );
+
+	//enqueue the custom JS
 	if ( foogallery_get_setting( 'custom_js', '' ) !== '' ) {
 		$custom_assets = get_option( FOOGALLERY_OPTION_CUSTOM_ASSETS );
 		if ( is_array( $custom_assets ) && array_key_exists( 'script', $custom_assets ) ) {
-			wp_enqueue_script( 'foogallery-custom', $custom_assets['script'], array('foogallery-core'), FOOGALLERY_VERSION );
+			wp_enqueue_script( 'foogallery-custom', $custom_assets['script'], $feature_deps, FOOGALLERY_VERSION );
+			$feature_deps[] = 'foogallery-custom';
 		}
 	}
+	
+	//enqueue the ready script
+	wp_enqueue_script( 'foogallery-ready', FOOGALLERY_DEFAULT_TEMPLATES_EXTENSION_SHARED_URL . 'js/foogallery.ready' . $filename_suffix . '.js', $feature_deps, FOOGALLERY_VERSION );
 }
 
 /**
