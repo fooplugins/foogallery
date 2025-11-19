b1d6e071115c31b4fad83c1f2e788b3d31b6e835
Brad Vincent <bradvin@gmail.com>
2025-11-19 11:15:40 +0000

    update to block version

--- gutenberg/assets/blocks.asset.php
+++ gutenberg/assets/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fd0202593346ea4ba56e');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cc33d6f3b2f7e6d5d26a');
